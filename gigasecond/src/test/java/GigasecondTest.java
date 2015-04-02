00d01105d2aa32ceced1f8d4f20c6494d0b9fd6c
Piet van Dongen <piet@studio-donder.nl>
2015-04-02 16:31:23 +0200

    Added exercise gigasecond (see #3)

---
+++ gigasecond/src/test/java/GigasecondTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.junit.Assert.assertEquals;
+
+public class GigasecondTest {
+
+    @Test
+    public void modernTime() {
+        Gigasecond gigaSecond = new Gigasecond(LocalDate.of(2011, Month.APRIL, 25));
+
+        assertEquals(gigaSecond.Date(), LocalDateTime.of(2043, Month.JANUARY, 1, 1, 46, 40));
+    }
+
+    @Test
+    public void afterEpochTime() {
+        Gigasecond gigaSecond = new Gigasecond(LocalDate.of(1977, Month.JUNE, 13));
+
+        assertEquals(gigaSecond.Date(), LocalDateTime.of(2009, Month.FEBRUARY, 19, 1, 46, 40));
+    }
+
+    @Test
+    public void beforeEpochTime() {
+        Gigasecond gigaSecond = new Gigasecond(LocalDate.of(1959, Month.JULY, 19));
+
+        assertEquals(gigaSecond.Date(), LocalDateTime.of(1991, Month.MARCH, 27, 1, 46, 40));
+    }
+
+    @Test
+    public void withFullTimeSpecified() {
+        Gigasecond gigaSecond = new Gigasecond(LocalDateTime.of(2015, Month.JANUARY, 24, 22, 0, 0));
+
+        assertEquals(gigaSecond.Date(), LocalDateTime.of(2046, Month.OCTOBER, 2, 23, 46, 40));
+    }
+
+    @Test
+    public void withFullTimeSpecifiedAndDayRollover() {
+        Gigasecond gigaSecond = new Gigasecond(LocalDateTime.of(2015, Month.JANUARY, 24, 23, 59, 59));
+
+        assertEquals(gigaSecond.Date(), LocalDateTime.of(2046, Month.OCTOBER, 3, 1, 46, 39));
+    }
+}
