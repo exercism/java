cb10cd2adb04dd55760b4785175d4e1e3dbd7993
Piet van Dongen <piet@studio-donder.nl>
2015-04-03 15:48:20 +0200

    Added exercise binary

---
+++ binary/example.java
@@ -0,0 +1,35 @@
+public class Binary {
+
+    private String binary;
+    private int decimal;
+
+    public Binary(String binary) {
+        this.binary = binary;
+        this.decimal = getDecimalFromBinary(binary);
+    }
+
+    public int getDecimal() {
+        return decimal;
+    }
+
+    private static int getDecimalFromBinary(String binary) {
+        if (!isValid(binary)) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < binary.length(); i++) {
+            sum += Character.getNumericValue(binary.charAt(i)) * (int) Math.pow(2, binary.length() - i - 1);
+        }
+
+        return sum;
+    }
+
+    private static boolean isValid(String binaryRepresentation) {
+        boolean isValid = binaryRepresentation.chars()
+                .allMatch(x -> Character.isDigit((char) x) && Character.getNumericValue((char) x) < 2);
+
+        return isValid;
+    }
+}
