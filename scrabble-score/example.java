62c4c39b5db9b873cf268e0feb0b87e568a78063
Piet van Dongen <piet@studio-donder.nl>
2015-04-03 09:32:58 +0200

    Added scrabble-score exercise

---
+++ scrabble-score/example.java
@@ -0,0 +1,59 @@
+public class Scrabble {
+
+    private String word;
+
+    private static final Map<Character, Integer> letterScores = new HashMap<>();
+
+    static {
+        letterScores.put('a', 1);
+        letterScores.put('e', 1);
+        letterScores.put('i', 1);
+        letterScores.put('o', 1);
+        letterScores.put('u', 1);
+        letterScores.put('l', 1);
+        letterScores.put('n', 1);
+        letterScores.put('r', 1);
+        letterScores.put('s', 1);
+        letterScores.put('t', 1);
+        letterScores.put('d', 2);
+        letterScores.put('g', 2);
+        letterScores.put('b', 3);
+        letterScores.put('c', 3);
+        letterScores.put('m', 3);
+        letterScores.put('p', 3);
+        letterScores.put('f', 4);
+        letterScores.put('h', 4);
+        letterScores.put('v', 4);
+        letterScores.put('w', 4);
+        letterScores.put('y', 4);
+        letterScores.put('k', 5);
+        letterScores.put('j', 8);
+        letterScores.put('x', 8);
+        letterScores.put('q', 10);
+        letterScores.put('z', 10);
+    }
+
+    private Object score;
+
+    public Scrabble(String word) {
+        this.word = word;
+    }
+
+    public int getScore() {
+        return getScore(word);
+    }
+
+    public static int getScore(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            return 0;
+        }
+
+        int score = 0;
+
+        for (char letter : input.toLowerCase().toCharArray()) {
+            score += letterScores.get(letter);
+        }
+
+        return score;
+    }
+}
