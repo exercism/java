0ea1cc4248ce2f922cfb00b3f2e75ec3ca9e7d29
Piet van Dongen <piet@studio-donder.nl>
2015-04-04 20:06:33 +0200

    Example now preserves collection order (closes #38)

--- accumulate/example.java
+++ accumulate/example.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 
 public class Accumulate {
 
-	public static <T> Collection<T> accumulate(Collection<T> collection, Function<T, T> function) {
+	public static <T> List<T> accumulate(List<T> collection, Function<T, T> function) {
 		List<T> newCollection = new ArrayList<>();
 
 		for (T item : collection) {
