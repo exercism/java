e30d1f11508900cc603cf2816a841a498a2ac4fc
t0tec <t0tec.olmec@gmail.com>
2015-04-06 14:51:43 +0200

    Added Pascal's Triangle exercise

---
+++ pascals-triangle/src/main/java/PascalsTriangle.java
@@ -0,0 +1,10 @@
+public class PascalsTriangle {
+
+    public static int[][] computeTriangle(int rows) {
+        return null;
+    }
+
+    public static boolean isTriangle(int[][] triangle) {
+        return false;
+    }
+}
