2c3fcdfb0d2ab80e29ef2f5c33334432e9c4fd45
Dan Neumann <dan@bestangle.com>
2016-07-08 08:33:23 -0700

    Serve exercises from the HEAD of master of xjava
    - otherwise this journey is testing not the latest copy of xjava, but
      what's at origin/master.
    - add progress indicator.
    - x-api can't serve binaries; must clean xjava of any build artifacts
      (e.g. .class files) before attempting to serve.
    
    Signed-off-by: Bronwyn Perry-Huston <bronhuston@gmail.com>
    Signed-off-by: Cen Ge <cge@corelogic.com>
    Signed-off-by: John Ryan <jtigger@infosysengr.com>

--- bin/run-journey-test-in-ci.sh
+++ bin/run-journey-test-in-ci.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-
-# Local clean-up
-rm -rf ~/workspace/exercism/x-api
-killall ruby
-
 set -e
 
-export GOPATH=$HOME/workspace
-REPO_ROOT=~/workspace/exercism/xjava
+eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=1.5.3 bash)"
+export HOME=$PWD/..
+export GOPATH=$HOME
+
+REPO_ROOT=~/xjava
 EXERCISM_HOME=~/workspace/exercism/exercises
-SET_RUBY_VER_CMD="rbenv local 2.2.1"
+SET_RUBY_VER_CMD=""
 
 mkdir -p ~/workspace/exercism
 cd ~/workspace/exercism/
 git clone https://github.com/exercism/x-api
 cd x-api
 git submodule init -- metadata
-git submodule init -- tracks/java
 git submodule update
+( cd tracks && rmdir java && ln -s $REPO_ROOT java && cd java/exercises && gradle clean )
 
 $SET_RUBY_VER_CMD
 gem install bundler
@@ -40,10 +38,18 @@ exercism debug
 
 cd $REPO_ROOT
 EXERCISES=`cat config.json | jq '.problems []' --raw-output`
+TOTAL_EXERCISES=`cat config.json | jq '.problems | length'`
+CURRENT_EXERCISE_NUMBER=1
 
 for EXERCISE in $EXERCISES; do
+  echo ''
+  echo '****' Testing $EXERCISE '('$CURRENT_EXERCISE_NUMBER / $TOTAL_EXERCISES') ****'
+  echo ''
   exercism fetch java $EXERCISE
   cp -R -H $REPO_ROOT/exercises/$EXERCISE/src/example/java/* $EXERCISM_HOME/java/$EXERCISE/src/main/java/
   cd $EXERCISM_HOME/java/$EXERCISE/
   gradle test
+
+  CURRENT_EXERCISE_NUMBER=$((CURRENT_EXERCISE_NUMBER + 1))
 done
+
