c232bccaa6aa42e1ceb2d2289c7cc5896ce9c9fb
Bronwyn Perry-Huston <bronhuston@gmail.com>
2016-07-08 08:32:43 -0700

    Stand-up X-API in development mode
    Signed-off-by: Cen Ge <cge@corelogic.com>
    Signed-off-by: Dan Neumann <dan@bestangle.com>
    Signed-off-by: John Ryan <jtigger@infosysengr.com>

--- bin/journey-setup.sh
+++ bin/journey-setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-rvm install 2.2.1
-rvm 2.2.1 do rvm env --path
-source $(rvm 2.2.1 do rvm env --path)
-
 git clone https://github.com/exercism/x-api
 cd x-api
+git submodule init -- metadata
 git submodule init -- tracks/java
 git submodule update
 
 gem install bundler
 bundle install
-rackup
+
+RACK_ENV=development rackup&
+
+sleep 5
+
+ps aux | grep rackup
+
+curl -i 'localhost:9292/tracks/java/hello-world'
