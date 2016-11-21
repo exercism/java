eebb41ef5fcdd6f59484c4d0153ed6804dbb866c
Fuad <Fuad.Mohamoud@und.edu>
2016-11-21 10:40:00 -0600

    Updates
    -Fixed White space by copy pasting code and clicking Beautify on this
    site: http://codebeautify.org/javaviewer
    -Modified names for Test Cases as suggested—> now includes numbers
    being tested along with max number in method call.

--- exercises/SumOfMultiples/src/java/SumOfMultiples.java
+++ exercises/SumOfMultiples/src/java/SumOfMultiples.java
@@ -1,4 +1,4 @@
 import java.util.Arrays;
 public class SumOfMultiples {
-	
+    
 }
--- exercises/SumOfMultiples/src/test/SumOfMultiplesTest.java
+++ exercises/SumOfMultiples/src/test/SumOfMultiplesTest.java
@@ -4,150 +4,184 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 public class SumOfMultiplesTest {
-
-	
-	@Test
-	public void testSumOfMultiplesInputOne() {
-		
-		SumOfMultiples mySum = new SumOfMultiples();
-		int[] set={3,5};
-		int output = mySum.Sum(1,set);
-		assertEquals(0,output);
-		
-	}
-	
-	
-	@Test
-	@Ignore
-    public void testSumOfMultiplesInputFour() {
-		
-		SumOfMultiples mySum = new SumOfMultiples();
-		int[] set={3,5};
-		int output = mySum.Sum(4,set);
-		assertEquals(3,output);
-		
-	}
     
     
-	@Test
-	@Ignore
-     public void testSumOfMultiplesInputTen() {
-		
-		SumOfMultiples mySum = new SumOfMultiples();
-		int[] set={3,5};
-		int output = mySum.Sum(10,set);
-		assertEquals(23,output);
-		
-	}
-     
-	
-	@Test
-	@Ignore
-     public void testSumOfMultiplesInputOneHundred() {
-		
-		SumOfMultiples mySum = new SumOfMultiples();
-		int[] set={3,5};
-		int output = mySum.Sum(100,set);
-		assertEquals(2318,output);
-		
-	}
+    @Test
+    public void testSumOfMultiplesOf3and4UpToOne() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            3,
+            5
+        };
+        int output = mySum.Sum(1, set);
+        assertEquals(0, output);
+        
+    }
     
-	
-	@Test
-	@Ignore
-     public void testSumOfMultiplesInputOneThousand() {
-		
-		SumOfMultiples mySum = new SumOfMultiples();
-		int[] set={3,5};
-		int output = mySum.Sum(1000,set);
-		assertEquals(233168,output);
-		
-	}
-     
-	
-	@Test
-	@Ignore
-     public void testSumOfMultiplesInputTwenty() {
-		
-		SumOfMultiples mySum = new SumOfMultiples();
-		int[] set={7,13,17};
-		int output = mySum.Sum(20,set);
-		assertEquals(51,output);
-		
-	}
-     
-	
-	@Test
-	@Ignore
-     public void testSumOfMultiplesInputThirty() {
-		
-		SumOfMultiples mySum = new SumOfMultiples();
-		int[] set={4,6};
-		int output = mySum.Sum(15,set);
-		assertEquals(30,output);
-		
-	}
-     
-	
-	@Test
-	@Ignore
-     public void testSumOfMultiplesInputOneHundredFifty() {
-		
-		SumOfMultiples mySum = new SumOfMultiples();
-		int[] set={5,6,8};
-		int output = mySum.Sum(150,set);
-		assertEquals(4419,output);
-		
-	}
-	
-	
-	@Test
-	@Ignore
-     public void testSumOfMultiplesInputFiftyOne() {
- 		
- 		SumOfMultiples mySum = new SumOfMultiples();
- 		int[] set={5,25};
- 		int output = mySum.Sum(51,set);
- 		assertEquals(275,output);
- 		
- 	}
-	
-	
-	@Test
-	@Ignore
-     public void testSumOfMultiplesInputTenThousand() {
-  		
-  		SumOfMultiples mySum = new SumOfMultiples();
-  		int[] set={43,47};
-  		int output = mySum.Sum(10000,set);
-  		assertEquals(2203160,output);
-  		
-  	}
-	
-	
-	@Test
-	@Ignore
-     public void testSumOfMultiplesInputOneNumberArray() {
-   		
-   		SumOfMultiples mySum = new SumOfMultiples();
-   		int[] set={1};
-   		int output = mySum.Sum(100,set);
-   		assertEquals(4950,output);
-   		
-   	}
-	
-	
-	@Test
-	@Ignore
-     public void testSumOfMultiplesEmptyArray() {
-    		
-    		SumOfMultiples mySum = new SumOfMultiples();
-    		int[] set={};
-    		int output = mySum.Sum(10000,set);
-    		assertEquals(0,output);
-    		
-    	}
-	
-	
-
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOf3and5UpToFour() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            3,
+            5
+        };
+        int output = mySum.Sum(4, set);
+        assertEquals(3, output);
+        
+    }
+    
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOf3and5UpToTen() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            3,
+            5
+        };
+        int output = mySum.Sum(10, set);
+        assertEquals(23, output);
+        
+    }
+    
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOf3and5UpToOneHundred() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            3,
+            5
+        };
+        int output = mySum.Sum(100, set);
+        assertEquals(2318, output);
+        
+    }
+    
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOf3and5UpToOneThousand() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            3,
+            5
+        };
+        int output = mySum.Sum(1000, set);
+        assertEquals(233168, output);
+        
+    }
+    
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOf7and13and17UpToTwenty() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            7,
+            13,
+            17
+        };
+        int output = mySum.Sum(20, set);
+        assertEquals(51, output);
+        
+    }
+    
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOf4and6UpToFifteen() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            4,
+            6
+        };
+        int output = mySum.Sum(15, set);
+        assertEquals(30, output);
+        
+    }
+    
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOf5and6and8UpToOneHundredFifty() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            5,
+            6,
+            8
+        };
+        int output = mySum.Sum(150, set);
+        assertEquals(4419, output);
+        
+    }
+    
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOf5and25UpToTwoHundredSeventyFive() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            5,
+            25
+        };
+        int output = mySum.Sum(51, set);
+        assertEquals(275, output);
+        
+    }
+    
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOf43and47UpToTenThousand() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            43,
+            47
+        };
+        int output = mySum.Sum(10000, set);
+        assertEquals(2203160, output);
+        
+    }
+    
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOfOneUpToOneHundred() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {
+            1
+        };
+        int output = mySum.Sum(100, set);
+        assertEquals(4950, output);
+        
+    }
+    
+    
+    @Test
+    @Ignore
+    public void testSumOfMultiplesOfNoneUpToTenThousand() {
+        
+        SumOfMultiples mySum = new SumOfMultiples();
+        int[] set = {};
+        int output = mySum.Sum(10000, set);
+        assertEquals(0, output);
+        
+    }
+    
+    
+    
 }
