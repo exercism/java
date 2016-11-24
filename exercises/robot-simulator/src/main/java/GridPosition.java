78295f4e69ca85924da8b75c588b85dc3715189d
Stuart Kent <skent.dev@gmail.com>
2016-11-23 23:43:55 -0500

    robot-simulator: add to track

---
+++ exercises/robot-simulator/src/main/java/GridPosition.java
@@ -0,0 +1,29 @@
+final class GridPosition {
+
+    final int x;
+
+    final int y;
+
+    GridPosition(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /*
+     * This equals method is of deliberately narrow scope (only allows comparison with another GridPosition) to increase
+     * readability. In general, one should provide a full implementation of Object.equals(Object obj) and a
+     * corresponding implementation of Object.hashCode(). See
+     *
+     * https://docs.oracle.com/javase/7/docs/api/java/lang/Object.html#equals(java.lang.Object)
+     *
+     * and
+     *
+     * https://docs.oracle.com/javase/7/docs/api/java/lang/Object.html#hashCode()
+     *
+     * for more information.
+     */
+    boolean equals(final GridPosition gridPosition) {
+        return this.x == gridPosition.x && this.y == gridPosition.y;
+    }
+
+}
