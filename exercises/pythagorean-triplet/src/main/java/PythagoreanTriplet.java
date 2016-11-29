a5878711a7d3ce8b030b76c2e0f8966fd5071c12
Dmitry Noranovich <javaeeeee@gmail.com>
2016-11-29 15:59:10 -0500

    Implemented changes except introducing the Builder Pattern

---
+++ exercises/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -0,0 +1,7 @@
+
+public class PythagoreanTriplet {
+
+    public static final String MAX_FACTOR = "maxFactor";
+    public static final String MIN_FACTOR = "minFactor";
+    public static final String SUM = "sum";
+}
