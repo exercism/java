3c547166238973c2de4d90944d625e327d5fa471
Dmitry Noranovich <javaeeeee@gmail.com>
2016-12-05 15:49:18 -0500

    Created an empty project

---
+++ exercises/custom-set/src/test/java/CustomSetTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CustomSetTest {
+
+    public CustomSetTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+}
