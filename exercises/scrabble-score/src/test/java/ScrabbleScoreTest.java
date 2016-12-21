c8b4918adcf87c294bbde1e973ba6924bd0e6845
Frida Tveit <Frida.Tveit@softwire.com>
2016-12-21 15:22:57 +0000

    Issue #166: Added format string to @Parameters annotation in ScrabbleScoreTest.

--- exercises/scrabble-score/src/test/java/ScrabbleScoreTest.java
+++ exercises/scrabble-score/src/test/java/ScrabbleScoreTest.java
@@ -14,7 +14,7 @@ public class ScrabbleScoreTest {
     private String input;
     private int expectedOutput;
 
-    @Parameterized.Parameters
+    @Parameterized.Parameters(name = "Test {index}: input is {0}, expected output is {1}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
                 {"", 0},
