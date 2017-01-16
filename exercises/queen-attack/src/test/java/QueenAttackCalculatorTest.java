c531aee7afd2d17b6b60468235c484d0653f1a1c
Colin Mullikin <ColinMullikin@Colins-MacBook-Pro.local>
2017-01-16 01:58:39 -0500

    Reorder queen-attack tests
    Fixes issue #241.

--- exercises/queen-attack/src/test/java/QueenAttackCalculatorTest.java
+++ exercises/queen-attack/src/test/java/QueenAttackCalculatorTest.java
@@ -8,67 +8,6 @@ import static org.junit.Assert.assertTrue;
 
 public final class QueenAttackCalculatorTest {
 
-    /*
-     * See https://github.com/junit-team/junit4/wiki/Rules for information on JUnit Rules in general and
-     * ExpectedExceptions in particular.
-     */
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
-
-    @Test
-    public void testCoordinateWithNegativeRankNotAllowed() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Coordinate must have positive rank.");
-
-        new BoardCoordinate(-2, 2);
-    }
-
-    @Ignore
-    @Test
-    public void testCoordinateWithRankGreaterThan7NotAllowed() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Coordinate must have rank <= 7.");
-
-        new BoardCoordinate(8, 4);
-    }
-
-    @Ignore
-    @Test
-    public void testCoordinateWithNegativeFileNotAllowed() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Coordinate must have positive file.");
-
-        new BoardCoordinate(2, -2);
-    }
-
-    @Ignore
-    @Test
-    public void testCoordinateWithFileGreaterThan7NotAllowed() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Coordinate must have file <= 7.");
-
-        new BoardCoordinate(4, 8);
-    }
-
-    @Ignore
-    @Test
-    public void testNullCoordinateNotAllowed() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("You must supply valid board coordinates for both Queens.");
-
-        new QueenAttackCalculator(null, new BoardCoordinate(0, 7));
-    }
-
-    @Ignore
-    @Test
-    public void testQueensMustNotOccupyTheSameSquare() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Queens may not occupy the same board coordinate.");
-
-        new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(2, 2));
-    }
-
-    @Ignore
     @Test
     public void testQueensThatDoNotShareRankFileOrDiagonalCannotAttack() {
         final QueenAttackCalculator calculator
@@ -129,6 +68,67 @@ public final class QueenAttackCalculatorTest {
                 = new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(5, 5));
 
         assertTrue(calculator.canQueensAttackOneAnother());
+    }
+
+    /*
+     * See https://github.com/junit-team/junit4/wiki/Rules for information on JUnit Rules in general and
+     * ExpectedExceptions in particular.
+     */
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Ignore
+    @Test
+    public void testCoordinateWithNegativeRankNotAllowed() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Coordinate must have positive rank.");
+
+        new BoardCoordinate(-2, 2);
+    }
+
+    @Ignore
+    @Test
+    public void testCoordinateWithRankGreaterThan7NotAllowed() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Coordinate must have rank <= 7.");
+
+        new BoardCoordinate(8, 4);
+    }
+
+    @Ignore
+    @Test
+    public void testCoordinateWithNegativeFileNotAllowed() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Coordinate must have positive file.");
+
+        new BoardCoordinate(2, -2);
+    }
+
+    @Ignore
+    @Test
+    public void testCoordinateWithFileGreaterThan7NotAllowed() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Coordinate must have file <= 7.");
+
+        new BoardCoordinate(4, 8);
+    }
+
+    @Ignore
+    @Test
+    public void testNullCoordinateNotAllowed() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("You must supply valid board coordinates for both Queens.");
+
+        new QueenAttackCalculator(null, new BoardCoordinate(0, 7));
+    }
+
+    @Ignore
+    @Test
+    public void testQueensMustNotOccupyTheSameSquare() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Queens may not occupy the same board coordinate.");
+
+        new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(2, 2));
     }
 
 }
