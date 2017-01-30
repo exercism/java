69418473a1ede8d95caa84064d7ed78e53fa0b21
Frida Tveit <Frida.Tveit@softwire.com>
2017-01-30 18:57:40 +0000

    Issue #261: Bring phone-number exercise in to compliance with canonical data

--- exercises/phone-number/src/example/java/PhoneNumber.java
+++ exercises/phone-number/src/example/java/PhoneNumber.java
@@ -7,23 +7,34 @@ public class PhoneNumber {
     }
 
     private String extractDigits(String dirtyNumber) {
-        return dirtyNumber.replaceAll("[^\\d]", "");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : dirtyNumber.toCharArray()) {
+            if (c == ' ' || c == '.' || c == '(' || c == ')' || c == '-') {
+                // Remove spaces, dots, parentheses and hyphens
+                continue;
+            }
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("Illegal character in phone number. "
+                        + "Only digits, spaces, parentheses, hyphens or dots accepted.");
+            }
+            stringBuilder.append(c);
+        }
+        return stringBuilder.toString();
     }
 
     private String normalize(String number) {
-        if(number.length() > 11 || number.length() < 10){
+        if (number.length() > 11 || number.length() < 10) {
             throw new IllegalArgumentException("Number must be 10 or 11 digits");
         }
 
-        if(number.length() == 11){
-            if(number.startsWith("1")){
+        if (number.length() == 11) {
+            if (number.startsWith("1")) {
                 number = number.substring(1, number.length());
-            }
-            else{
+            } else {
                 throw new IllegalArgumentException("Can only have 11 digits if number starts with '1'");
             }
-        } 
-        
+        }
+
         return number;
     }
 
--- exercises/phone-number/src/test/java/PhoneNumberTest.java
+++ exercises/phone-number/src/test/java/PhoneNumberTest.java
@@ -1,11 +1,16 @@
-import org.junit.Test;
 import org.junit.Ignore;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class PhoneNumberTest {
+    private final static String wrongLengthExceptionMessage = "Number must be 10 or 11 digits";
+    private final static String numberIs11DigitsButDoesNotStartWith1ExceptionMessage =
+            "Can only have 11 digits if number starts with '1'";
+    private final static String illegalCharacterExceptionMessage =
+            "Illegal character in phone number. Only digits, spaces, parentheses, hyphens or dots accepted.";
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -16,7 +21,7 @@ public class PhoneNumberTest {
         final String actualNumber = new PhoneNumber("(123) 456-7890").getNumber();
 
         assertEquals(
-            expectedNumber, actualNumber
+                expectedNumber, actualNumber
         );
     }
 
@@ -27,8 +32,35 @@ public class PhoneNumberTest {
         final String actualNumber = new PhoneNumber("123.456.7890").getNumber();
 
         assertEquals(
-            expectedNumber, actualNumber
+                expectedNumber, actualNumber
         );
+    }
+
+    @Ignore
+    @Test
+    public void cleansNumberWithMultipleSpaces() {
+        final String expectedNumber = "1234567890";
+        final String actualNumber = new PhoneNumber("123 456   7890   ").getNumber();
+
+        assertEquals(
+                expectedNumber, actualNumber
+        );
+    }
+
+    @Ignore
+    @Test
+    public void invalidWhen9Digits() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(wrongLengthExceptionMessage);
+        new PhoneNumber("123456789");
+    }
+
+    @Ignore
+    @Test
+    public void invalidWhen11Digits() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(numberIs11DigitsButDoesNotStartWith1ExceptionMessage);
+        new PhoneNumber("21234567890");
     }
 
     @Ignore
@@ -38,24 +70,40 @@ public class PhoneNumberTest {
         final String actualNumber = new PhoneNumber("11234567890").getNumber();
 
         assertEquals(
-            expectedNumber, actualNumber
+                expectedNumber, actualNumber
         );
     }
 
     @Ignore
     @Test
-    public void invalidWhenOnly11Digits() {
+    public void invalidWhen12Digits() {
         expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Can only have 11 digits if number starts with '1'");
-        new PhoneNumber("21234567890").getNumber();
+        expectedException.expectMessage(wrongLengthExceptionMessage);
+        new PhoneNumber("321234567890");
     }
 
     @Ignore
     @Test
-    public void invalidWhen9Digits() {
+    public void invalidWithLetters() {
         expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Number must be 10 or 11 digits");
-        new PhoneNumber("123456789").getNumber();
+        expectedException.expectMessage(illegalCharacterExceptionMessage);
+        new PhoneNumber("123-abc-7890");
+    }
+
+    @Ignore
+    @Test
+    public void invalidWithPunctuation() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(illegalCharacterExceptionMessage);
+        new PhoneNumber("123-@:!-7890");
+    }
+
+    @Ignore
+    @Test
+    public void invalidWithRightNumberOfDigitsButLettersMixedIn() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(illegalCharacterExceptionMessage);
+        new PhoneNumber("1a2b3c4d5e6f7g8h9i0j");
     }
 
     @Ignore
@@ -65,7 +113,7 @@ public class PhoneNumberTest {
         final String actualAreaCode = new PhoneNumber("1234567890").getAreaCode();
 
         assertEquals(
-            expectedAreaCode, actualAreaCode
+                expectedAreaCode, actualAreaCode
         );
     }
 
@@ -76,7 +124,7 @@ public class PhoneNumberTest {
         final String actualPrettyNumber = new PhoneNumber("1234567890").pretty();
 
         assertEquals(
-            expectedPrettyNumber, actualPrettyNumber
+                expectedPrettyNumber, actualPrettyNumber
         );
     }
 
@@ -87,7 +135,7 @@ public class PhoneNumberTest {
         final String actualPrettyNumber = new PhoneNumber("11234567890").pretty();
 
         assertEquals(
-            expectedPrettyNumber, actualPrettyNumber
+                expectedPrettyNumber, actualPrettyNumber
         );
     }
 
