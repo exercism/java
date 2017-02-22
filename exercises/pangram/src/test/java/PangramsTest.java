92597d145ac6d301e3936be0079ffa06e6d182e4
MichaelSpets <michael_spets@hotmail.com>
2017-02-22 23:55:46 +0200

    pangram: rename missingLettersReplacedByNumbers [Fix #313]

--- exercises/pangram/src/test/java/PangramsTest.java
+++ exercises/pangram/src/test/java/PangramsTest.java
@@ -44,7 +44,7 @@ public class PangramsTest {
 
     @Ignore
     @Test
-    public void missingLettersReplacedByNumbersIsNotPangram() {
+    public void phraseWithMissingLettersReplacedByNumbersIsNotPangram() {
         assertFalse(Pangrams.isPangram("\"7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog\""));
     }
 
