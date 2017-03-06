126a6c13afc41a8b9b8a51266d5e2e210a4d06fc
Frida Johanne Tveit <frida.tveit@softwire.com>
2017-03-06 19:18:56 +0000

    pangram: make object-orientated

--- exercises/pangram/src/example/java/Pangrams.java
+++ exercises/pangram/src/example/java/Pangrams.java
@@ -4,8 +4,10 @@ import java.util.stream.Collectors;
 public class Pangrams {
     private static final int alphaLength = 26;
 
-    public static boolean isPangram(String input){
-        return Arrays.stream(input.replaceAll("[^a-zA-Z]","").toLowerCase().split(""))
+    public boolean isPangram(String input) {
+        return Arrays.stream(input.replaceAll("[^a-zA-Z]", "")
+                .toLowerCase()
+                .split(""))
                 .collect(Collectors.toSet())
                 .size() == alphaLength;
     }
--- exercises/pangram/src/test/java/PangramsTest.java
+++ exercises/pangram/src/test/java/PangramsTest.java
@@ -1,75 +1,82 @@
-import org.junit.Test;
+import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class PangramsTest {
+    private Pangrams pangrams;
+
+    @Before
+    public void setup() {
+        pangrams = new Pangrams();
+    }
 
 
     @Test
     public void emptySentenceIsNotPangram() {
-        assertFalse(Pangrams.isPangram(""));
+        assertFalse(pangrams.isPangram(""));
     }
 
     @Ignore
     @Test
     public void pangramWithOnlyLowerCaseLettersIsRecognizedAsPangram() {
-        assertTrue(Pangrams.isPangram("the quick brown fox jumps over the lazy dog"));
+        assertTrue(pangrams.isPangram("the quick brown fox jumps over the lazy dog"));
     }
 
     @Ignore
     @Test
     public void phraseMissingCharacterXIsNotPangram() {
-        assertFalse(Pangrams.isPangram("a quick movement of the enemy will jeopardize five gunboats"));
+        assertFalse(pangrams.isPangram("a quick movement of the enemy will jeopardize five gunboats"));
     }
 
     @Ignore
     @Test
     public void anotherPhraseMissingCharacterXIsNotPangram() {
-        assertFalse(Pangrams.isPangram("the quick brown fish jumps over the lazy dog"));
+        assertFalse(pangrams.isPangram("the quick brown fish jumps over the lazy dog"));
     }
 
     @Ignore
     @Test
     public void pangramWithUnderscoresIsRecognizedAsPangram() {
-        assertTrue(Pangrams.isPangram("\"the_quick_brown_fox_jumps_over_the_lazy_dog\""));
+        assertTrue(pangrams.isPangram("\"the_quick_brown_fox_jumps_over_the_lazy_dog\""));
     }
 
     @Ignore
     @Test
     public void pangramWithNumbersIsRecognizedAsPangram() {
-        assertTrue(Pangrams.isPangram("\"the 1 quick brown fox jumps over the 2 lazy dogs\""));
+        assertTrue(pangrams.isPangram("\"the 1 quick brown fox jumps over the 2 lazy dogs\""));
     }
 
     @Ignore
     @Test
     public void phraseWithMissingLettersReplacedByNumbersIsNotPangram() {
-        assertFalse(Pangrams.isPangram("\"7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog\""));
+        assertFalse(pangrams.isPangram("\"7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog\""));
     }
 
     @Ignore
     @Test
     public void pangramWithMixedCaseAndPunctuationIsRecognizedAsPangram() {
-        assertTrue(Pangrams.isPangram("\"Five quacking Zephyrs jolt my wax bed.\""));
+        assertTrue(pangrams.isPangram("\"Five quacking Zephyrs jolt my wax bed.\""));
     }
 
     @Ignore
     @Test
     public void pangramWithNonAsciiCharactersIsRecognizedAsPangram() {
-        assertTrue(Pangrams.isPangram("Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich."));
+        assertTrue(pangrams.isPangram("Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich."));
     }
 
 
     @Ignore
     @Test
     public void panagramInAlphabetOtherThanAsciiIsNotRecognizedAsPangram() {
-        assertFalse(Pangrams.isPangram("Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства."));
+        assertFalse(pangrams.isPangram("Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства."));
     }
 
     @Ignore
     @Test
     public void upperAndLowerCaseVersionsOfTheSameCharacterShouldNotBeCountedSeparately() {
-        assertFalse(Pangrams.isPangram("the quick brown fox jumped over the lazy FOX"));
+        assertFalse(pangrams.isPangram("the quick brown fox jumped over the lazy FOX"));
     }
 }
