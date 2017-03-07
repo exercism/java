99f4a1d30b64ba7d0dc358cde7c1d4f3de3a3c64
FridaTveit <FridaTveit@users.noreply.github.com>
2017-03-07 21:15:37 +0000

    pangram: add starter implementation and make object-based (#344)
    Pangrams is currently the third exercise in the new ordering therefore
    it should have a starter implementation.
    
    * make object-orientated
    * add starter implementation
    * rename class to PangramChecker

--- exercises/pangram/src/example/java/PangramChecker.java
+++ exercises/pangram/src/example/java/PangramChecker.java
@@ -1,11 +1,13 @@
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class Pangrams {
+public class PangramChecker {
     private static final int alphaLength = 26;
 
-    public static boolean isPangram(String input){
-        return Arrays.stream(input.replaceAll("[^a-zA-Z]","").toLowerCase().split(""))
+    public boolean isPangram(String input) {
+        return Arrays.stream(input.replaceAll("[^a-zA-Z]", "")
+                .toLowerCase()
+                .split(""))
                 .collect(Collectors.toSet())
                 .size() == alphaLength;
     }
---
+++ exercises/pangram/src/main/java/PangramChecker.java
@@ -0,0 +1,6 @@
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+        throw new UnsupportedOperationException("Method has not been implemented yet.");
+    }
+}
