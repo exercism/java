812e86ebc405563ff6776df71f9a94ef11972114
Ladent Thibault <ladent@a10p21.fil.univ-lille1.fr>
2017-03-16 11:33:54 +0100

    using camelCase instead of snake_case for tests methods

--- exercises/book-store/src/test/java/BookStoreTest.java
+++ exercises/book-store/src/test/java/BookStoreTest.java
@@ -7,120 +7,96 @@ import org.junit.Test;
 public class BookStoreTest{
 
 	@Test
-	public void Only_a_single_book(){
+	public void OnlyASingleBook(){
 		Integer[] p = {1};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(8,BookStore.CalculateTotalCost(books),2);
 	}
 
-
 	@Test
-	public void Two_of_same_book(){
+	public void TwoOfSameBook(){
 		Integer[] p = {1,1};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(16,BookStore.CalculateTotalCost(books),2);
 	}
 
-
 	@Test
-	public void Empty_basket(){
+	public void EmptyBasket(){
 		ArrayList<Integer> books = new ArrayList<>();
 		assertEquals(0,BookStore.CalculateTotalCost(books),2);
 	}
 
 	@Test
-	public void Two_different_books(){
+	public void TwoDifferentBooks(){
 		Integer[] p = {1,2};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(15.20,BookStore.CalculateTotalCost(books),2);
 	}
 
-
-
-
-
 	@Test
-	public void Three_different_books(){
+	public void ThreeDifferentBooks(){
 		Integer[] p = {1,2,3};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(21.6,BookStore.CalculateTotalCost(books),2);
 	}
 
-
 	@Test
-	public void Four_different_books(){
+	public void FourDifferentBooks(){
 		Integer[] p = {1,2,3,4};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(25.6,BookStore.CalculateTotalCost(books),2);
 
 	}
 
-
 	@Test
-	public void Five_different_books(){
+	public void FiveDifferentBooks(){
 		Integer[] p = {1,2,3,4,5};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(30,BookStore.CalculateTotalCost(books),2);
 	}
 
-
-
-
 	@Test
-	public void Two_groups_of_four_is_cheaper_than_group_of_five_plus_group_of_three(){
+	public void TwoGroupsOfFourIsCheaperThanGroupOfFivePlusGroupOfThree(){
 		Integer[] p = {1,1,2,2,3,3,4,5};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(51.20,BookStore.CalculateTotalCost(books),2);
 	}
 
 	@Test
-	public void Group_of_four_plus_group_of_two_is_cheaper_than_two_groups_of_three(){
+	public void GroupOfFourPlusGroupOfTwoIsCheaperThanTwoGroupsOfThree(){
 		Integer[] p = {1,1,2,2,3,4};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(40.8,BookStore.CalculateTotalCost(books),2);
 	}
 
 
-
 	@Test
-	public void Two_each_of_first_4_books_and_1_copy_each_of_rest(){
+	public void TwoEachOfFirst4BooksAnd1CopyEachOfRest(){
 		Integer[] p = {1,1,2,2,3,3,4,4,5};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(55.60,BookStore.CalculateTotalCost(books),2);
 	}
 
-
-	
-
 	@Test
-	public void Two_copies_of_each_book(){
+	public void TwoCopiesOfEachBook(){
 		Integer[] p = {1,1,2,2,3,3,4,4,5,5};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(60.00,BookStore.CalculateTotalCost(books),2);
 	}
 
-
 	@Test
-	public void Three_copies_of_first_book_and_2_each_of_remaining(){
+	public void ThreeCopiesOfFirstBookAnd2EachOfRemaining(){
 		Integer[] p = {1,1,2,2,3,3,4,4,5,5,1};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(68.00,BookStore.CalculateTotalCost(books),2);
 	}
 
-
 	@Test
-	public void Three_each_of_first_2_books_and_2_each_of_remaining_books(){
+	public void ThreeEachOFirst2BooksAnd2EachOfRemainingBooks(){
 		Integer[] p = {1,1,2,2,3,3,4,4,5,5,1,2};
 		ArrayList<Integer> books = new ArrayList<>(Arrays.asList(p));
 		assertEquals(75.20,BookStore.CalculateTotalCost(books),2);
 	}
-
-
-
-
-
-
-
 
 }
 
