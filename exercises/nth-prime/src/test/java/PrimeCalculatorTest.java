4c807cac84484ce27e0b47584f7baa5d177223d4
morrme <morrme@users.noreply.github.com>
2017-05-04 14:07:00 -0500

    Update PrimeCalculatorTest.java

--- exercises/nth-prime/src/test/java/PrimeCalculatorTest.java
+++ exercises/nth-prime/src/test/java/PrimeCalculatorTest.java
@@ -1,5 +1,5 @@
 import org.junit.Before;
-import org.junit.@Ignore("Remove to run test");
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
