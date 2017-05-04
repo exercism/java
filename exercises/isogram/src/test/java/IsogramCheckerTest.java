34a630ef1457aa3c737c0c6369eccfe808fcd80d
morrme <memthings@gmail.com>
2017-05-04 14:58:53 -0500

    isogram: add hint to @Ignore annotations

--- exercises/isogram/src/test/java/IsogramCheckerTest.java
+++ exercises/isogram/src/test/java/IsogramCheckerTest.java
@@ -11,63 +11,63 @@ public class IsogramCheckerTest {
         assertTrue(iso.isIsogram("duplicates"));
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void testNotIsogram() {
         IsogramChecker iso = new IsogramChecker();
         assertFalse(iso.isIsogram("eleven"));
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void testMediumLongIsogram() {
         IsogramChecker iso = new IsogramChecker();
         assertTrue(iso.isIsogram("subdermatoglyphic"));
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void testCaseInsensitive() {
         IsogramChecker iso = new IsogramChecker();
         assertFalse(iso.isIsogram("Alphabet"));
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void testIsogramWithHyphen() {
         IsogramChecker iso = new IsogramChecker();
         assertTrue(iso.isIsogram("thumbscrew-japingly"));
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void testIgnoresMultipleHyphens() {
         IsogramChecker iso = new IsogramChecker();
         assertTrue(iso.isIsogram("Hjelmqvist-Gryb-Zock-Pfund-Wax"));
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void testWorksWithGermanLetters() {
         IsogramChecker iso = new IsogramChecker();
         assertTrue(iso.isIsogram("Heizölrückstoßabdämpfung"));
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void testIgnoresSpaces() {
         IsogramChecker iso = new IsogramChecker();
         assertFalse(iso.isIsogram("the quick brown fox"));
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void testIgnoresSpaces2() {
         IsogramChecker iso = new IsogramChecker();
         assertTrue(iso.isIsogram("Emily Jung Schwartzkopf"));
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void testDuplicateAccentedLetters() {
         IsogramChecker iso = new IsogramChecker();
