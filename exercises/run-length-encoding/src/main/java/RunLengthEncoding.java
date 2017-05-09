e7481091e38c7c42c1bfb288c773e34f39f0c226
Logan Stucki <loganstucki@gmail.com>
2017-05-09 11:49:49 -0500

    Added files and updated config. TODO: Example file

---
+++ exercises/run-length-encoding/src/main/java/RunLengthEncoding.java
@@ -0,0 +1,9 @@
+public class RunLengthEncoding {
+    public String encode(String input) {
+        throw new UnsupportedOperationException("Method has not been implemented yet.");
+    }
+
+    public String decode(String input) {
+        throw new UnsupportedOperationException("Method has not been implemented yet.");
+    }
+}
