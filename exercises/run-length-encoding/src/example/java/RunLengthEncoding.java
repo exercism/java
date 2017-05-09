e7070b0e0c345375a072d24f4ccdf66c2d30ff09
Smarticles101 <loganstucki@gmail.com>
2017-05-09 16:25:19 -0500

    Fixed formatting

--- exercises/run-length-encoding/src/example/java/RunLengthEncoding.java
+++ exercises/run-length-encoding/src/example/java/RunLengthEncoding.java
@@ -3,43 +3,43 @@ import java.util.regex.Pattern;
 
 public class RunLengthEncoding {
     public String encode(String data) {
-    	String encodedData = "";
+        String encodedData = "";
 
-    	for(int i = 0; i < data.length(); i++) {
-    		char c = data.charAt(i);
+        for (int i = 0; i < data.length(); i++) {
+            char c = data.charAt(i);
 
-    		if(i != 0 && c == data.charAt(i - 1)) continue;
+            if (i != 0 && c == data.charAt(i - 1)) continue;
 
-    		int amt = 1;
+            int amt = 1;
 
-    		for(int j = i+1; j < data.length() && c == data.charAt(j); j++) {
-    			amt++;
-    		}
+            for (int j = i + 1; j < data.length() && c == data.charAt(j); j++) {
+                amt++;
+            }
 
-    		if(amt == 1) encodedData += c;
-    		else encodedData += amt + Character.toString(c);
-    	}
+            if (amt == 1) encodedData += c;
+            else encodedData += amt + Character.toString(c);
+        }
 
-    	return encodedData;
+        return encodedData;
     }
 
     public String decode(String encodedData) {
-    	String decodedData = "";
+        String decodedData = "";
         Pattern pattern = Pattern.compile("[0-9]+|[a-zA-Z\\s]");
         Matcher matcher = pattern.matcher(encodedData);
 
         while (matcher.find()) {
-        	try {
-	            int number = Integer.parseInt(matcher.group());
-	        
-	            matcher.find();
-	        
-	            while (number-- != 0) {
-	                decodedData += matcher.group();
-	            }
-        	} catch(NumberFormatException e) {
-        		decodedData+=matcher.group();
-        	}
+            try {
+                int number = Integer.parseInt(matcher.group());
+
+                matcher.find();
+
+                while (number-- != 0) {
+                    decodedData += matcher.group();
+                }
+            } catch (NumberFormatException e) {
+                decodedData += matcher.group();
+            }
         }
 
         return decodedData;
