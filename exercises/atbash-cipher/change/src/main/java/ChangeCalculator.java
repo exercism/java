92118632bb3c63794fbe55313319a713ca51553b
Leon D'souza <leonsurajdsouza@gmail.com>
2017-05-14 00:58:02 +0530

    Did some mistakes on pulling changes

--- exercises/atbash-cipher/change/src/main/java/ChangeCalculator.java
+++
@@ -1,5 +0,0 @@
-final class ChangeCalculator {
-
-
-
-}
--- exercises/atbash-cipher/etl/src/main/java/Etl.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-import java.util.Map;
-
-public class Etl {
-    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-        return null;
-    }
-}
