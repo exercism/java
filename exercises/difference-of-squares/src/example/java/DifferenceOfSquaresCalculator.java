fad9d2e16e0c86250cb9270504ecb93d957641b8
Stuart Kent <skent.dev@gmail.com>
2017-05-14 16:18:26 -0400

    difference-of-squares: standardize starter implementation

--- exercises/difference-of-squares/src/example/java/DifferenceOfSquaresCalculator.java
+++ exercises/difference-of-squares/src/example/java/DifferenceOfSquaresCalculator.java
@@ -1,19 +1,19 @@
 import java.util.stream.IntStream;
 
-public final class DifferenceOfSquaresCalculator {
+final class DifferenceOfSquaresCalculator {
 
-    public int computeSquareOfSumTo(final int input) {
+    int computeSquareOfSumTo(final int input) {
         final int sum = input * (input + 1) / 2;
         return (int) Math.pow(sum, 2);
     }
 
-    public int computeSumOfSquaresTo(final int input) {
+    int computeSumOfSquaresTo(final int input) {
         return IntStream.rangeClosed(1, input)
                 .map(i -> (int) Math.pow(i, 2))
                 .sum();
     }
 
-    public int computeDifferenceOfSquares(final int input) {
+    int computeDifferenceOfSquares(final int input) {
         return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
     }
 
