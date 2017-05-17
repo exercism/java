536ea2928099266071166b995629d2492cbc211e
morrme <morrme@users.noreply.github.com>
2017-05-17 08:15:10 -0500

    Update BracketCheckerTest.java

--- exercises/bracket-push/src/test/java/BracketCheckerTest.java
+++ exercises/bracket-push/src/test/java/BracketCheckerTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-class BracketCheckerTest {
+public class BracketCheckerTest {
 
     @Test
     public void testPairedSquareBrackets() {
