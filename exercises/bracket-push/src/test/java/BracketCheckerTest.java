7a734df5a74b91605166c3ab678939fd6c535978
morrme <morrme@users.noreply.github.com>
2017-05-17 00:30:21 -0500

    bracket-push: remove "public final" from test class
    See issue #571

--- exercises/bracket-push/src/test/java/BracketCheckerTest.java
+++ exercises/bracket-push/src/test/java/BracketCheckerTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public final class BracketCheckerTest {
+class BracketCheckerTest {
 
     @Test
     public void testPairedSquareBrackets() {
