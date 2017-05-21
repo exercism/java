12d4f627ac33c1adaa045072301224312fcadfa0
morrme <morrme@users.noreply.github.com>
2017-05-21 15:37:06 -0500

    rectangles: remove "final" from class declaration
    see #571

--- exercises/rectangles/src/test/java/RectangleCounterTest.java
+++ exercises/rectangles/src/test/java/RectangleCounterTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public final class RectangleCounterTest {
+public class RectangleCounterTest {
 
     private RectangleCounter rectangleCounter;
 
