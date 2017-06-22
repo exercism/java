30fc6d619cb8bd0467fbd046f91bd96ac55311fe
Smarticles101 <loganstucki@gmail.com>
2017-06-22 13:40:13 -0500

    Transpose exercise implemented

---
+++ exercises/transpose/src/main/java/Transpose.java
@@ -0,0 +1,5 @@
+public class Transpose {
+    public String transpose(String toTranspose) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
