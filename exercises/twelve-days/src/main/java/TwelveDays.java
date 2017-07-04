5c29756841293bc41de6a0295349707dcabf687e
Logan Stucki <loganstucki@gmail.com>
2017-07-04 12:37:23 -0500

    twelve-days: remove public's from starter implementation

--- exercises/twelve-days/src/main/java/TwelveDays.java
+++ exercises/twelve-days/src/main/java/TwelveDays.java
@@ -1,9 +1,9 @@
-public class TwelveDays {
-    public String verse(int verseNumber) {
+class TwelveDays {
+    String verse(int verseNumber) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
-    public String verses(int startVerse, int endVerse) {
+    String verses(int startVerse, int endVerse) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 }
