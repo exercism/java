3a41b31280fd7726ed4d617382054443b4d0cb7c
redshirt4 <redshirt4@users.noreply.github.com>
2017-07-04 15:05:02 -0400

    run length encoding stubs removed

--- exercises/run-length-encoding/src/main/java/RunLengthEncoding.java
+++
@@ -1,9 +0,0 @@
-public class RunLengthEncoding {
-    public String encode(String data) {
-        throw new UnsupportedOperationException("Method has not been implemented yet.");
-    }
-
-    public String decode(String encodedData) {
-        throw new UnsupportedOperationException("Method has not been implemented yet.");
-    }
-}
