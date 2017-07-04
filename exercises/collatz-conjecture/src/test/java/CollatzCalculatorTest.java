0deaff1e4b9ba4e77b765cccfab4a1ec675a6dd1
redshirt4 <redshirt4@users.noreply.github.com>
2017-07-04 14:45:57 -0400

    Addressed pull request comments:
    - Classes as a whole removed, not just method/constructor stubs
    - Commenting out tests undone per discussion, #683
    - Enum 'Plant' re-added to Kindergarten Garden per discussion
    - .keep added to empty source folders

--- exercises/collatz-conjecture/src/test/java/CollatzCalculatorTest.java
+++ exercises/collatz-conjecture/src/test/java/CollatzCalculatorTest.java
@@ -23,43 +23,43 @@ public class CollatzCalculatorTest {
 
     @Test
     public void testZeroStepsRequiredWhenStartingFrom1() {
-//        assertEquals(0, collatzCalculator.computeStepCount(1));
+        assertEquals(0, collatzCalculator.computeStepCount(1));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testCorrectNumberOfStepsWhenAllStepsAreDivisions() {
-//        assertEquals(4, collatzCalculator.computeStepCount(16));
+        assertEquals(4, collatzCalculator.computeStepCount(16));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testCorrectNumberOfStepsWhenBothStepTypesAreNeeded() {
-//        assertEquals(9, collatzCalculator.computeStepCount(12));
+        assertEquals(9, collatzCalculator.computeStepCount(12));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testAVeryLargeInput() {
-//        assertEquals(152, collatzCalculator.computeStepCount(1000000));
+        assertEquals(152, collatzCalculator.computeStepCount(1000000));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testZeroIsConsideredInvalidInput() {
-//        expectedException.expect(IllegalArgumentException.class);
-//        expectedException.expectMessage("Only natural numbers are allowed");
-//
-//        collatzCalculator.computeStepCount(0);
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Only natural numbers are allowed");
+
+        collatzCalculator.computeStepCount(0);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testNegativeIntegerIsConsideredInvalidInput() {
-//        expectedException.expect(IllegalArgumentException.class);
-//        expectedException.expectMessage("Only natural numbers are allowed");
-//
-//        collatzCalculator.computeStepCount(-15);
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Only natural numbers are allowed");
+
+        collatzCalculator.computeStepCount(-15);
     }
 
 }
