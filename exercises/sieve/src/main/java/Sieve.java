9bdcee50a69dbfaa042f9ca57f417fec3d151ad9
Smarticles101 <loganstucki@gmail.com>
2017-07-04 11:15:49 -0500

    sieve: create and validate starter implementation according to POLICIES.md

---
+++ exercises/sieve/src/main/java/Sieve.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Sieve {
+	public Sieve(int maxPrime) {
+		throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+    }
+
+    public List<Integer> getPrimes() {
+    	throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+    }
+}
