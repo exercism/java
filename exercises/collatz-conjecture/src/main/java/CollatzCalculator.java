ef200ce99de533b08b45049f993379ed88c18e11
redshirt4 <redshirt4@users.noreply.github.com>
2017-07-05 22:21:45 -0400

    Remove starter implementations (#681)
    Removed starter implementations for exercises 21 and beyond, according to POLICIES.md.

--- exercises/collatz-conjecture/src/main/java/CollatzCalculator.java
+++
@@ -1,7 +0,0 @@
-class CollatzCalculator {
-
-    int computeStepCount(int start) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
-}
--- exercises/sublist/src/main/java/RelationshipComputer.java
+++
@@ -1,5 +0,0 @@
-class RelationshipComputer<T> {
-
-
-
-}
