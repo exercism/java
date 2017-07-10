8e30cdaa6e398d213615b6c81741f266b50823b9
Leon D'souza <leonsurajdsouza@gmail.com>
2017-07-10 23:27:48 +0530

    Rotational Cipher add to track

---
+++ exercises/rotational-cipher/src/example/java/RotationalCipher.java
@@ -0,0 +1,44 @@
+
+public class RotationalCipher {
+
+    int shiftKey;
+
+    RotationalCipher(int shiftKey) {
+        this.shiftKey = shiftKey;
+    }
+
+    public String rotate(String data) {
+        StringBuilder dataStringBuilder = new StringBuilder();
+        for (char c : data.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                dataStringBuilder.append(getReplacementCharacter(c, 'A', 'Z'));
+            } else if (Character.isLowerCase(c)) {
+                dataStringBuilder.append(getReplacementCharacter(c, 'a', 'z'));
+            } else {
+                dataStringBuilder.append(c);
+            }
+        }
+        return dataStringBuilder.toString();
+    }
+
+    /**
+     * 
+     * @param characterToReplace
+     * @param alphabetCaseStart
+     * @param alphabetCaseEnd
+     * @param shiftKey
+     * @return ReplacementCharacter
+     * 
+     * For Uppercase CaseStart = 'A' and CaseEnd = 'Z'
+     * For Lowercase CaseStart = 'a' and CaseEnd = 'z'
+     */
+    public char getReplacementCharacter(char characterToReplace,
+            char alphabetCaseStart, char alphabetCaseEnd) {
+        char replacementCharacter = (char) (characterToReplace + shiftKey);
+        if (replacementCharacter > alphabetCaseEnd) {
+            replacementCharacter = (char) ((alphabetCaseStart - 1) +
+             (replacementCharacter % alphabetCaseEnd));
+        }
+        return replacementCharacter;
+    }
+}
---
+++ exercises/rotational-cipher/src/test/java/RotationalCipherTest.java
@@ -0,0 +1,86 @@
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class RotationalCipherTest {
+
+    private RotationalCipher rotationalCipher;
+
+    /*@Before
+    public void setUp() {
+        rotationalCipher = new RotationalCipher();
+    }
+    */
+    @Test
+    public void rotateSingleCharacterBy1() {
+        rotationalCipher = new RotationalCipher(1);
+        Assert.assertEquals("b", rotationalCipher.rotate("a"));
+    }
+    
+    @Ignore("Remove to run test")
+    @Test
+    public void rotateSingleCharacterBy26() {
+        rotationalCipher = new RotationalCipher(26);
+        Assert.assertEquals("a", rotationalCipher.rotate("a"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void rotateSingleCharacterBy0() {
+        rotationalCipher = new RotationalCipher(0);
+        Assert.assertEquals("a", rotationalCipher.rotate("a"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void rotateSingleCharacterBy13() {
+        rotationalCipher = new RotationalCipher(13);
+        Assert.assertEquals("z", rotationalCipher.rotate("m"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void rotateSingleCharacterWithWrapAround() {
+        rotationalCipher = new RotationalCipher(13);
+        Assert.assertEquals("a", rotationalCipher.rotate("n"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void rotateCapitalLetters() {
+        rotationalCipher = new RotationalCipher(5);
+        Assert.assertEquals("TRL", rotationalCipher.rotate("OMG"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void rotateSpaces() {
+        rotationalCipher = new RotationalCipher(5);
+        Assert.assertEquals("T R L", rotationalCipher.rotate("O M G"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void rotateNumbers() {
+        rotationalCipher = new RotationalCipher(4);
+        Assert.assertEquals("Xiwxmrk 1 2 3 xiwxmrk", rotationalCipher.rotate("Testing 1 2 3 testing"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void rotatePunctuation() {
+        rotationalCipher = new RotationalCipher(21);
+        Assert.assertEquals("Gzo'n zvo, Bmviyhv!", rotationalCipher.rotate("Let's eat, Grandma!"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void rotateAllLetters() {
+        rotationalCipher = new RotationalCipher(13);
+        Assert.assertEquals("The quick brown fox jumps over the lazy dog.",
+         rotationalCipher.rotate("Gur dhvpx oebja sbk whzcf bire gur ynml qbt."));
+    } 
+
+}
