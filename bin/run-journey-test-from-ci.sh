e5d6d3760a14bcd7dbbe89f755b249fef18055a6
Logan Stucki <loganstucki@gmail.com>
2017-08-09 14:41:18 -0500

    Add script to run journey test into it's own file in order to make it less complicated

---
+++ bin/run-journey-test-from-ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+bin/build-jq.sh
+
+pr_files_json=`curl -s https://api.github.com/repos/exercism/java/pulls/${TRAVIS_PULL_REQUEST}/files`
+modded_files=`echo $pr_files_json | bin/jq -r '.[].filename'`
+
+for file in $modded_files
+  do if [[ $file == exercises* ]] || [[ $file == config.json ]]
+    then rvm install 2.2.5
+    rvm use 2.2.5
+    for file2 in $modded_files
+      do if [[ $file2 == exercises* ]]
+        then modded_exercise=${file2#exercises/}
+        modded_exercise=${modded_exercise%%/*}
+      fi
+    done
+    bin/journey-test.sh $modded_exercise
+    break
+  fi
+done
