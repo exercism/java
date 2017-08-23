cd75179e421ebe1524c3c565532e57584fb1d93d
Tyler Matthews <matthews.tyl@gmail.com>
2017-08-23 14:53:11 -0400

    tab fix

--- exercises/robot-simulator/src/example/java/GridPosition.java
+++ exercises/robot-simulator/src/example/java/GridPosition.java
@@ -9,26 +9,26 @@ final class GridPosition {
         this.y = y;
     }
 
-   @Override
-   public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + x;
-      result = prime * result + y;
-      return result;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
    }
 
-   @Override
-   public boolean equals(Object obj) {
-      if (this == obj) {
-         return true;
-      } else if (obj == null || getClass() != obj.getClass()) {
-         return false;
-      } else if (x != ((GridPosition) obj).x || y != ((GridPosition)obj).y) {
-         return false;
-      } else {
-         return true;
-      }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        } else if (x != ((GridPosition) obj).x || y != ((GridPosition)obj).y) {
+            return false;
+        } else {
+            return true;
+        }
    }
 
 }
