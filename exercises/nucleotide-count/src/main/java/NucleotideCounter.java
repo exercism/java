bc16053e9831d3f1b7e0df3e44c730c2d8572ed9
feng liu <fengxliu@gmail.com>
2017-10-01 22:29:56 -0600

    add NucleotideCounter.java

---
+++ exercises/nucleotide-count/src/main/java/NucleotideCounter.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NucleotideCounter {
+    private static final List<Character> DICT = Arrays.asList('T', 'A', 'G', 'C');
+    private final int[] table;
+
+    public NucleotideCounter(final String s) {
+        this.table = new int[26];
+        for (final char c : s.toUpperCase().toCharArray()) {
+            this.table[c - 'A']++;
+        }
+        // System.out.println(Arrays.toString(this.table));
+    }
+
+    public int count(final char c) {
+        if (!DICT.contains(c)) {
+            throw new IllegalArgumentException("Invalid DNA nucleotide!");
+        }
+        return this.table[Character.toUpperCase(c) - 'A'];
+    }
+
+    public Map<Character, Integer> nucleotideCounts() {
+        final Map<Character, Integer> map = new HashMap<>();
+        for (final char c : DICT) {
+            map.put(c, this.table[c - 'A']);
+        }
+        return map;
+    }
+
+}
