5dba3e1ee0b235e09d0feb7b568545ede8774b7a
Morten Nygaard Åsnes <Morten.NygaardAsnes@free2.no>
2017-10-06 19:59:43 +0200

    Added stubs to Matrix #547

---
+++ exercises/matrix/src/main/java/Matrix.java
@@ -0,0 +1,24 @@
+import java.util.regex.Pattern;
+
+public class Matrix {
+
+    public Matrix(String matrixAsString) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    public int[] getRow(int rowNumber) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    public int[] getColumn(int columnNumber) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    public int getRowsCount() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    public int getColumnsCount() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
