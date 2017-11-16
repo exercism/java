89fe95e5a3e2144ecb1a7f2a30fd044dcf8477bb
Sam Warner <30871823+sjwarner-bp@users.noreply.github.com>
2017-11-16 09:59:10 +0000

    Diamond: add starter implementation. (#1001)
    * Diamond: add starter implementation.
    
    * Add import statement

---
+++ exercises/diamond/src/main/java/DiamondPrinter.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+class DiamondPrinter {
+
+    List<String> printToList(char a) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
