bd4806ab2bdfc1ddafc5c10bb0fa9df005560609
Sam Warner <sam.warner@blackpepper.co.uk>
2017-11-16 16:04:20 +0000

    isbn-verifier: Update reference solution.

--- exercises/isbn-verifier/.meta/src/reference/java/IsbnVerifier.java
+++ exercises/isbn-verifier/.meta/src/reference/java/IsbnVerifier.java
@@ -6,9 +6,11 @@ class IsbnVerifier {
 
         this.isbn = s.replace("-", "");
         int total = 0;
-        if (isbn.length() == 9 || isbn.length() == 10) {
-            for (int i = 0; i < isbn.length() - 1; i++) {
 
+        if (isbn.length() != 10) {
+            return false;
+        } else {
+            for (int i = 0; i < isbn.length() - 1; i++) {
                 if(Character.isDigit(isbn.charAt(i))) {
                     int currentCharVal = Character.getNumericValue(isbn.charAt(i));
                     total += currentCharVal * (10 - i);
@@ -16,11 +18,7 @@ class IsbnVerifier {
                     return false;
                 }
             }
-        } else {
-            return false;
-        }
 
-        if (isbn.length() == 10) {
             if (Character.isDigit(isbn.charAt(9))) {
                 total += Character.getNumericValue(isbn.charAt(9));
             } else if (isbn.charAt(9) == 'X') {
@@ -28,12 +26,12 @@ class IsbnVerifier {
             } else {
                 return false;
             }
-        }
 
-        if (total % 11 == 0) {
-            return true;
-        } else {
-            return false;
+            if (total % 11 == 0) {
+                return true;
+            } else {
+                return false;
+            }
         }
 
     }
