a58900f5a89db2c2ba9674226d7e3e9e8b95dfe6
Sam Warner <30871823+sjwarner-bp@users.noreply.github.com>
2017-11-16 12:09:26 +0000

    saddle-points: add starter implementation. (#1003)
    * saddle-points: add starter implementation.
    
    * saddle-points: Update parameter name.

---
+++ exercises/saddle-points/src/main/java/Matrix.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.Set;
+
+class Matrix {
+
+    Matrix(List<List<Integer>> values) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    Set<MatrixCoordinate> getSaddlePoints() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
