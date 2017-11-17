639f4a541c9d797b47df04ff784d378f47411148
Sam Warner <sam.warner@blackpepper.co.uk>
2017-11-17 08:22:01 +0000

    Add @FridaTveit 's improvements

--- exercises/isbn-verifier/.meta/src/reference/java/IsbnVerifier.java
+++ exercises/isbn-verifier/.meta/src/reference/java/IsbnVerifier.java
@@ -1,38 +1,35 @@
 class IsbnVerifier {
 
-    private String isbn;
+    boolean isValid(String stringToVerify) {
 
-    boolean isValid(String s) {
-
-        this.isbn = s.replace("-", "");
+        String isbn;
+        isbn = stringToVerify.replace("-", "");
         int total = 0;
 
         if (isbn.length() != 10) {
             return false;
-        } else {
-            for (int i = 0; i < isbn.length() - 1; i++) {
-                if(Character.isDigit(isbn.charAt(i))) {
-                    int currentCharVal = Character.getNumericValue(isbn.charAt(i));
-                    total += currentCharVal * (10 - i);
-                } else {
-                    return false;
-                }
-            }
+        }
 
-            if (Character.isDigit(isbn.charAt(9))) {
-                total += Character.getNumericValue(isbn.charAt(9));
-            } else if (isbn.charAt(9) == 'X') {
-                total += 10;
-            } else {
-                return false;
-            }
-
-            if (total % 11 == 0) {
-                return true;
+        for (int i = 0; i < isbn.length() - 1; i++) {
+            char currentDigit = isbn.charAt(i);
+            if(Character.isDigit(currentDigit)) {
+                int currentCharVal = Character.getNumericValue(currentDigit);
+                total += currentCharVal * (10 - i);
             } else {
                 return false;
             }
         }
+
+        char finalCharacter = isbn.charAt(isbn.length() - 1);
+        if (Character.isDigit(finalCharacter)) {
+            total += Character.getNumericValue(finalCharacter);
+        } else if (finalCharacter == 'X') {
+            total += 10;
+        } else {
+            return false;
+        }
+
+        return total % 11 == 0;
 
     }
 
--- exercises/isbn-verifier/src/test/java/IsbnVerifierTest.java
+++ exercises/isbn-verifier/src/test/java/IsbnVerifierTest.java
@@ -19,73 +19,73 @@ public class IsbnVerifierTest {
     @Ignore("Remove to run test")
     @Test
     public void invalidIsbnCheckDigit() {
-        Assert.assertEquals(false, isbnVerifier.isValid("3-598-21508-9"));
+        Assert.assertFalse(isbnVerifier.isValid("3-598-21508-9"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void validIsbnNumberWithCheckDigitOfTen() {
-        Assert.assertEquals(true, isbnVerifier.isValid("3-598-21507-X"));
+        Assert.assertTrue(isbnVerifier.isValid("3-598-21507-X"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void checkDigitIsACharacterOtherThanX() {
-        Assert.assertEquals(false, isbnVerifier.isValid("3-598-21507-A"));
+        Assert.assertFalse(isbnVerifier.isValid("3-598-21507-A"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void invalidCharacterInIsbn() {
-        Assert.assertEquals(false, isbnVerifier.isValid("3-598-2K507-0"));
+        Assert.assertFalse(isbnVerifier.isValid("3-598-2K507-0"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void xIsOnlyValidAsACheckDigit() {
-        Assert.assertEquals(false, isbnVerifier.isValid("3-598-2X507-9"));
+        Assert.assertFalse(isbnVerifier.isValid("3-598-2X507-9"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void validIsbnWithoutSeparatingDashes() {
-        Assert.assertEquals(true, isbnVerifier.isValid("3598215088"));
+        Assert.assertTrue(isbnVerifier.isValid("3598215088"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void isbnWithoutSeparatingDashesAndXAsCheckDigit() {
-        Assert.assertEquals(true, isbnVerifier.isValid("359821507X"));
+        Assert.assertTrue(isbnVerifier.isValid("359821507X"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void isbnWithoutCheckDigitAndDashes() {
-        Assert.assertEquals(false, isbnVerifier.isValid("359821507"));
+        Assert.assertFalse(isbnVerifier.isValid("359821507"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void tooLongIsbnAndNoDashes() {
-        Assert.assertEquals(false, isbnVerifier.isValid("3598215078X"));
+        Assert.assertFalse(isbnVerifier.isValid("3598215078X"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void isbnWithoutCheckDigit() {
-        Assert.assertEquals(false, isbnVerifier.isValid("3-598-21507"));
+        Assert.assertFalse(isbnVerifier.isValid("3-598-21507"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void tooLongIsbn() {
-        Assert.assertEquals(false, isbnVerifier.isValid("3-598-21507-XX"));
+        Assert.assertFalse(isbnVerifier.isValid("3-598-21507-XX"));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void checkDigitOfXShouldNotBeUsedForZero() {
-        Assert.assertEquals(false, isbnVerifier.isValid("3-598-21515-X"));
+        Assert.assertFalse(isbnVerifier.isValid("3-598-21515-X"));
     }
 
 }
