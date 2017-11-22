8f56c75c44c348cfc5cea4cdb3e225629491d688
Sam Warner <sam.warner@blackpepper.co.uk>
2017-11-22 11:48:57 +0000

    kindergarten-garden: add constructors to starter

--- exercises/kindergarten-garden/src/main/java/KindergartenGarden.java
+++ exercises/kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,4 +1,14 @@
+import java.util.List;
+
 class KindergartenGarden {
+
+    KindergartenGarden(String garden, String[] students) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    KindergartenGarden(String garden) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 
     List<Plant> getPlantsOfStudent(String student) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
