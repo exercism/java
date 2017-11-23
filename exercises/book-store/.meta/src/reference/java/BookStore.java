a2d38901eee0ee4482fd69009e2d70d142cb7355
Sam Warner <30871823+sjwarner-bp@users.noreply.github.com>
2017-11-23 10:14:17 +0000

    book-store: change indent from 2 to 4 spaces (#1029)
    * book-store: change indent from 2 to 4 spaces
    
    * book-store: Update with @FridaTveit 's improvement

--- exercises/book-store/.meta/src/reference/java/BookStore.java
+++ exercises/book-store/.meta/src/reference/java/BookStore.java
@@ -4,47 +4,47 @@ import java.util.stream.Collectors;
 
 class BookStore {
 
-  private static final int BOOK_PRICE = 8, MAX_GROUP_SIZE = 5;
+    private static final int BOOK_PRICE = 8, MAX_GROUP_SIZE = 5;
 
-  private static double[] DISCOUNT_TIERS = {0, 5, 10, 20, 25};
+    private static double[] DISCOUNT_TIERS = {0, 5, 10, 20, 25};
 
-  double calculateBasketCost(final List<Integer> books) {
-    return calculateBasketCost(books, 0);
-  }
-
-  private double calculateBasketCost(final List<Integer> books, final double priceSoFar) {
-    if (books.size() == 0) {
-      return priceSoFar;
+    double calculateBasketCost(final List<Integer> books) {
+        return calculateBasketCost(books, 0);
     }
 
-    List<Integer> availableBookNumbers = books.stream()
-        .distinct()
-        .collect(Collectors.toList());
+    private double calculateBasketCost(final List<Integer> books, final double priceSoFar) {
+        if (books.size() == 0) {
+            return priceSoFar;
+        }
 
-    double minPrice = Double.MAX_VALUE;
+        List<Integer> availableBookNumbers = books.stream()
+            .distinct()
+            .collect(Collectors.toList());
 
-    for (int i = 0; i < availableBookNumbers.size(); i++) {
-      List<Integer> newGroupBooks = new ArrayList<>(availableBookNumbers.subList(0, i + 1));
-      List<Integer> remainingBooks = new ArrayList<>(books);
+        double minPrice = Double.MAX_VALUE;
 
-      for (final Integer newGroupBook : newGroupBooks) {
-        //noinspection UseBulkOperation - we want to remove _one_ of each book number, not _all_ of each book number.
-        remainingBooks.remove(newGroupBook);
-      }
+        for (int i = 0; i < availableBookNumbers.size(); i++) {
+            List<Integer> newGroupBooks = new ArrayList<>(availableBookNumbers.subList(0, i + 1));
+            List<Integer> remainingBooks = new ArrayList<>(books);
 
-      double price = calculateBasketCost(remainingBooks, priceSoFar + costOfGroupSize(newGroupBooks.size()));
-      minPrice = Math.min(minPrice, price);
+            for (final Integer newGroupBook : newGroupBooks) {
+                //noinspection UseBulkOperation - we want to remove _one_ of each book number, not _all_ of each book number.
+                remainingBooks.remove(newGroupBook);
+            }
+
+            double price = calculateBasketCost(remainingBooks, priceSoFar + costOfGroupSize(newGroupBooks.size()));
+            minPrice = Math.min(minPrice, price);
+        }
+
+        return minPrice;
     }
 
-    return minPrice;
-  }
+    private double costOfGroupSize(int groupSize) {
+        if (groupSize < 1 || groupSize > MAX_GROUP_SIZE) {
+            throw new IllegalStateException("Invalid group size : " + groupSize);
+        }
 
-  private double costOfGroupSize(int groupSize) {
-    if (groupSize < 1 || groupSize > MAX_GROUP_SIZE) {
-      throw new IllegalStateException("Invalid group size : " + groupSize);
+        return BOOK_PRICE * groupSize * (100 - DISCOUNT_TIERS[groupSize - 1]) / 100;
     }
-
-    return BOOK_PRICE * groupSize * (100 - DISCOUNT_TIERS[groupSize - 1]) / 100;
-  }
 
 }
