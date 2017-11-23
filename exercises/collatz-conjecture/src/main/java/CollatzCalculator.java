d58392dc9adb22f9486629d2498ed37294d002f0
Sam Warner <sam.warner@blackpepper.co.uk>
2017-11-23 08:14:15 +0000

    collatz-conjecture: update with @FridaTveit 's suggestion

--- exercises/collatz-conjecture/src/main/java/CollatzCalculator.java
+++ exercises/collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,6 +1,6 @@
 class CollatzCalculator {
 
-    int computeStepCount(final int start) {
+    int computeStepCount(int start) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
