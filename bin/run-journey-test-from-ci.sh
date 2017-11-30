f42a65852e198e91f10cd5d5606cc34c54fc90d9
Brandon Kiefer <3527363+bmkiefer@users.noreply.github.com>
2017-11-30 14:45:11 -0600

    revert b3220e5

--- bin/run-journey-test-from-ci.sh
+++ bin/run-journey-test-from-ci.sh
@@ -14,7 +14,7 @@ echo "Changes in pr json: ${pr_files_json}"
 modded_files=`echo $pr_files_json | bin/jq -r '.[].filename'` || bin/journey-test.sh
 
 for file in $modded_files
-  do if [[ ! $file == exercises/build.gradle ]] && ([[ $file == exercises* ]] || [[ $file == config.json ]])
+  do if [[ $file == exercises* ]] || [[ $file == config.json ]]
     then
     for file2 in $modded_files
       do if [[ $file2 == exercises* ]] && [[ $file2 != exercises/settings.gradle ]]
