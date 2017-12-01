ef231ad4756b5cebd41a4f221e76a4bf816a53db
Saatvik Arya <aryasaatvik@users.noreply.github.com>
2017-12-01 14:01:12 -0600

    linked-list: make classes and methods package private  (#1066)
    Fixes #1055 
    
    * make classes and methods package private #1055
    
    * changed nested class to public
    
    * made Element<T> private and its constructor package-private

--- exercises/linked-list/.meta/src/reference/java/DoublyLinkedList.java
+++ exercises/linked-list/.meta/src/reference/java/DoublyLinkedList.java
@@ -1,7 +1,7 @@
-public final class DoublyLinkedList<T> {
+final class DoublyLinkedList<T> {
     private Element<T> head;
 
-    public void push(T value) {
+    void push(T value) {
         if (head == null) {
             head = new Element<>(value, null, null);
             head.next = head;
@@ -15,17 +15,17 @@ public final class DoublyLinkedList<T> {
         head.prev = tail;
     }
 
-    public T pop() {
+    T pop() {
         head = head.prev;
         return shift();
     }
 
-    public void unshift(T value) {
+    void unshift(T value) {
         push(value);
         head = head.prev;
     }
 
-    public T shift() {
+    T shift() {
         T value = head.value;
 
         Element<T> newHead = head.next;
@@ -48,7 +48,7 @@ public final class DoublyLinkedList<T> {
         private Element<T> prev;
         private Element<T> next;
 
-        public Element(T value, Element<T> prev, Element<T> next) {
+        Element(T value, Element<T> prev, Element<T> next) {
             this.value = value;
             this.prev = prev;
             this.next = next;
