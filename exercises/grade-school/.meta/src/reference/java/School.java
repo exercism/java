b087e6263ac432ea7399548769116106d4f4430a
Saatvik Arya <aryasaatvik@gmail.com>
2017-11-30 21:42:18 -0600

    grade-school: make classes and methods package private in reference solution

--- exercises/grade-school/.meta/src/reference/java/School.java
+++ exercises/grade-school/.meta/src/reference/java/School.java
@@ -1,34 +1,34 @@
 import java.util.*;
 
-public class School {
+class School {
 
   private final Map<Integer, List<String>> database = new HashMap<>();
 
-  public int numberOfStudents() {
+  int numberOfStudents() {
     int result = 0;
-    for (List<String> studentsInGrade: database.values()) {
+    for (List<String> studentsInGrade : database.values()) {
       result += studentsInGrade.size();
     }
     return result;
   }
 
-  public void add(String student, int grade) {
+  void add(String student, int grade) {
     List<String> students = fetchGradeFromDatabase(grade);
     students.add(student);
   }
 
-  public List<String> grade(int grade) {
+  List<String> grade(int grade) {
     return new ArrayList<>(fetchGradeFromDatabase(grade));
   }
 
-  private List<String> fetchGradeFromDatabase(int grade) {
+  List<String> fetchGradeFromDatabase(int grade) {
     if (!database.containsKey(grade)) {
       database.put(grade, new LinkedList<>());
     }
     return database.get(grade);
   }
 
-  public Map<Integer, List<String>> studentsByGradeAlphabetical() {
+  Map<Integer, List<String>> studentsByGradeAlphabetical() {
     Map<Integer, List<String>> sortedStudents = new HashMap<>();
     for (Integer grade : database.keySet()) {
       List<String> studentsInGrade = database.get(grade);
