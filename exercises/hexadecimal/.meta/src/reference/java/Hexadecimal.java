8aea0a208df69fe365feeafd8c1228178b114553
Todd Mahoney <todd.developer@gmail.com>
2017-12-02 20:31:58 -0700

    removing public access modifiers from Hexadecimal.java

--- exercises/hexadecimal/.meta/src/reference/java/Hexadecimal.java
+++ exercises/hexadecimal/.meta/src/reference/java/Hexadecimal.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 
-public class Hexadecimal {
+class Hexadecimal {
     private static final String HEXES = "0123456789abcdef";
-    public static int toDecimal(String input){
+    static int toDecimal(String input){
         boolean  isHex = input.matches("^[0-9a-f]+");
         if (!isHex) return 0;
         return Arrays.stream(input.split(""))
@@ -11,5 +11,3 @@ public class Hexadecimal {
 
     }
 }
-
-
