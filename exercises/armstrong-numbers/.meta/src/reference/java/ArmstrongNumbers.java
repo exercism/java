79228f0221cd5780c68d3b078b55efc4051ad7ab
Sam Warner <sam.warner@blackpepper.co.uk>
2017-12-05 09:05:16 +0000

    armstrong-numbers: add tests and ref solution

---
+++ exercises/armstrong-numbers/.meta/src/reference/java/ArmstrongNumbers.java
@@ -0,0 +1,19 @@
+import static java.lang.Math.pow;
+
+class ArmstrongNumbers {
+
+	boolean isArmstrongNumber(int i) {
+		int number = i;
+		int calculation = 0;
+		int length = String.valueOf(number).length();
+
+		while (number > 0) {
+    		calculation += pow((number % 10), length);
+    		number = number / 10;
+		}
+
+		return number = calculation;
+
+	}
+
+}
