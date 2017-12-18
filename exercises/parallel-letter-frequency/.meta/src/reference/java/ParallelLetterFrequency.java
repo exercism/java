54050fd0ec747e05f4419cea9ebf7fbf6fcc27c2
Matthew Morgan <matt@dulcetsoftware.com>
2017-12-18 07:05:35 -0500

    Add exercise parallel-letter-frequency

---
+++ exercises/parallel-letter-frequency/.meta/src/reference/java/ParallelLetterFrequency.java
@@ -0,0 +1,45 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+class ParallelLetterFrequency {
+
+    private static Map<Integer, Integer> letterFrequencyMap;
+    private final String NOT_A_LETTER = "\\P{L}+";
+
+
+    ParallelLetterFrequency(String letters) {
+        letterFrequencyMap = getMapFromLetters(letters.toLowerCase().replaceAll(NOT_A_LETTER, ""));
+    }
+
+    Map<Integer, Integer> getMapFromLetters(String letters) {
+        return letters
+                .chars()
+                .parallel()
+                .collect(Counts::new, Counts::increment, Counts::combine)
+                .buildMap();
+    }
+
+    private static class Counts {
+
+        private Map<Integer, Integer> letterCounts = new HashMap<>();
+
+        private void increment(int letter) {
+            letterCounts.put(letter, letterCounts.getOrDefault(letter, 0) + 1);
+        }
+
+        private void combine(Counts other) {
+            other.letterCounts.keySet()
+                    .forEach(letter -> letterCounts.merge(letter, other.letterCounts.get(letter), Integer::sum));
+        }
+
+        private Map<Integer, Integer> buildMap() {
+            return Collections.unmodifiableMap(letterCounts);
+        }
+    }
+
+    Map<Integer, Integer> letterCounts() {
+        return letterFrequencyMap;
+    }
+}
---
+++ exercises/parallel-letter-frequency/README.md
@@ -0,0 +1,28 @@
+# Parallel Letter Frequency
+
+Count the frequency of letters in texts using parallel computation.
+
+Parallelism is about doing things in parallel that can also be done
+sequentially. A common example is counting the frequency of letters.
+Create a function that returns the total frequency of each letter in a
+list of texts and that employs parallelism.
+
+# Java Tips
+
+Single-threaded (non-concurrent) solutions can pass all tests [but the last.](https://www.youtube.com/watch?v=mJZZNHekEQw)  Your solution will be tested for concurrency by submitting it as a [Runnable](https://docs.oracle.com/javase/7/docs/api/java/lang/Runnable.html) to an [ExecutorService.](https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ExecutorService.html) Your solution must leverage multiple [Threads](https://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html) to pass the final test.
+
+Java documentation on [parallel streams](https://docs.oracle.com/javase/tutorial/collections/streams/parallelism.html) may provide some help.
+
+# Running the tests
+
+You can run all the tests for an exercise by entering
+
+```sh
+$ gradle test
+```
+
+in your terminal.
+
+## Submitting Incomplete Solutions
+
+It's possible to submit an incomplete solution so you can see how others have completed the exercise.
