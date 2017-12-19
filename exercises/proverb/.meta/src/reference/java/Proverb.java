9ad2354596ec040f2b4ae8ac5b248303fb6d6733
Ryan Workman <rdavid1099@gmail.com>
2017-12-19 10:32:30 -0700

    Refactor out extraneous if-statement

--- exercises/proverb/.meta/src/reference/java/Proverb.java
+++ exercises/proverb/.meta/src/reference/java/Proverb.java
@@ -9,10 +9,7 @@ final class Proverb {
         if (words.length < 1) return "";
         final StringBuilder result = new StringBuilder();
         for(int i = 1; i < words.length; i++) {
-            if (i != words.length) {
-                result.append("For want of a " + words[i - 1] + " ");
-            }
-            result.append("the " + words[i] + " was lost.\n");
+            result.append("For want of a " + words[i - 1] + " the " + words[i] + " was lost.\n");
         }
         result.append("And all for the want of a " + words[0] + ".");
         return result.toString();
