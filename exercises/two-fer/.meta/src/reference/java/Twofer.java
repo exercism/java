15c2cb92f278668f3b6694b177238e689139bd97
Ryan Workman <rdavid1099@gmail.com>
2017-12-26 12:07:30 -0700

    Changed classes and methods to be package private

--- exercises/two-fer/.meta/src/reference/java/Twofer.java
+++ exercises/two-fer/.meta/src/reference/java/Twofer.java
@@ -1,5 +1,5 @@
-public class Twofer {
-    public String twofer(String name) {
+class Twofer {
+    String twofer(String name) {
         return "One for " + (name != null ? name : "you") + ", one for me.";
     }
 }
