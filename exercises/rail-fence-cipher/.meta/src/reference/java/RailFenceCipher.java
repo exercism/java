9917119bafd72425b70550bd0bc2d3fd37cc76aa
Sam Warner <sam.warner@blackpepper.co.uk>
2017-12-26 14:45:20 +0000

    rail-fence-cipher: update with @FridaTveit's suggestions.

--- exercises/rail-fence-cipher/.meta/src/reference/java/RailFenceCipher.java
+++ exercises/rail-fence-cipher/.meta/src/reference/java/RailFenceCipher.java
@@ -1,21 +1,23 @@
-public class RailFenceCipher {
+import java.util.Arrays;
 
-    private static int key;
+class RailFenceCipher {
 
-    public RailFenceCipher(int key) {
-        RailFenceCipher.key = key;
+    private int key;
+
+    RailFenceCipher(int key) {
+        this.key = key;
     }
 
-    public static String getEncryptedData(String message) {
+    String getEncryptedData(String message) {
         String[] lines = splitIntoLines(message, false);
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (String line : lines) {
-            result += line;
+            result.append(line);
         }
-        return result;
+        return result.toString();
     }
 
-    public static String getDecryptedData(String message) {
+    String getDecryptedData(String message) {
         String[] lines = splitIntoLines(message, true);
 
         int charCount = 0;
@@ -27,20 +29,20 @@ public class RailFenceCipher {
             }
         }
 
-        String result = "";
+        StringBuilder result = new StringBuilder();
         int lineCount = 0;
         int direction = -1;
         for (int i = 0; i < message.length(); ++i) {
             String letter = String.valueOf(lines[lineCount].charAt(0));
             lines[lineCount] = lines[lineCount].substring(1);
-            result += letter;
+            result.append(letter);
             direction *= lineCount == 0 || lineCount == key - 1 ? -1 : 1;
             lineCount += direction;
         }
-        return result;
+        return result.toString();
     }
 
-    private static String[] splitIntoLines(String message, boolean encrypted) {
+    private String[] splitIntoLines(String message, boolean encrypted) {
         String[] result = generateEmptyStrings(key);
         int lineCount = 0;
         int direction = -1;
@@ -53,11 +55,9 @@ public class RailFenceCipher {
         return result;
     }
 
-    private static String[] generateEmptyStrings(int num) {
+    private String[] generateEmptyStrings(int num) {
         String[] strings = new String[num];
-        for (int i = 0; i < num; ++i) {
-            strings[i] = "";
-        }
+        Arrays.fill(strings, "");
         return strings;
     }
 
