f674c63b6a58f5d37a5f64a21f68a4cdf287c1cd
arica chakraborty <aricachakraborty18@gmail.com>
2017-12-26 22:35:16 +0530

    lamda-method-added

--- exercises/accumulate/.meta/src/reference/java/Accumulate.java
+++ exercises/accumulate/.meta/src/reference/java/Accumulate.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.lang;
 
 class Accumulate {
 
 	static <T> List<T> accumulate(List<T> collection, Function<T, T> function) {
 		List<T> newCollection = new ArrayList<>();
+
+        add= n-> Math.pow(n,2);//method added
 
 		for (T item : collection) {
 			newCollection.add(function.apply(item));
