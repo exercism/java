9295255cd138615e0ec42a2eeea72334627cb8d1
Samuel Warner <sam.warner@blackpepper.co.uk>
2018-01-02 21:08:14 +0000

    pascals-triangle: update tests to 1.2.0

--- exercises/pascals-triangle/src/test/java/PascalsTriangleGeneratorTest.java
+++ exercises/pascals-triangle/src/test/java/PascalsTriangleGeneratorTest.java
@@ -75,6 +75,54 @@ public class PascalsTriangleGeneratorTest {
 
     @Ignore("Remove to run test")
     @Test
+    public void testTriangleWithFiveRows() {
+        int[][] expectedOutput = new int[][]{
+            {1},
+            {1, 1},
+            {1, 2, 1},
+            {1, 3, 3, 1},
+            {1, 4, 6, 4, 1}
+        };
+
+        assertArrayEquals(expectedOutput, pascalsTriangleGenerator.generateTriangle(5));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testTriangleWithSixRows() {
+        int[][] expectedOutput = new int[][]{
+            {1},
+            {1, 1},
+            {1, 2, 1},
+            {1, 3, 3, 1},
+            {1, 4, 6, 4, 1},
+            {1, 5, 10, 10, 5, 1}
+        };
+
+        assertArrayEquals(expectedOutput, pascalsTriangleGenerator.generateTriangle(6));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testTriangleWithTenRows() {
+        int[][] expectedOutput = new int[][]{
+            {1},
+            {1, 1},
+            {1, 2, 1},
+            {1, 3, 3, 1},
+            {1, 4, 6, 4, 1},
+            {1, 5, 10, 10, 5, 1},
+            {1, 6, 15, 20, 15, 6, 1},
+            {1, 7, 21, 35, 35, 21, 7, 1},
+            {1, 8, 28, 56, 70, 56, 28, 8, 1},
+            {1, 9, 36, 84, 126, 126, 84, 36, 9, 1}
+        };
+
+        assertArrayEquals(expectedOutput, pascalsTriangleGenerator.generateTriangle(10));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
     public void testValidatesNotNegativeRows() {
         expectedException.expect(IllegalArgumentException.class);
         pascalsTriangleGenerator.generateTriangle(-1);
