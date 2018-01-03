d7f1a668bb0caf40718e62c06fe938518d6b5748
FridaTveit <FridaTveit@users.noreply.github.com>
2018-01-03 18:01:21 +0000

    crypto-square: update to match canonical data (#1178)
    * crypto-square: update to match canonical data
    
    * crypto-square: rename crypto to cryptoSquare in tests

--- exercises/crypto-square/src/test/java/CryptoSquareTest.java
+++ exercises/crypto-square/src/test/java/CryptoSquareTest.java
@@ -8,138 +8,65 @@ import static org.junit.Assert.assertEquals;
 
 public class CryptoSquareTest {
 
-
     @Test
-    public void strangeCharactersAreStrippedDuringNormalization() {
-        Crypto crypto = new Crypto("s#$%^&plunk");
-        String expectedOutput = "splunk";
+    public void emptyPlaintextResultsInEmptyCiphertext() {
+        CryptoSquare cryptoSquare = new CryptoSquare("");
+        String expectedOutput = "";
 
-        assertEquals(expectedOutput, crypto.getNormalizedPlaintext());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void lettersAreLowerCasedDuringNormalization() {
-        Crypto crypto = new Crypto("WHOA HEY!");
-        String expectedOutput = "whoahey";
+    public void lettersAreLowerCasedDuringEncryption() {
+        CryptoSquare cryptoSquare = new CryptoSquare("A");
+        String expectedOutput = "a";
 
-        assertEquals(expectedOutput, crypto.getNormalizedPlaintext());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void numbersAreKeptDuringNormalization() {
-        Crypto crypto = new Crypto("1, 2, 3, GO!");
-        String expectedOutput = "123go";
+    public void spacesAreRemovedDuringEncryption() {
+        CryptoSquare cryptoSquare = new CryptoSquare(" b ");
+        String expectedOutput = "b";
 
-        assertEquals(expectedOutput, crypto.getNormalizedPlaintext());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void smallestSquareSizeIs2() {
-        Crypto crypto = new Crypto("1234");
-        int expectedOutput = 2;
+    public void punctuationIsRemovedDuringEncryption() {
+        CryptoSquare cryptoSquare = new CryptoSquare("@1,%!");
+        String expectedOutput = "1";
 
-        assertEquals(expectedOutput, crypto.getSquareSize());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void sizeOfTextWhoseLengthIsPerfectSquareIsItsSquareRoot() {
-        Crypto crypto = new Crypto("123456789");
-        int expectedOutput = 3;
+    public void nineCharacterPlaintextResultsInThreeChunksOfThreeCharacters() {
+        CryptoSquare cryptoSquare = new CryptoSquare("This is fun!");
+        String expectedOutput = "tsf hiu isn";
 
-        assertEquals(expectedOutput, crypto.getSquareSize());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void sizeOfTextWhoseLengthIsNoPerfectSquareIsNextBiggestSquareRoot() {
-        Crypto crypto = new Crypto("123456789abc");
-        int expectedOutput = 4;
+    public void eightCharacterPlaintextResultsInThreeChunksWithATrailingSpace() {
+        CryptoSquare cryptoSquare = new CryptoSquare("Chill out.");
+        String expectedOutput = "clu hlt io ";
 
-        assertEquals(expectedOutput, crypto.getSquareSize());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void sizeIsDeterminedByNormalizedText() {
-        Crypto crypto = new Crypto("Oh hey, this is nuts!");
-        int expectedOutput = 4;
+    public void fiftyFourCharacterPlaintextResultsInSevenChunksWithTrailingSpaces() {
+        CryptoSquare cryptoSquare = new CryptoSquare("If man was meant to stay on the ground, god would have given us roots.");
+        String expectedOutput = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau ";
 
-        assertEquals(expectedOutput, crypto.getSquareSize());
-    }
-
-    @Ignore("Remove to run test")
-    @Test
-    public void segmentsAreSplitBySquareSize() {
-        Crypto crypto = new Crypto("Never vex thine heart with idle woes");
-        List<String> expectedOutput = Arrays.asList(new String[]{"neverv", "exthin", "eheart", "withid", "lewoes"});
-
-        assertEquals(expectedOutput, crypto.getPlaintextSegments());
-    }
-
-    @Ignore("Remove to run test")
-    @Test
-    public void segmentsAreSplitBySquareSizeUntilTextRunsOut() {
-        Crypto crypto = new Crypto("ZOMG! ZOMBIES!!!");
-        List<String> expectedOutput = Arrays.asList(new String[]{"zomg", "zomb", "ies"});
-
-        assertEquals(expectedOutput, crypto.getPlaintextSegments());
-    }
-
-    @Ignore("Remove to run test")
-    @Test
-    public void cipherTextCombinesTextByColumn() {
-        Crypto crypto = new Crypto("First, solve the problem. Then, write the code.");
-        String expectedOutput = "foeewhilpmrervrticseohtottbeedshlnte";
-
-        assertEquals(expectedOutput, crypto.getCipherText());
-    }
-
-    @Ignore("Remove to run test")
-    @Test
-    public void cipherTextSkipsCellsWithNoText() {
-        Crypto crypto = new Crypto("Time is an illusion. Lunchtime doubly so.");
-        String expectedOutput = "tasneyinicdsmiohooelntuillibsuuml";
-
-        assertEquals(expectedOutput, crypto.getCipherText());
-    }
-
-    @Ignore("Remove to run test")
-    @Test
-    public void normalizedCipherTextIsSplitByHeightOfSquare() {
-        Crypto crypto = new Crypto("Vampires are people too!");
-        String expectedOutput = "vrel aepe mset paoo irpo";
-
-        assertEquals(expectedOutput, crypto.getNormalizedCipherText());
-    }
-
-    @Ignore("Remove to run test")
-    @Test
-    public void normalizedCipherNotExactlyDivisibleBy5SpillsIntoSmallerSegment() {
-        Crypto crypto = new Crypto("Madness, and then illumination.");
-        String expectedOutput = "msemo aanin dnin ndla etlt shui";
-
-        assertEquals(expectedOutput, crypto.getNormalizedCipherText());
-    }
-
-    @Ignore("Remove to run test")
-    @Test
-    public void normalizedCipherIsSplitIntoSegmentsOfCorrectSize() {
-        Crypto crypto = new Crypto("If man was meant to stay on the ground god would have given us roots");
-        String expectedOutput = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau";
-
-        assertEquals(expectedOutput, crypto.getNormalizedCipherText());
-    }
-
-    @Ignore("Remove to run test")
-    @Test
-    public void normalizedCipherTextIsSplitIntoSegmentsOfCorrectSizeWithPunctuation() {
-        Crypto crypto = new Crypto("Have a nice day. Feed the dog & chill out!");
-        String expectedOutput = "hifei acedl veeol eddgo aatcu nyhht";
-
-        assertEquals(expectedOutput, crypto.getNormalizedCipherText());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 }
