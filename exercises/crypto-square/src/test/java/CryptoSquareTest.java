9605006d86218eea0fa8a8bd0c8a6f08d0497140
Frida Tveit <Frida.Tveit@softwire.com>
2018-01-03 16:17:00 +0000

    crypto-square: rename crypto to cryptoSquare in tests

--- exercises/crypto-square/src/test/java/CryptoSquareTest.java
+++ exercises/crypto-square/src/test/java/CryptoSquareTest.java
@@ -10,63 +10,63 @@ public class CryptoSquareTest {
 
     @Test
     public void emptyPlaintextResultsInEmptyCiphertext() {
-        Crypto crypto = new Crypto("");
+        CryptoSquare cryptoSquare = new CryptoSquare("");
         String expectedOutput = "";
 
-        assertEquals(expectedOutput, crypto.getCiphertext());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
     public void lettersAreLowerCasedDuringEncryption() {
-        Crypto crypto = new Crypto("A");
+        CryptoSquare cryptoSquare = new CryptoSquare("A");
         String expectedOutput = "a";
 
-        assertEquals(expectedOutput, crypto.getCiphertext());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
     public void spacesAreRemovedDuringEncryption() {
-        Crypto crypto = new Crypto(" b ");
+        CryptoSquare cryptoSquare = new CryptoSquare(" b ");
         String expectedOutput = "b";
 
-        assertEquals(expectedOutput, crypto.getCiphertext());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
     public void punctuationIsRemovedDuringEncryption() {
-        Crypto crypto = new Crypto("@1,%!");
+        CryptoSquare cryptoSquare = new CryptoSquare("@1,%!");
         String expectedOutput = "1";
 
-        assertEquals(expectedOutput, crypto.getCiphertext());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
     public void nineCharacterPlaintextResultsInThreeChunksOfThreeCharacters() {
-        Crypto crypto = new Crypto("This is fun!");
+        CryptoSquare cryptoSquare = new CryptoSquare("This is fun!");
         String expectedOutput = "tsf hiu isn";
 
-        assertEquals(expectedOutput, crypto.getCiphertext());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
     public void eightCharacterPlaintextResultsInThreeChunksWithATrailingSpace() {
-        Crypto crypto = new Crypto("Chill out.");
+        CryptoSquare cryptoSquare = new CryptoSquare("Chill out.");
         String expectedOutput = "clu hlt io ";
 
-        assertEquals(expectedOutput, crypto.getCiphertext());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 
     @Ignore("Remove to run test")
     @Test
     public void fiftyFourCharacterPlaintextResultsInSevenChunksWithTrailingSpaces() {
-        Crypto crypto = new Crypto("If man was meant to stay on the ground, god would have given us roots.");
+        CryptoSquare cryptoSquare = new CryptoSquare("If man was meant to stay on the ground, god would have given us roots.");
         String expectedOutput = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau ";
 
-        assertEquals(expectedOutput, crypto.getCiphertext());
+        assertEquals(expectedOutput, cryptoSquare.getCiphertext());
     }
 }
