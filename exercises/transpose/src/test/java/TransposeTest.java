8f38615449ebdc7007f08458e93c7181b5cc3a81
Sam Warner <30871823+sjwarner-bp@users.noreply.github.com>
2018-01-06 19:39:12 +0000

    transpose: update test suite to 1.1.0 (#1200)
    * transpose: update test suite to 1.1.0
    
    * transpose: remove final test

--- exercises/transpose/src/test/java/TransposeTest.java
+++ exercises/transpose/src/test/java/TransposeTest.java
@@ -131,6 +131,35 @@ public class TransposeTest {
 
     @Ignore("Remove to run test")
     @Test
+    public void mixedLineLength() {
+        String input = "The longest line.\n" +
+                       "A long line.\n" +
+                       "A longer line.\n" +
+                       "A line.";
+
+        String expected = "TAAA\n" +
+                          "h   \n" +
+                          "elll\n" +
+                          " ooi\n" +
+                          "lnnn\n" +
+                          "ogge\n" +
+                          "n e.\n" +
+                          "glr\n" +
+                          "ei \n" +
+                          "snl\n" +
+                          "tei\n" +
+                          " .n\n" +
+                          "l e\n" +
+                          "i .\n" +
+                          "n\n" +
+                          "e\n" +
+                          ".";
+
+        assertEquals(expected, transpose.transpose(input));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
     public void square() {
         String input = "HEART\n" +
                        "EMBER\n" +
@@ -187,78 +216,4 @@ public class TransposeTest {
         assertEquals(expected, transpose.transpose(input));
     }
 
-    @Ignore("Remove to run test")
-    @Test
-    public void manyLines() {
-        String input = "Chor. Two households, both alike in dignity,\n" +
-                       "In fair Verona, where we lay our scene,\n" +
-                       "From ancient grudge break to new mutiny,\n" +
-                       "Where civil blood makes civil hands unclean.\n" +
-                       "From forth the fatal loins of these two foes\n" +
-                       "A pair of star-cross'd lovers take their life;\n" +
-                       "Whose misadventur'd piteous overthrows\n" +
-                       "Doth with their death bury their parents' strife.\n" +
-                       "The fearful passage of their death-mark'd love,\n" +
-                       "And the continuance of their parents' rage,\n" +
-                       "Which, but their children's end, naught could remove,\n" +
-                       "Is now the two hours' traffic of our stage;\n" +
-                       "The which if you with patient ears attend,\n" +
-                       "What here shall miss, our toil shall strive to mend.";
-
-        String expected = "CIFWFAWDTAWITW\n" +
-                          "hnrhr hohnhshh\n" +
-                          "o oeopotedi ea\n" +
-                          "rfmrmash  cn t\n" +
-                          ".a e ie fthow \n" +
-                          " ia fr weh,whh\n" +
-                          "Trnco miae  ie\n" +
-                          "w ciroitr btcr\n" +
-                          "oVivtfshfcuhhe\n" +
-                          " eeih a uote  \n" +
-                          "hrnl sdtln  is\n" +
-                          "oot ttvh tttfh\n" +
-                          "un bhaeepihw a\n" +
-                          "saglernianeoyl\n" +
-                          "e,ro -trsui ol\n" +
-                          "h uofcu sarhu \n" +
-                          "owddarrdan o m\n" +
-                          "lhg to'egccuwi\n" +
-                          "deemasdaeehris\n" +
-                          "sr als t  ists\n" +
-                          ",ebk 'phool'h,\n" +
-                          "  reldi ffd   \n" +
-                          "bweso tb  rtpo\n" +
-                          "oea ileutterau\n" +
-                          "t kcnoorhhnatr\n" +
-                          "hl isvuyee'fi \n" +
-                          " atv es iisfet\n" +
-                          "ayoior trr ino\n" +
-                          "l  lfsoh  ecti\n" +
-                          "ion   vedpn  l\n" +
-                          "kuehtteieadoe \n" +
-                          "erwaharrar,fas\n" +
-                          "   nekt te  rh\n" +
-                          "ismdsehphnnosa\n" +
-                          "ncuse ra-tau l\n" +
-                          " et  tormsural\n" +
-                          "dniuthwea'g t \n" +
-                          "iennwesnr hsts\n" +
-                          "g,ycoi tkrttet\n" +
-                          "n ,l r s'a anr\n" +
-                          "i  ef  'dgcgdi\n" +
-                          "t  aol   eoe,v\n" +
-                          "y  nei sl,u; e\n" +
-                          ",  .sf to l   \n" +
-                          "     e rv d  t\n" +
-                          "     ; ie    o\n" +
-                          "       f, r   \n" +
-                          "       e  e  m\n" +
-                          "       .  m  e\n" +
-                          "          o  n\n" +
-                          "          v  d\n" +
-                          "          e  .\n" +
-                          "          ,";
-
-        assertEquals(expected, transpose.transpose(input));
-    }
 }
