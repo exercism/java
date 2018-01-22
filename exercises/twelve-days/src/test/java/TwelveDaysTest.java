689ee88e55578ff4af0c8cebe23e1f4e9c224e6d
Kyle P <31393170+Kyle-P@users.noreply.github.com>
2018-01-21 16:40:13 -0800

    Update TwelveDaysTest.java
    Fixed error

--- exercises/twelve-days/src/test/java/TwelveDaysTest.java
+++ exercises/twelve-days/src/test/java/TwelveDaysTest.java
@@ -137,7 +137,7 @@ public class TwelveDaysTest {
                 "On the fifth day of Christmas my true love gave to me, five Gold Rings, four Calling Birds, " + 
                 "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" +
                 "On the sixth day of Christmas my true love gave to me, six Geese-a-Laying, five Gold Rings, " + 
-                "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+                "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
         assertEquals(expectedVersesFourToSix, twelveDays.verses(4, 6));
     }
 
