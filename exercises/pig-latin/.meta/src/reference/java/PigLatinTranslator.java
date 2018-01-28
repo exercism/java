f44421e96a96a969c802df17579b6cdb03de87c3
Kyle P <31393170+Kyle-P@users.noreply.github.com>
2018-01-28 10:19:23 -0800

    Update PigLatinTranslator.java
    Fixed reference solution so words with "y" after a consonant cluster are taken care of properly.

--- exercises/pig-latin/.meta/src/reference/java/PigLatinTranslator.java
+++ exercises/pig-latin/.meta/src/reference/java/PigLatinTranslator.java
@@ -21,9 +21,15 @@ class PigLatinTranslator {
                 .collect(Collectors.joining(" "));
     }
 
+    private static int yLocation;
+
     private String translateWord(String word) {
         if (wordStartsWithVowelLike(word)) {
             return word + AY;
+        }
+
+        if(hasYAfterConsonantCluster(word)){
+            return word.substring(yLocation) + word.substring(0, yLocation) + AY;
         }
 
         if (wordStartsWithPrefixes(word, THR, SCH)) {
@@ -39,6 +45,18 @@ class PigLatinTranslator {
         }
 
         return word.substring(1) + word.toCharArray()[0] + AY;
+    }
+
+    private static boolean hasYAfterConsonantCluster(String word) {
+        for(int i = 0; i < word.length() - 1; i++){
+            if(word.substring(i, i + 1).matches(VOWELS_REGEX)){
+                return false;
+            }else if((i != 0) && word.substring(i, i + 1).equals("y")){
+            	yLocation = i;
+                return true;
+            }
+        }
+      return false;
     }
 
     private boolean wordStartsWithVowelLike(String word) {
