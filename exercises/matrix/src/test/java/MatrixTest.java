98b08b7884bde750f600b3be65d15c918834404f
Jeffrey Sander <jeffreysander@gmail.com>
2018-02-09 12:02:21 -0500

    updated matrix

--- exercises/matrix/src/test/java/MatrixTest.java
+++ exercises/matrix/src/test/java/MatrixTest.java
@@ -13,97 +13,97 @@ import static org.junit.Assert.assertEquals;
 public class MatrixTest {
 	
 	@Test
-	public void ExtractRowFromOneNumberMatrixTest() {
+	public void extractRowFromOneNumberMatrixTest() {
 		String matrixAsString = "1";
 		int rowIndex = 0;
-		int[] row = {1};
+		int[] expectedRow = {1};
 		
 		Matrix matrix = new Matrix(matrixAsString);
 		
-		assertArrayEquals(row, matrix.getRow(rowIndex));
+		assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
 	}
 	
 	@Ignore("Remove to run test")
 	@Test
-	public void ExtractRowFromMatrixTest() {
+	public void extractRowFromMatrixTest() {
 		String matrixAsString = "1 2\n3 4";
 		int rowIndex = 1;
-		int[] row = {3, 4};
+		int[] expectedRow = {3, 4};
 		
 		Matrix matrix = new Matrix(matrixAsString);
 		
-		assertArrayEquals(row, matrix.getRow(rowIndex));
+		assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
 	}
 	
 	@Ignore("Remove to run test")
 	@Test
-	public void ExtractRowFromDiffWidthsMatrixTest() {
+	public void extractRowFromDiffWidthsMatrixTest() {
 		String matrixAsString = "1 2\n10 20";
 		int rowIndex = 1;
-		int[] row = {10, 20};
+		int[] expectedRow = {10, 20};
 		
 		Matrix matrix = new Matrix(matrixAsString);
 		
-		assertArrayEquals(row, matrix.getRow(rowIndex));
+		assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
 	}
 	
 	@Ignore("Remove to run test")
 	@Test
-	public void ExtractRowFromNonSquareMatrixTest() {
+	public void extractRowFromNonSquareMatrixTest() {
 		String matrixAsString = "1 2 3\n4 5 6\n7 8 9\n8 7 6";
 		int rowIndex = 2;
-		int[] row = {7, 8, 9};
+		int[] expectedRow = {7, 8, 9};
 		
 		Matrix matrix = new Matrix(matrixAsString);
 		
-		assertArrayEquals(row, matrix.getRow(rowIndex));
+		assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
 	}
 	
 	@Ignore("Remove to run test")
 	@Test
-	public void ExtractColumnFromOneNumberMatrixTest() {
+	public void extractColumnFromOneNumberMatrixTest() {
 		String matrixAsString = "1";
 		int columnIndex = 0;
-		int[] column = {1};
+		int[] expectedColumn = {1};
 		
 		Matrix matrix = new Matrix(matrixAsString);
 		
-		assertArrayEquals(column, matrix.getColumn(columnIndex));
+		assertArrayEquals(expectedColumn, matrix.getColumn(columnIndex));
 	}
 	
 	@Ignore("Remove to run test")
 	@Test
-	public void ExtractColumnMatrixTest() {
+	public void extractColumnMatrixTest() {
 		String matrixAsString = "1 2 3\n4 5 6\n7 8 9";
 		int columnIndex = 2;
-		int[] column = {3, 6, 9};
+		int[] expectedColumn = {3, 6, 9};
 		
 		Matrix matrix = new Matrix(matrixAsString);
 		
-		assertArrayEquals(column, matrix.getColumn(columnIndex));
+		assertArrayEquals(expectedColumn, matrix.getColumn(columnIndex));
 	}
 	
 	@Ignore("Remove to run test")
 	@Test
-	public void ExtractColumnFromNonSquareMatrixTest() {
+	public void extractColumnFromNonSquareMatrixTest() {
 		String matrixAsString = "1 2 3\n4 5 6\n7 8 9\n8 7 6";
 		int columnIndex = 2;
-		int[] column = {3, 6, 9, 6};
+		int[] expectedColumn = {3, 6, 9, 6};
 		
 		Matrix matrix = new Matrix(matrixAsString);
 		
-		assertArrayEquals(column, matrix.getColumn(columnIndex));
+		assertArrayEquals(expectedColumn, matrix.getColumn(columnIndex));
 	}
 	
 	@Ignore("Remove to run test")
 	@Test
-	public void ExtractColumnFromDiffWidthsMatrixTest() {
+	public void extractColumnFromDiffWidthsMatrixTest() {
 		String matrixAsString = "89 1903 3\n18 3 1\n9 4 800";
 		int columnIndex = 1;
-		int[] column = {1903, 3, 4};
+		int[] expectedColumn = {1903, 3, 4};
 		
 		Matrix matrix = new Matrix(matrixAsString);
 		
-		assertArrayEquals(column, matrix.getColumn(columnIndex));
+		assertArrayEquals(expectedColumn, matrix.getColumn(columnIndex));
 	}
 }
