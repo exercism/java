1d02221a1239087147ed18082cae7356e2a223dd
Ahchuang <E0036072@u.nus.edu>
2018-02-12 21:46:21 +0800

    Update tests

--- exercises/atbash-cipher/src/test/java/AtbashTest.java
+++ exercises/atbash-cipher/src/test/java/AtbashTest.java
@@ -14,44 +14,74 @@ public class AtbashTest {
     }
 
     @Test
-    public void testEncodingSingleWord() {
+    public void testEncodeYes() {
         assertEquals("bvh", atbash.encode("yes"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testEncodeNo() {
         assertEquals("ml", atbash.encode("no"));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void testEncodingWordsWithCapitalCases() {
+    public void testEncodeOmgInCapital() {
         assertEquals("lnt", atbash.encode("OMG"));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void testEncodingWordsWithSpace() {
+    public void testEncodeOmgWithSpaces() {
         assertEquals("lnt", atbash.encode("O M G"));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void testEncodingWordsLeadingToCodeWithSpace() {
+    public void testEncodeMindBlowingly() {
         assertEquals("nrmwy oldrm tob", atbash.encode("mindblowingly"));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void testEncodingSentences() {
+    public void testEncodeNumbers() {
         assertEquals("gvhgr mt123 gvhgr mt", atbash.encode("Testing,1 2 3, testing."));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testEncodeDeepThought() {
         assertEquals("gifgs rhurx grlm", atbash.encode("Truth is fiction."));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testEncodeAllTheLetters() {
         assertEquals("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt",
                 atbash.encode("The quick brown fox jumps over the lazy dog."));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void testDecoding() {
+    public void testDecodeExercism() {
         assertEquals("exercism", atbash.decode("vcvix rhn"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testDecodeASentence() {
         assertEquals("anobstacleisoftenasteppingstone", atbash.decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testDecodeNumbers() {
         assertEquals("testing123testing", atbash.decode("gvhgr mt123 gvhgr mt"));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testDecodeAllTheLetters() {
         assertEquals("thequickbrownfoxjumpsoverthelazydog",
                 atbash.decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"));
     }
