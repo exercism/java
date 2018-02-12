c5f7350951bf63e45d8f6ed4091328371be8c2cb
Lu Lechuan <E0036072@u.nus.edu>
2018-02-12 21:18:58 +0800

    Update PrimeFactorsCalculatorTest (#1308)
    * Update PrimeFactorsCalculatorTest
    
    * Separate tests individually
    
    * update tests

--- exercises/prime-factors/src/test/java/PrimeFactorsCalculatorTest.java
+++ exercises/prime-factors/src/test/java/PrimeFactorsCalculatorTest.java
@@ -1,42 +1,60 @@
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class PrimeFactorsCalculatorTest {
 
-    private long input;
-    private List<Long> expectedOutput;
+    private PrimeFactorsCalculator primeFactorsCalculator;
 
-    @Parameters(name="Prime factors of {0} = {1}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {1L, Collections.emptyList()},
-                {2L, Collections.singletonList(2L)},
-                {9L, Arrays.asList(3L, 3L)},
-                {8L, Arrays.asList(2L, 2L, 2L)},
-                {12L, Arrays.asList(2L, 2L, 3L)},
-                {901255L, Arrays.asList(5L, 17L, 23L, 461L)},
-                {93819012551L, Arrays.asList(11L, 9539L, 894119L)}
-        });
-    }
-
-    public PrimeFactorsCalculatorTest(long input, List<Long> expectedOutput) {
-        this.input = input;
-        this.expectedOutput = expectedOutput;
+    @Before
+    public void setUp() {
+        primeFactorsCalculator = new PrimeFactorsCalculator();
     }
 
     @Test
-    public void test() {
-        assertEquals(expectedOutput, new PrimeFactorsCalculator().calculatePrimeFactorsOf(input));
+    public void testNoFactors() {
+        assertEquals(Collections.emptyList(), primeFactorsCalculator.calculatePrimeFactorsOf(1L));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testPrimeNumber() {
+        assertEquals(Collections.singletonList(2L), primeFactorsCalculator.calculatePrimeFactorsOf(2L));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testSquareOfAPrime() {
+        assertEquals(Arrays.asList(3L, 3L), primeFactorsCalculator.calculatePrimeFactorsOf(9L));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testCubeOfAPrime() {
+        assertEquals(Arrays.asList(2L, 2L, 2L), primeFactorsCalculator.calculatePrimeFactorsOf(8L));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testProductOfPrimesAndNonPrimes() {
+        assertEquals(Arrays.asList(2L, 2L, 3L), primeFactorsCalculator.calculatePrimeFactorsOf(12L));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testProductOfPrimes() {
+        assertEquals(Arrays.asList(5L, 17L, 23L, 461L), primeFactorsCalculator.calculatePrimeFactorsOf(901255L));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testFactorsIncludingALargePrime() {
+        assertEquals(Arrays.asList(11L, 9539L, 894119L), primeFactorsCalculator.calculatePrimeFactorsOf(93819012551L));
     }
 
 }
