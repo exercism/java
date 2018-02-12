9ff085871ed4315a9030fb292bc81d92381480a4
Ahchuang <E0036072@u.nus.edu>
2018-02-12 21:13:41 +0800

    update tests

--- exercises/prime-factors/src/test/java/PrimeFactorsCalculatorTest.java
+++ exercises/prime-factors/src/test/java/PrimeFactorsCalculatorTest.java
@@ -17,43 +17,43 @@ public class PrimeFactorsCalculatorTest {
     }
 
     @Test
-    public void testEmptyListResult() {
+    public void testNoFactors() {
         assertEquals(Collections.emptyList(), primeFactorsCalculator.calculatePrimeFactorsOf(1L));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void testSingleTonListResult() {
+    public void testPrimeNumber() {
         assertEquals(Collections.singletonList(2L), primeFactorsCalculator.calculatePrimeFactorsOf(2L));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void testSquareNumberPrimeFactorResult() {
+    public void testSquareOfAPrime() {
         assertEquals(Arrays.asList(3L, 3L), primeFactorsCalculator.calculatePrimeFactorsOf(9L));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void test8LPrimeFactorResult() {
+    public void testCubeOfAPrime() {
         assertEquals(Arrays.asList(2L, 2L, 2L), primeFactorsCalculator.calculatePrimeFactorsOf(8L));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void test12LPrimeFactorResult() {
+    public void testProductOfPrimesAndNonPrimes() {
         assertEquals(Arrays.asList(2L, 2L, 3L), primeFactorsCalculator.calculatePrimeFactorsOf(12L));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void testMedianLargeNumberPrimeFactorResult() {
+    public void testProductOfPrimes() {
         assertEquals(Arrays.asList(5L, 17L, 23L, 461L), primeFactorsCalculator.calculatePrimeFactorsOf(901255L));
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void testLargeNumberPrimeFactorResult() {
+    public void testFactorsIncludingALargePrime() {
         assertEquals(Arrays.asList(11L, 9539L, 894119L), primeFactorsCalculator.calculatePrimeFactorsOf(93819012551L));
     }
 
