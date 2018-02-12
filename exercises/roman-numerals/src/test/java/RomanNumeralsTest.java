0bccd33a6ab809ca5b5b7b3d23a915528ab47a31
Ahchuang <E0036072@u.nus.edu>
2018-02-12 11:30:57 +0800

    Update RomanNumberalsTest

--- exercises/roman-numerals/src/test/java/RomanNumeralsTest.java
+++ exercises/roman-numerals/src/test/java/RomanNumeralsTest.java
@@ -1,54 +1,79 @@
 import org.junit.Test;
 import org.junit.Ignore;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class RomanNumeralsTest {
 
-    private int arabicNumeral;
-    private String romanNumeral;
-
-    @Parameters(name = "{index}: expected arabic numeral {0} to be converted to roman numeral \"{1}\".")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {1, "I"},
-                {2, "II"},
-                {3, "III"},
-                {4, "IV"},
-                {5, "V"},
-                {6, "VI"},
-                {9, "IX"},
-                {27, "XXVII"},
-                {48, "XLVIII"},
-                {59, "LIX"},
-                {93, "XCIII"},
-                {141, "CXLI"},
-                {163, "CLXIII"},
-                {402, "CDII"},
-                {575, "DLXXV"},
-                {911, "CMXI"},
-                {1024, "MXXIV"},
-                {3000, "MMM"}
-        });
-    }
-
-    public RomanNumeralsTest(int arabicNumeral, String romanNumeral) {
-        this.arabicNumeral = arabicNumeral;
-        this.romanNumeral = romanNumeral;
-    }
-
+    private RomanNumeral romanNumeral;
 
     @Test
-    public void convertArabicNumberalToRomanNumeral() {
-        RomanNumeral romanNumeral = new RomanNumeral(arabicNumeral);
+    public void testSingleDigitArabicToRomanNumber() {
+        romanNumeral = new RomanNumeral(1);
+        assertEquals("I", romanNumeral.getRomanNumeral());
 
-        assertEquals(this.romanNumeral, romanNumeral.getRomanNumeral());
+        romanNumeral = new RomanNumeral(2);
+        assertEquals("II", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(3);
+        assertEquals("III", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(4);
+        assertEquals("IV", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(5);
+        assertEquals("V", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(6);
+        assertEquals("VI", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(9);
+        assertEquals("IX", romanNumeral.getRomanNumeral());
     }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testDoubleDigitArabicToRomanNumber() {
+        romanNumeral = new RomanNumeral(27);
+        assertEquals("XXVII", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(48);
+        assertEquals("XLVIII", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(59);
+        assertEquals("LIX", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(93);
+        assertEquals("XCIII", romanNumeral.getRomanNumeral());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testThreeDigitArabicToRomanNumber() {
+        romanNumeral = new RomanNumeral(141);
+        assertEquals("CXLI", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(163);
+        assertEquals("CLXIII", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(402);
+        assertEquals("CDII", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(575);
+        assertEquals("DLXXV", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(911);
+        assertEquals("CMXI", romanNumeral.getRomanNumeral());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testFourDigitArabicToRomanNumber() {
+        romanNumeral = new RomanNumeral(1024);
+        assertEquals("MXXIV", romanNumeral.getRomanNumeral());
+
+        romanNumeral = new RomanNumeral(3000);
+        assertEquals("MMM", romanNumeral.getRomanNumeral());
+    }
+
 }
