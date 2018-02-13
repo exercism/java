cf757a086ec6446c4211195eb3a5268d3b148542
Christopher Thornton <christopher.a.thornton@gmail.com>
2018-02-12 22:52:41 -0500

    binary: removed use of parameterized tests
    Removed the parameterized tests and ensured that the tests follow the current canonical data.

--- exercises/binary/src/test/java/BinaryTest.java
+++ exercises/binary/src/test/java/BinaryTest.java
@@ -1,49 +1,114 @@
 import org.junit.Test;
 import org.junit.Ignore;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class BinaryTest {
 
-    private String binaryNumberAsString;
-    private int decimalNumber;
-
-    @Parameters(name = "{index}: expected {1} when converting \"{0}\" from binary to decimal.")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {"1", 1},
-                {"10", 2},
-                {"11", 3},
-                {"100", 4},
-                {"1001", 9},
-                {"11010", 26},
-                {"10001101000", 1128},
-                {"2", 0},
-                {"5", 0},
-                {"9", 0},
-                {"134678", 0},
-                {"abc10z", 0},
-                {"011", 3}
-        });
-    }
-
-    public BinaryTest(String binaryNumberAsString, int decimalNumber) {
-        this.binaryNumberAsString = binaryNumberAsString;
-        this.decimalNumber = decimalNumber;
-    }
-
+    private Binary binary;
 
     @Test
-    public void test() {
-        Binary binary = new Binary(binaryNumberAsString);
-
-        assertEquals(decimalNumber, binary.getDecimal());
+    public void testBinary0IsDecimal0() {
+	binary = new Binary("0");
+	assertEquals(0, binary.getDecimal());
     }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testBinary1IsDecimal1() {
+	binary = new Binary("1");
+	assertEquals(1, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testBinary10IsDecimal2() {
+	binary = new Binary("10");
+	assertEquals(2, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testBinary11IsDecimal3() {
+	binary = new Binary("11");
+	assertEquals(3, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testBinary100IsDecimal4() {
+	binary = new Binary("100");
+	assertEquals(4, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testBinary1001IsDecimal9() {
+	binary = new Binary("1001");
+	assertEquals(9, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testBinary11010IsDecimal26() {
+	binary = new Binary("11010");
+	assertEquals(26, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testBinary10001101000IsDecimal1128() {
+	binary = new Binary("10001101000");
+	assertEquals(1128, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testBinaryIgnoresLeadingZeros() {
+	binary = new Binary("000011111");
+	assertEquals(31, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void test2NotValidBinaryDigit() {
+	binary = new Binary("2");
+	assertEquals(0, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testNumberContainingNonBinaryDigitInvalid() {
+	binary = new Binary("01201");
+	assertEquals(0, binary.getDecimal());
+   }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testNumberWithTrailingNonBinaryCharactersInvalid() {
+	binary = new Binary("10nope");
+	assertEquals(0, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testNumberWithLeadingNonBinaryCharactersInvalid() {
+	binary = new Binary("nope10");
+	assertEquals(0, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testNumberWithInternalNonBinaryCharactersInvalid() {
+	binary = new Binary("10nope10");
+	assertEquals(0, binary.getDecimal());
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testNumberAndWordWhitespaceSeparatedInvalid() {
+	binary = new Binary("001 nope");
+	assertEquals(0, binary.getDecimal());
+    }
+
 }
