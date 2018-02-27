59e76806998f7ca83c6db0c42e89a941f683ce0c
Jack Mustacato <jmustacato@knights.ucf.edu>
2018-02-27 16:13:36 -0500

    Add new exercise Variable Length Quantity

---
+++ exercises/variable-length-quantity/src/main/java/VariableLengthQuantity.java
@@ -0,0 +1,10 @@
+class VariableLengthQuantity {
+
+    List<String> encode(List<Long> numbers) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    List<String> decode(List<Long> bytes) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
