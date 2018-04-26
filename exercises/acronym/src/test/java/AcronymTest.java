33c2d1cfcd6b75b8359e49cb2a60caf3e4996ca3
Karan Mirani <miranikaran@gmail.com>
2018-04-26 03:16:46 -0400

    removed final from tests (#1434)

--- exercises/acronym/src/test/java/AcronymTest.java
+++ exercises/acronym/src/test/java/AcronymTest.java
@@ -7,40 +7,40 @@ public class AcronymTest {
 
     @Test
     public void basic() {
-        final String phrase = "Portable Network Graphics";
-        final String expected = "PNG";
+        String phrase = "Portable Network Graphics";
+        String expected = "PNG";
         assertEquals(expected, new Acronym(phrase).get());
     }
 
     @Ignore("Remove to run test")
     @Test
     public void lowercaseWords() {
-        final String phrase = "Ruby on Rails";
-        final String expected = "ROR";
+        String phrase = "Ruby on Rails";
+        String expected = "ROR";
         assertEquals(expected, new Acronym(phrase).get());
     }
 
     @Ignore("Remove to run test")
     @Test
     public void punctuation() {
-        final String phrase = "First In, First Out";
-        final String expected = "FIFO";
+        String phrase = "First In, First Out";
+        String expected = "FIFO";
         assertEquals(expected, new Acronym(phrase).get());
     }
 
     @Ignore("Remove to run test")
     @Test
     public void NonAcronymAllCapsWord() {
-        final String phrase = "GNU Image Manipulation Program";
-        final String expected = "GIMP";
+        String phrase = "GNU Image Manipulation Program";
+        String expected = "GIMP";
         assertEquals(expected, new Acronym(phrase).get());
     }
 
     @Ignore("Remove to run test")
     @Test
     public void punctuationWithoutWhitespace() {
-        final String phrase = "Complementary metal-oxide semiconductor";
-        final String expected = "CMOS";
+        String phrase = "Complementary metal-oxide semiconductor";
+        String expected = "CMOS";
         assertEquals(expected, new Acronym(phrase).get());
     }
 
