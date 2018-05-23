e7b7a430a21f7252e9766e28bc9c60ac9dedfa79
Aaditya Arvind Kulkarni <aadikulkarni91@gmail.com>
2018-05-23 03:37:01 -0400

    queen-attack: removed `final` from tests (#1456)

--- exercises/queen-attack/src/test/java/QueenAttackCalculatorTest.java
+++ exercises/queen-attack/src/test/java/QueenAttackCalculatorTest.java
@@ -55,7 +55,7 @@ public class QueenAttackCalculatorTest {
     @Ignore("Remove to run test")
     @Test
     public void testQueensCannotAttack() {
-        final QueenAttackCalculator calculator
+        QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 4), new Queen(6, 6));
 
         assertFalse(calculator.canQueensAttackOneAnother());
@@ -64,7 +64,7 @@ public class QueenAttackCalculatorTest {
     @Ignore("Remove to run test")
     @Test
     public void testQueensCanAttackOnTheSameRow() {
-        final QueenAttackCalculator calculator
+        QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 4), new Queen(2, 6));
 
         assertTrue(calculator.canQueensAttackOneAnother());
@@ -73,7 +73,7 @@ public class QueenAttackCalculatorTest {
     @Ignore("Remove to run test")
     @Test
     public void testQueensCanAttackOnTheSameColumn() {
-        final QueenAttackCalculator calculator
+        QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(4, 5), new Queen(2, 5));
 
         assertTrue(calculator.canQueensAttackOneAnother());
@@ -82,7 +82,7 @@ public class QueenAttackCalculatorTest {
     @Ignore("Remove to run test")
     @Test
     public void testQueensCanAttackOnFirstDiagonal() {
-        final QueenAttackCalculator calculator
+        QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 2), new Queen(0, 4));
 
         assertTrue(calculator.canQueensAttackOneAnother());
@@ -91,7 +91,7 @@ public class QueenAttackCalculatorTest {
     @Ignore("Remove to run test")
     @Test
     public void testQueensCanAttackOnSecondDiagonal() {
-        final QueenAttackCalculator calculator
+        QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 2), new Queen(3, 1));
 
         assertTrue(calculator.canQueensAttackOneAnother());
@@ -100,7 +100,7 @@ public class QueenAttackCalculatorTest {
     @Ignore("Remove to run test")
     @Test
     public void testQueensCanAttackOnThirdDiagonal() {
-        final QueenAttackCalculator calculator
+        QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 2), new Queen(1, 1));
 
         assertTrue(calculator.canQueensAttackOneAnother());
@@ -109,7 +109,7 @@ public class QueenAttackCalculatorTest {
     @Ignore("Remove to run test")
     @Test
     public void testQueensCanAttackOnFourthDiagonal() {
-        final QueenAttackCalculator calculator
+        QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 2), new Queen(5, 5));
 
         assertTrue(calculator.canQueensAttackOneAnother());
