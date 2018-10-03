1440171e42edf0563ea49dbed25b4f5c772894ed
alison c <alisonc@alisonc.net>
2018-10-03 10:48:48 -0400

    Update grade-school tests and reference implementation to 1.0.0 (#1538) (#1572)
    * Update grade-school tests and reference implementation to 1.0.0 (#1538)
    
    * Apply PR suggestions
    
    * private school in tests

--- exercises/grade-school/.meta/src/reference/java/School.java
+++ exercises/grade-school/.meta/src/reference/java/School.java
@@ -4,35 +4,35 @@ class School {
 
   private final Map<Integer, List<String>> database = new HashMap<>();
 
-  int numberOfStudents() {
-    int result = 0;
-    for (List<String> studentsInGrade : database.values()) {
-      result += studentsInGrade.size();
-    }
-    return result;
-  }
-
   void add(String student, int grade) {
     List<String> students = fetchGradeFromDatabase(grade);
     students.add(student);
+  }
+
+  List<String> roster() {
+    List<String> result = new ArrayList<String>();
+    for (List<String> studentsInGrade : studentsByGradeAlphabetical().values()) {
+      result.addAll(studentsInGrade);
+    }
+    return result;
   }
 
   List<String> grade(int grade) {
     return new ArrayList<>(fetchGradeFromDatabase(grade));
   }
 
-  List<String> fetchGradeFromDatabase(int grade) {
+  private List<String> fetchGradeFromDatabase(int grade) {
     if (!database.containsKey(grade)) {
       database.put(grade, new LinkedList<>());
     }
+    Collections.sort(database.get(grade));
     return database.get(grade);
   }
 
-  Map<Integer, List<String>> studentsByGradeAlphabetical() {
+  private Map<Integer, List<String>> studentsByGradeAlphabetical() {
     Map<Integer, List<String>> sortedStudents = new HashMap<>();
     for (Integer grade : database.keySet()) {
-      List<String> studentsInGrade = database.get(grade);
-      Collections.sort(studentsInGrade);
+      List<String> studentsInGrade = fetchGradeFromDatabase(grade);
       sortedStudents.put(grade, studentsInGrade);
     }
     return sortedStudents;
--- exercises/grade-school/src/test/java/SchoolTest.java
+++ exercises/grade-school/src/test/java/SchoolTest.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 import java.util.Collection;
+import java.util.Arrays;
 
 import org.hamcrest.Matcher;
 import org.hamcrest.collection.IsIterableContainingInOrder;
@@ -24,109 +25,74 @@ public class SchoolTest {
   }
 
   @Test
-  public void startsWithNoStudents() {
-    assertThat(school.numberOfStudents(), is(0));
-  }
-
-  @Ignore("Remove to run test")
-  @Test
-  public void addsStudents() {
+  public void addingAStudentAddsThemToTheSortedRoster() {
+    school = new School();
     school.add("Aimee", 2);
-    assertThat(school.grade(2), hasItem("Aimee"));
+    List<String> expected = Arrays.asList("Aimee");
+    assertThat(school.roster(), is(expected));
   }
 
   @Ignore("Remove to run test")
   @Test
-  public void addsMoreStudentsInSameGrade() {
+  public void addingMoreStudentsAddsThemToTheSameSortedRoster() {
+    school = new School();
     int grade = 2;
-    school.add("James", grade);
     school.add("Blair", grade);
+    school.add("James", grade);
     school.add("Paul", grade);
-
-    assertThat(school.grade(grade).size(), is(3));
-    assertThat(school.grade(grade), allOf(hasItem("James"), hasItem("Blair"), hasItem("Paul")));
+    List<String> expected = Arrays.asList("Blair", "James", "Paul");
+    assertThat(school.roster(), is(expected));
   }
 
   @Ignore("Remove to run test")
   @Test
-  public void addsStudentsInMultipleGrades() {
+  public void addingStudentsToDifferentGradesAddsThemToTheSameSortedRoster() {
+    school = new School();
     school.add("Chelsea", 3);
     school.add("Logan", 7);
-
-    assertThat(school.numberOfStudents(), is(2));
-    assertThat(school.grade(3).size(), is(1));
-    assertThat(school.grade(3), hasItem("Chelsea"));
-    assertThat(school.grade(7).size(), is(1));
-    assertThat(school.grade(7), hasItem("Logan"));
+    List<String> expected = Arrays.asList("Chelsea", "Logan");
+    assertThat(school.roster(), is(expected));
   }
 
   @Ignore("Remove to run test")
   @Test
-  public void getsStudentsInEmptyGrade() {
-    assertTrue(school.grade(1).isEmpty());
+  public void rosterReturnsAnEmptyListIfThereAreNoStudentsEnrolled() {
+    school = new School();
+    List<String> expected = Arrays.asList();
+    assertThat(school.roster(), is(expected));
   }
 
   @Ignore("Remove to run test")
   @Test
-  public void sortsSchool() {
-    school.add("Kyle", 4);
-    school.add("Zed", 4);
-    school.add("Adam", 4);
-    school.add("Jennifer", 4);
-    school.add("Kareem", 6);
-    school.add("Christopher", 4);
-    school.add("Kylie", 3);
-    Map<Integer, Matcher> sortedStudents = new HashMap<Integer, Matcher>();
-    sortedStudents.put(6, IsIterableContainingInOrder
-      .contains("Kareem"));
-    sortedStudents.put(4, IsIterableContainingInOrder
-      .contains("Adam", "Christopher", "Jennifer", "Kyle", "Zed"));
-    sortedStudents.put(3, IsIterableContainingInOrder
-      .contains("Kylie"));
-
-    Map schoolStudents = school.studentsByGradeAlphabetical();
-    for (Map.Entry<?, Matcher> entry : sortedStudents.entrySet()) {
-
-      assertThat((Collection) schoolStudents.get(entry.getKey()), entry.getValue());
-    }
+  public void studentNamesWithGradesAreDisplayedInTheSameSortedRoster() {
+    school = new School();
+    school.add("Peter", 2);
+    school.add("Anna", 1);
+    school.add("Barb", 1);
+    school.add("Zoe", 2);
+    school.add("Alex", 2);
+    school.add("Jim", 3);
+    school.add("Charlie", 1);
+    List<String> expected = Arrays.asList("Anna", "Barb", "Charlie", "Alex", "Peter", "Zoe", "Jim");
+    assertThat(school.roster(), is(expected));
   }
 
   @Ignore("Remove to run test")
   @Test
-  public void modifyingFetchedGradeShouldNotModifyInternalDatabase() {
-    String shouldNotBeAdded = "Should not be added to school";
-    int grade = 1;
-
-    Collection students = school.grade(grade);
-
-    try {
-      students.add(shouldNotBeAdded);
-    } catch (Exception exception) {
-      // Also valid that the add operation throws an exception
-      // Such as UnsupportedOperationException when an umodifiable collection type is used
-    }
-
-    assertThat(school.grade(grade), not(hasItem(shouldNotBeAdded)));
+  public void gradeReturnsTheStudentsInThatGradeInAlphabeticalOrder() {
+    school = new School();
+    school.add("Franklin", 5);
+    school.add("Bradley", 5);
+    school.add("Jeff", 1);
+    List<String> expected = Arrays.asList("Bradley", "Franklin");
+    assertThat(school.grade(5), is(expected));
   }
 
   @Ignore("Remove to run test")
   @Test
-  public void modifyingSortedStudentsShouldNotModifyInternalDatabase() {
-    int grade = 2;
-    String studentWhichShouldNotBeAdded = "Should not be added";
-    List<String> listWhichShouldNotBeAdded = new ArrayList<>();
-    listWhichShouldNotBeAdded.add(studentWhichShouldNotBeAdded);
-
-    Map sortedStudents = school.studentsByGradeAlphabetical();
-
-    try {
-      sortedStudents.put(grade, listWhichShouldNotBeAdded);
-    } catch (Exception exception) {
-      // Also valid that the put operation throws an exception
-      // Such as UnsupportedOperationException when an unmodifiableMap is used
-    }
-
-    assertThat(school.studentsByGradeAlphabetical().get(grade),
-      not(hasItem(studentWhichShouldNotBeAdded)));
+  public void gradeReturnsAnEmptyListIfThereAreNoStudentsInThatGrade() {
+    school = new School();
+    List<String> expected = Arrays.asList();
+    assertThat(school.grade(1), is(expected));
   }
 }
