5a0550ae904623cf8c12dead39b8cb81e2a5eafa
mirkoperillo <mirko.perillo@gmail.com>
2018-10-22 06:43:26 +0200

    hexadecimal: fix style

--- exercises/hexadecimal/.meta/src/reference/java/Hexadecimal.java
+++ exercises/hexadecimal/.meta/src/reference/java/Hexadecimal.java
@@ -2,9 +2,11 @@ import java.util.Arrays;
 
 class Hexadecimal {
     private static final String HEXES = "0123456789abcdef";
-    static int toDecimal(String input){
+    static int toDecimal(String input) {
         boolean  isHex = input.matches("^[0-9a-f]+");
-        if (!isHex) return 0;
+        if (!isHex) {
+            return 0;
+        }
         return Arrays.stream(input.split(""))
                 .map(HEXES::indexOf)
                 .reduce(0, (decimal, hex) -> decimal * 16 + hex);
