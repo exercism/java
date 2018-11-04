3d352a7b934558b3cdd0d46b5162f661ee4fcfac
lemoncurry <git@annakrueger.de>
2018-11-04 09:52:55 +0100

    change exercise implementation to avoid division by 0

--- exercises/sum-of-multiples/.meta/src/reference/java/SumOfMultiples.java
+++ exercises/sum-of-multiples/.meta/src/reference/java/SumOfMultiples.java
@@ -4,21 +4,18 @@ import java.util.stream.IntStream;
 public class SumOfMultiples {
     private final int sum;
 
-    public SumOfMultiples(int number, int[] set) {
-        sum = calculateSum(number, set);
+    public SumOfMultiples(int limit, int[] set) {
+        sum = calculateSum(limit, set);
     }
 
     public int getSum() {
         return sum;
     }
 
-    private int calculateSum(int number, int[] set) {
-
-        int sum = IntStream.range(1, number)
-                .filter(i -> Arrays.stream(set).filter(j -> i % j == 0).count() > 0)
-                .sum();
-
-        return sum;
+    private int calculateSum(int limit, int[] set) {
+        return IntStream.range(1, limit)
+            .filter(n -> Arrays.stream(set).filter(s -> s != 0).anyMatch(s -> n % s == 0))
+            .sum();
     }
 
 }
