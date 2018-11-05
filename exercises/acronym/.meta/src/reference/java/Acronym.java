5dab193de6f66f1e39b2e98f6959643c40713e94
lemoncurry <git@annakrueger.de>
2018-11-05 19:25:11 +0100

    acronym: change regex to also pass new apostrophe test

--- exercises/acronym/.meta/src/reference/java/Acronym.java
+++ exercises/acronym/.meta/src/reference/java/Acronym.java
@@ -1,24 +1,24 @@
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-final class Acronym {
+class Acronym {
 
-    private final String acronym;
+    private String acronym;
 
     Acronym(String phrase) {
-        acronym = generateAcronym(phrase);
-    }
-
-    String get() {
-        return acronym;
+        this.acronym = generateAcronym(phrase);
     }
 
     private String generateAcronym(String phrase) {
-        return Arrays.stream(phrase.split("[^a-zA-Z]"))
-                .filter(word -> !word.isEmpty()) // Remove empty strings from the result of phrase.split
-                .map(word -> word.substring(0, 1)) // Get the first character of each word
-                .collect(Collectors.joining()) // Concatenate the characters
-                .toUpperCase();
+        return Arrays.stream(phrase.split("(?!')\\W"))
+            .filter(word -> !word.isEmpty()) // Remove empty strings from the result of phrase.split
+            .map(word -> word.substring(0, 1)) // Get the first character of each word
+            .collect(Collectors.joining()) // Concatenate the characters
+            .toUpperCase();
+    }
+
+    String getAcronym() {
+        return acronym;
     }
 
 }
