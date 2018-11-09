f01df2d7d2df382f6418135ef136bc97747da226
Shubhayu Das <43082352+sateslayer@users.noreply.github.com>
2018-11-09 15:53:34 +0530

    Update Acronym.java

--- exercises/acronym/src/main/java/Acronym.java
+++ exercises/acronym/src/main/java/Acronym.java
@@ -1,11 +1,20 @@
-class Acronym {
-
-    Acronym(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
-    String get() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;//Importing java.io for faster input/output
+import java.io.IOException;
+class Acronym
+{	static String input;
+	public static void main(String[] args)throws IOException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		input="  "+br.readLine().trim();
+		System.out.println(getAcronym());
+	}
+	public static String getAcronym()       //Function to perform the main claculations
+	{
+		String output="";
+		for(int i=0;i<input.length()-1;i++)
+			if(input.charAt(i)==' ')      //If a character is a space, then add the next character in upper case to the output
+				output+=Character.toString(input.charAt(i+1)).toUpperCase();
+		return output.substring(1);     //Return the output without the preceding space
+	}
 }
