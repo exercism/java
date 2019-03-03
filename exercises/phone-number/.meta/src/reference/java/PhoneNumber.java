ee5bc1e5d08517b8021739b77f7897e65b7aa0d3
mirkoperillo <mirko.perillo@gmail.com>
2019-03-03 14:20:15 +0100

    align exception messages to problem spefication

--- exercises/phone-number/.meta/src/reference/java/PhoneNumber.java
+++ exercises/phone-number/.meta/src/reference/java/PhoneNumber.java
@@ -13,9 +13,11 @@ public class PhoneNumber {
                 // Remove spaces, dots, parentheses, hyphens and pluses
                 continue;
             }
+            if ("-@:!".indexOf(c) > -1) {
+                throw new IllegalArgumentException("punctuations not permitted");
+            }
             if (!Character.isDigit(c)) {
-                throw new IllegalArgumentException("Illegal character in phone number. "
-                        + "Only digits, spaces, parentheses, hyphens or dots accepted.");
+                throw new IllegalArgumentException("letters not permitted");
             }
             stringBuilder.append(c);
         }
@@ -23,24 +25,36 @@ public class PhoneNumber {
     }
 
     private String normalize(String number) {
-        if (number.length() > 11 || number.length() < 10) {
-            throw new IllegalArgumentException("Number must be 10 or 11 digits");
+        if (number.length() < 10) {
+            throw new IllegalArgumentException("incorrect number of digits");
         }
 
+        if (number.length() > 11) {
+            throw new IllegalArgumentException("more than 11 digits");
+        }
+        
         if (number.length() == 11) {
             if (number.startsWith("1")) {
                 number = number.substring(1, number.length());
             } else {
-                throw new IllegalArgumentException("Can only have 11 digits if number starts with '1'");
+                throw new IllegalArgumentException("11 digits must start with 1");
             }
         }
-
-        if (number.startsWith("0") || number.startsWith("1")) {
-            throw new IllegalArgumentException("Illegal Area Or Exchange Code. "
-                    + "Only 2-9 are valid digits");
-        } else if (number.charAt(3) == '0' || number.charAt(3) == '1') {
-            throw new IllegalArgumentException("Illegal Area Or Exchange Code. "
-                    + "Only 2-9 are valid digits");
+  
+        if (number.startsWith("0")) {
+            throw new IllegalArgumentException("area code cannot start with zero");
+        }
+        
+        if (number.startsWith("1")) {
+            throw new IllegalArgumentException("area code cannot start with one");
+        }
+        
+        if (number.charAt(3) == '0') {
+            throw new IllegalArgumentException("exchange code cannot start with zero");
+        }
+        
+        if (number.charAt(3) == '1') {
+            throw new IllegalArgumentException("exchange code cannot start with one");
         }
 
         return number;
