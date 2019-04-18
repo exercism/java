637eb98529c3da2ca6807b8297b910cf8a7c9922
Anna Weber <lemoncurry@users.noreply.github.com>
2019-04-18 11:24:03 +0200

    markdown: update to v1.4.0 (#1650)
    * markdown: update to v1.4.0
    
    * markdown: change to single backslash in new test input
    
    * markdown: remove comment

--- exercises/markdown/src/test/java/MarkdownTest.java
+++ exercises/markdown/src/test/java/MarkdownTest.java
@@ -119,4 +119,14 @@ public class MarkdownTest {
 
         assertEquals(expected, markdown.parse(input));
     }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void markdownUnorderedListsCloseProperlyWithPrecedingAndFollowingLines() {
+        String input = "# Start a list\n* Item 1\n* Item 2\nEnd a list";
+        String expected = "<h1>Start a list</h1><ul><li>Item 1</li><li>Item 2</li></ul><p>End a list</p>";
+
+        assertEquals(expected, markdown.parse(input));
+    }
+
 }
