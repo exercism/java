2d40093cd8229f9cc6ecf3206c4950ca14f93f80
KID1412 <Kameldadda@gmail.com>
2019-10-01 15:38:14 +0100

    changecalculator

--- exercises/change/src/main/java/ChangeCalculator.java
+++ exercises/change/src/main/java/ChangeCalculator.java
@@ -1,10 +1,19 @@
-/*
+class ChangeCalculator {
+    static int  [] coins  = {100,25,10,5,1};
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
+    public static ArrayList<> getChange(int x) { // x is the input 
+      ArrayList<> returned = new ArrayList<Integer>();// array contain all coins returned by change  
+        int  res = x;
+        int i = 0;
+        while (i<coins.length || res >0) {
+            if(coins[i]<=res){
+                res-=coins[i];
+                returned.add(coins[i]);
+            }else{
+                i++;
+            }
+        }
+        return returned;
+    }
+ 
+}
