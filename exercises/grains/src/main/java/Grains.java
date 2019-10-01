995416d5a53d3ca536bfb34c140bcb2081e51653
Olivier Peyrusse <ope@activeviam.com>
2019-10-01 14:14:42 +0200

    Implement the tests and the basic structure of the class for this exercise.

---
+++ exercises/grains/src/main/java/Grains.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+
+class Grains {
+
+	/**
+	 * Computes the number of grains on a given cell.
+	 * @param cell cell to consider
+	 * @return the number of grains on this cell
+	 */
+	static BigInteger compute(final int cell) {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
+
+	/**
+	 * Computes the total number of grains on the whole chessboard
+	 * @return the total number of grains
+	 */
+	static BigInteger computeTotal() {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
+
+}
