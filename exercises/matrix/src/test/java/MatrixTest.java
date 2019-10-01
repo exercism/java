7c3e0d7e525b81cb09e38c39b6ec0f1ed966fe58
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-10-01 21:59:55 +0530

    added ignore annotation to all tests except the first one
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- exercises/matrix/src/test/java/MatrixTest.java
+++ exercises/matrix/src/test/java/MatrixTest.java
@@ -16,6 +16,7 @@ public class MatrixTest {
         assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void extractRowFromMatrixTest() {
         String matrixAsString = "1 2\n3 4";
@@ -27,6 +28,7 @@ public class MatrixTest {
         assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void extractRowFromDiffWidthsMatrixTest() {
         String matrixAsString = "1 2\n10 20";
@@ -38,6 +40,7 @@ public class MatrixTest {
         assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void extractRowFromNonSquareMatrixTest() {
         String matrixAsString = "1 2 3\n4 5 6\n7 8 9\n8 7 6";
@@ -49,6 +52,7 @@ public class MatrixTest {
         assertArrayEquals(expectedRow, matrix.getRow(rowIndex));
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void extractColumnFromOneNumberMatrixTest() {
         String matrixAsString = "1";
@@ -60,6 +64,7 @@ public class MatrixTest {
         assertArrayEquals(expectedColumn, matrix.getColumn(columnIndex));
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void extractColumnMatrixTest() {
         String matrixAsString = "1 2 3\n4 5 6\n7 8 9";
@@ -71,6 +76,7 @@ public class MatrixTest {
         assertArrayEquals(expectedColumn, matrix.getColumn(columnIndex));
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void extractColumnFromNonSquareMatrixTest() {
         String matrixAsString = "1 2 3 4\n5 6 7 8\n9 8 7 6";
@@ -82,6 +88,7 @@ public class MatrixTest {
         assertArrayEquals(expectedColumn, matrix.getColumn(columnIndex));
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void extractColumnFromDiffWidthsMatrixTest() {
         String matrixAsString = "89 1903 3\n18 3 1\n9 4 800";
