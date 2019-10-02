6c34cce974a2a59d7a1319e7fc1a032d5a12d9de
Olivier Peyrusse <ope@activeviam.com>
2019-10-02 14:07:18 +0200

    Complete tests.

--- exercises/hangman/src/test/java/HangmanTest.java
+++ exercises/hangman/src/test/java/HangmanTest.java
@@ -2,6 +2,8 @@ import io.reactivex.Observable;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Collections;
+
 import static org.junit.Assert.assertEquals;
 
 class HangmanTest {
@@ -14,6 +16,11 @@ class HangmanTest {
         final Hangman.Output init = result.first(null).blockingGet();
         Assert.assertNotNull(init);
         assertEquals("secret", init.secret);
+        assertEquals("______", init.discovered);
+        assertEquals(Collections.emptySet(), init.guess);
+        assertEquals(Collections.emptySet(), init.misses);
+        assertEquals(Collections.emptyList(), init.parts);
+        assertEquals(Hangman.Status.PLAYING, init.status);
     }
 
     @Test
