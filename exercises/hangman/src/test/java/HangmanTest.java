bd5f69c1daa6cb7559f74505386e4d96cf3ee201
Kineolyan <kineolyan@gmail.com>
2019-10-02 21:31:01 +0200

    Add last tests on multiple games.

--- exercises/hangman/src/test/java/HangmanTest.java
+++ exercises/hangman/src/test/java/HangmanTest.java
@@ -5,6 +5,7 @@ import io.reactivex.Observable;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.junit.Test;
 
@@ -66,6 +67,63 @@ public class HangmanTest {
                         Hangman.Part.BODY),
                 last.parts);
         assertEquals(Hangman.Status.PLAYING, last.status);
+    }
+
+    @Test
+    public void wonGame() {
+        final Observable<Hangman.Output> result = Hangman.play(
+                Observable.fromArray("secret"),
+                Observable.fromArray("a", "e", "o", "s", "c", "r", "g", "t"));
+        final Hangman.Output last = result.blockingLast();
+        assertEquals("secret", last.discovered);
+        assertEquals(setOf("c", "e", "r", "s", "t"), last.guess);
+        assertEquals(Hangman.Status.WIN, last.status);
+    }
+
+    @Test
+    public void lostGame() {
+        final Observable<Hangman.Output> result = Hangman.play(
+                Observable.fromArray("secret"),
+                Observable.fromArray("a", "b", "c", "d", "e", "f", "g", "h"));
+        final Hangman.Output last = result.blockingLast();
+        assertEquals("_ec_e_", last.discovered);
+        assertEquals(setOf("a", "b", "d", "f", "g", "h"), last.misses);
+        assertEquals(Hangman.Status.LOSS, last.status);
+        assertEquals(
+                Arrays.asList(
+                        Hangman.Part.HEAD,
+                        Hangman.Part.BODY,
+                        Hangman.Part.LEFT_ARM,
+                        Hangman.Part.RIGHT_ARM,
+                        Hangman.Part.LEFT_LEG,
+                        Hangman.Part.RIGHT_LEG),
+                last.parts);
+    }
+
+    @Test
+    public void consecutiveGames() {
+        final Observable<Hangman.Output> outputs = Hangman.play(
+                Observable.fromArray("secret", "abba"),
+                Observable.fromArray(
+                        "a", "e", "o", "s", "c", "r", "g", "t",
+                        "a", "e", "s", "b"));
+        final List<Hangman.Output> results = outputs
+                .filter(output -> output.status != Hangman.Status.PLAYING)
+                .toList()
+                .blockingGet();
+        assertEquals(2, results.size());
+
+        final Hangman.Output first = results.get(0);
+        assertEquals("secret", first.discovered);
+        assertEquals(setOf("s", "e", "c", "r", "t"), first.guess);
+        assertEquals(setOf("a", "o", "g"), first.misses);
+        assertEquals(Hangman.Status.WIN, first.status);
+
+        final Hangman.Output second = results.get(1);
+        assertEquals("abba", second.discovered);
+        assertEquals(setOf("a", "b"), second.guess);
+        assertEquals(setOf("e", "s"), second.misses);
+        assertEquals(Hangman.Status.WIN, first.status);
     }
 
     private static <T> Set<T> setOf(final T... values) {
