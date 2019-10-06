b3a3a00f1af9a2f8a07cc98f67253f0e5f78ae3f
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-10-06 20:10:42 +0530

    updated reference implementation to not use if/else

--- exercises/leap/.meta/src/reference/java/Leap.java
+++ exercises/leap/.meta/src/reference/java/Leap.java
@@ -1,13 +1,13 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        boolean isLeapYear = false;
-        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
-            isLeapYear = true;
-        } else if (year % 4 == 0 && year % 100 != 0) {
-            isLeapYear = true;
-        }
-        return isLeapYear;
+        return (
+            (
+                (year % 4 == 0) && 
+                (year % 100 != 0)
+            ) || 
+            (year % 400 == 0)
+            ); 
     }
 
 }
