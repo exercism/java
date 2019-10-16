10cf1fa65f37074fd01448aa6fc9e2fc9082c551
Olivier Peyrusse <ope@activeviam.com>
2019-10-16 16:17:01 +0200

    Remove copyright.

--- exercises/hangman/.meta/src/reference/java/Status.java
+++ exercises/hangman/.meta/src/reference/java/Status.java
@@ -1,10 +1,3 @@
-/*
- * (C) ActiveViam 2019
- * ALL RIGHTS RESERVED. This material is the CONFIDENTIAL and PROPRIETARY
- * property of ActiveViam. Any unauthorized use
- * reproduction or transfer of this material is strictly prohibited
- */
-
 enum Status {
     PLAYING,
     WIN,
