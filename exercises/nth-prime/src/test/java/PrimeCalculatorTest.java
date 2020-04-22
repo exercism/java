913b3d3d1b9a6b9a6760868bf87a1dab8cbc8982
Jason Runkle <jmrunkle@google.com>
2020-04-22 10:34:08 -0700

    Remove uses of Hamcrest assertions or migrate to AssertJ (#1823)
    * Migrate robot-name tests to AssertJ.
    
    * Migrate nucleotide-count to AssertJ.
    
    * Remove unused hamcrest dependency in grade-school.
    
    * Migrate nth-prime to AssertJ.
    
    * Migrate diamond to AssertJ.
    
    * Migrate simple-linked-list to AssertJ.
    
    * Migrate linked-list to AssertJ.
    
    * Migrate proverb to AssertJ.
    
    * Migrate anagram to AssertJ.
    
    * Migrate circular-buffer to AssertJ.
    
    * Remove unnecessary hamcrest usage from alphametics.
    
    * Update .gitignore to ignore generated files from the IDE.

--- exercises/nth-prime/src/test/java/PrimeCalculatorTest.java
+++ exercises/nth-prime/src/test/java/PrimeCalculatorTest.java
@@ -1,11 +1,10 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
 
 public class PrimeCalculatorTest {
     private PrimeCalculator primeCalculator;
@@ -20,25 +19,25 @@ public class PrimeCalculatorTest {
 
     @Test
     public void testFirstPrime() {
-        assertThat(primeCalculator.nth(1), is(2));
+        assertThat(primeCalculator.nth(1)).isEqualTo(2);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testSecondPrime() {
-        assertThat(primeCalculator.nth(2), is(3));
+        assertThat(primeCalculator.nth(2)).isEqualTo(3);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testSixthPrime() {
-        assertThat(primeCalculator.nth(6), is(13));
+        assertThat(primeCalculator.nth(6)).isEqualTo(13);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testBigPrime() {
-        assertThat(primeCalculator.nth(10001), is(104743));
+        assertThat(primeCalculator.nth(10001)).isEqualTo(104743);
     }
 
     @Ignore("Remove to run test")
