2be15a4bc14cbe1794f8f840d93401f06d7c260e
André Santos <andrerfcsantos@gmail.com>
2020-05-18 17:32:41 +0100

    [Rust] Add "Raindrops"

---
+++ rust/raindrops/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn raindrops(n: u32) -> String {
+    let mut res = String::new();
+
+    if n % 3 == 0 {
+        res.push_str("Pling");
+    }
+    if n % 5 == 0 {
+        res.push_str("Plang");
+    }
+    if n % 7 == 0 {
+        res.push_str("Plong");
+    }
+
+    if res.len() == 0 {
+        res.push_str(&n.to_string());
+    }
+
+    return res;
+}
