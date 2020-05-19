e3f208f900d9b924dc8be5789026225376917159
André Santos <andrerfcsantos@gmail.com>
2020-05-19 17:30:40 +0100

    [Rust] "GIgasecond" - Take advantage of 'core::ops::Add' trait

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,6 @@
 use chrono::{DateTime, Duration, Utc};
-use core::ops::Add;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start.add(Duration::seconds(1_000_000_000))
+    start + Duration::seconds(1_000_000_000)
 }
