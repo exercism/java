7e254f2c1c633d7e5713710f0cdd7a9cec466b80
André Santos <andrerfcsantos@gmail.com>
2020-07-28 19:00:51 +0100

    [Python] Add "Hello World"

---
+++ python/hello-world/hello_world_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from hello_world import hello
+
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
+
+
+class HelloWorldTest(unittest.TestCase):
+    def test_say_hi(self):
+        self.assertEqual(hello(), "Hello, World!")
+
+
+if __name__ == "__main__":
+    unittest.main()
