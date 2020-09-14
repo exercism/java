120d33250a05beb1c5fb184c89576b7480528ee2
TalesDias <dtales15@gmail.com>
2020-09-14 07:19:16 -0300

    Implement concept exercise conditionals
    * implemented exercise and tests
    
    * changed docs
    
    * hotfix: design.md formatting
    
    * style review
    
    * add missing config entry
    
    * fix json
    
    Co-authored-by: Silva <silva@localhost.localdomain>
    Co-authored-by: mirkoperillo <mirko.perillo@gmail.com>

---
+++ exercises/concept/conditionals/.meta/design.md
@@ -0,0 +1,28 @@
+## Goal
+
+The goal of this exercise is to teach the student the basics of the Concept of Conditionals in Java.
+
+## Learning objectives
+
+- Know of the existence of the `bool` type.
+- Know how to define an if statement.
+- Know to avoid else after return.
+- Know how to define a switch statement.
+
+## Out of scope
+
+- iterations
+- continue (as there are no iterations)
+
+## Concepts
+
+- `booleans`
+- `conditionals-if`
+- `conditionals-switch`
+
+## Prequisites
+
+This exercise's prerequisites Concepts are:
+
+- `basic-strings`
+- `numbers`
---
+++ exercises/concept/conditionals/src/main/java/Blackjack.java
@@ -0,0 +1,32 @@
+public class Blackjack{
+
+    public int  parseCard(String card) {
+        throw new UnsupportedOperationException("Please implement the Blackjack.parseCard method");
+    }
+
+    public boolean isBlackjack(String card1, String card2) {
+        throw new UnsupportedOperationException("Please implement the Blackjack.isBlackjack method");
+    }
+
+    public String largeHand(boolean isBlackjack, int dealerScore) {
+        throw new UnsupportedOperationException("Please implement the Blackjack.largeHand method");
+    }
+
+    public String smallHand(handScore int, dealerScore int) {
+        throw new UnsupportedOperationException("Please implement the Blackjack.smallHand method");
+    }
+
+    // FirstTurn returns the semi-optimal decision for the first turn, given the cards of the player and the dealer.
+    // This function is already implemented and does not need to be edited. It pulls the other functions together in a
+    // complete decision tree for the first turn.
+    public String  firstTurn(String card1, String card2, String dealerCard) {
+        int handScore = parseCard(card1) + parseCard(card2);
+        int dealerScore = parseCard(dealerCard);
+
+        if (20 < handScore) 
+            return largeHand(isBlackjack(card1, card2), dealerScore);
+        
+        else
+            return smallHand(handScore, dealerScore);
+    }
+}
