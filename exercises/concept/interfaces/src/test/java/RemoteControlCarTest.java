80c5c7c3bc39982f6fe5cf6eefd2e69d08cf9ec4
Martin Carlsson <martin.ystrom@gmail.com>
2020-09-29 16:38:51 +0200

    Changes to assertj assertions.
    * Changes to assertj assertions.
    
    * Changes to use isAssignableFrom assertion.
    
    Co-authored-by: Martin Carlsson <martin.carlsson@svt.se>

--- exercises/concept/interfaces/src/test/java/RemoteControlCarTest.java
+++ exercises/concept/interfaces/src/test/java/RemoteControlCarTest.java
@@ -3,8 +3,7 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RemoteControlCarTest {
     @Test
@@ -15,7 +14,7 @@ public class RemoteControlCarTest {
         TestTrack.race((RemoteControlCar) productionCar);
         TestTrack.race((RemoteControlCar) experimentalCar);
         TestTrack.race((RemoteControlCar) experimentalCar);
-        assertSame(20, experimentalCar.getDistanceTravelled() - productionCar.getDistanceTravelled());
+        assertThat(experimentalCar.getDistanceTravelled() - productionCar.getDistanceTravelled()).isEqualTo(20);
     }
 
     @Ignore("Remove to run test")
@@ -26,12 +25,12 @@ public class RemoteControlCarTest {
         prc1.setNumberOfVictories(3);
         prc2.setNumberOfVictories(2);
         List<ProductionRemoteControlCar> rankings = TestTrack.getRankedCars(prc1, prc2);
-        assertSame(prc1, rankings.get(1));
+        assertThat(rankings.get(1)).isEqualTo(prc1);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void ensureCarsAreComparables() {
-        assertTrue(RemoteControlCar.class.isAssignableFrom(ProductionRemoteControlCar.class));
+        assertThat(RemoteControlCar.class).isAssignableFrom(ProductionRemoteControlCar.class);
     }
 }
