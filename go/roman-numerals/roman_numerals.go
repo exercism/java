837cbff9c1208e0d493dc7f7d16606a5b8f1937e
André Santos <andrerfcsantos@gmail.com>
2021-03-28 23:43:39 +0100

    [Go] Add "Roman Numerals"

---
+++ go/roman-numerals/roman_numerals.go
@@ -0,0 +1,48 @@
+package romannumerals
+
+import (
+	"fmt"
+	"strings"
+)
+
+type ConversionEntry struct {
+	Value int
+	Digit string
+}
+
+var conversions = []ConversionEntry{
+	{Value: 1000, Digit: "M"},
+	{Value: 900, Digit: "CM"},
+	{Value: 500, Digit: "D"},
+	{Value: 400, Digit: "CD"},
+	{Value: 100, Digit: "C"},
+	{Value: 90, Digit: "XC"},
+	{Value: 50, Digit: "L"},
+	{Value: 40, Digit: "XL"},
+	{Value: 10, Digit: "X"},
+	{Value: 9, Digit: "IX"},
+	{Value: 5, Digit: "V"},
+	{Value: 4, Digit: "IV"},
+	{Value: 1, Digit: "I"},
+}
+
+func ToRomanNumeral(number int) (string, error) {
+	if number > 3000 {
+		return "", fmt.Errorf("arabic number is bigger than 3000")
+	}
+
+	if number <= 0 {
+		return "", fmt.Errorf("arabic number is zero or negative")
+	}
+
+	var roman strings.Builder
+
+	for _, conversion := range conversions {
+		for number >= conversion.Value {
+			roman.WriteString(conversion.Digit)
+			number -= conversion.Value
+		}
+	}
+
+	return roman.String(), nil
+}
