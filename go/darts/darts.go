69733adf8601c7074013536fbbebb59b0c6b5dfa
André Santos <andrerfcsantos@gmail.com>
2021-03-28 14:47:57 +0100

    [Go] Add "Darts"

---
+++ go/darts/darts.go
@@ -0,0 +1,28 @@
+package darts
+
+import "math"
+
+func insideCircle(x, y, radius float64) bool {
+	return (math.Pow(x, 2) + math.Pow(y, 2)) <= math.Pow(radius, 2)
+}
+
+type RadiusPoints struct {
+	Radius float64
+	Points int
+}
+
+var pontuationTable = []RadiusPoints{
+	{Radius: 1, Points: 10},
+	{Radius: 5, Points: 5},
+	{Radius: 10, Points: 1},
+	{Radius: math.Inf(1), Points: 0},
+}
+
+func Score(x, y float64) int {
+	for _, pEntry := range pontuationTable {
+		if insideCircle(x, y, pEntry.Radius) {
+			return pEntry.Points
+		}
+	}
+	return 0
+}
