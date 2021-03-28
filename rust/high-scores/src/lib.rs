0a9d80aabd8856a8a4dd7a4d44310ad6343a5678
André Santos <andrerfcsantos@gmail.com>
2021-03-28 00:53:58 +0000

    [Rust] Improve "High Scores" to be more idiomatic

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 #[derive(Debug)]
 pub struct HighScores {
     scores: Vec<u32>,
@@ -5,11 +7,7 @@ pub struct HighScores {
 
 impl HighScores {
     pub fn new(scores: &[u32]) -> Self {
-        let mut s: Vec<u32> = Vec::new();
-        for score in scores {
-            s.push(*score);
-        }
-        HighScores { scores: s }
+        HighScores { scores: scores.to_vec() }
     }
 
     pub fn scores(&self) -> &[u32] {
@@ -17,36 +15,24 @@ impl HighScores {
     }
 
     pub fn latest(&self) -> Option<u32> {
-        let size = self.scores.len();
-        if size == 0 {
-            return None;
+        match self.scores.len(){
+            0 => None,
+            _ => Some(self.scores[self.scores.len() - 1])
         }
-        return Some(self.scores[size - 1]);
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        let size = self.scores.len();
-        if size == 0 {
-            return None;
+        match self.scores.len() {
+            0 => None,
+            _ => Some(*self.scores.iter().max().unwrap_or(&0))
         }
-
-        let mut greatest = self.scores[0];
-        for score in &self.scores {
-            if *score > greatest {
-                greatest = *score;
-            }
-        }
-        return Some(greatest);
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
+        let take = cmp::min(self.scores.len(), 3);
         let mut new_vec = self.scores.clone();
-        let size = new_vec.len();
-        let mut take = 3;
-        if size < 3 {
-            take = size;
-        }
-        new_vec.sort_by(|a, b| b.cmp(a));
-        return new_vec.split_at(take).0.into();
+        new_vec.sort();
+        new_vec.reverse();
+        new_vec.split_at(take).0.into()
     }
 }
