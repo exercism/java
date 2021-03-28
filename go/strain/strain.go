50ad386b93f15ad86db5f40c22458797cc4c4ada
André Santos <andrerfcsantos@gmail.com>
2021-03-29 00:03:04 +0100

    [Go] Add "Strain"

---
+++ go/strain/strain.go
@@ -0,0 +1,67 @@
+package strain
+
+type Lists []Ints
+
+func (ls Lists) Keep(predicate func([]int) bool) Lists {
+	res := Lists{}
+	for _, list := range ls {
+		if predicate(list) {
+			res = append(res, list)
+		}
+	}
+	return res
+}
+
+func (ls Lists) Discard(predicate func([]int) bool) Lists {
+	res := Lists{}
+	for _, list := range ls {
+		if !predicate(list) {
+			res = append(res, list)
+		}
+	}
+	return res
+}
+
+type Ints []int
+
+func (l Ints) Keep(predicate func(x int) bool) Ints {
+	var res Ints
+	for _, elem := range l {
+		if predicate(elem) {
+			res = append(res, elem)
+		}
+	}
+	return res
+}
+
+func (l Ints) Discard(predicate func(int) bool) Ints {
+	var res Ints
+	for _, elem := range l {
+		if !predicate(elem) {
+			res = append(res, elem)
+		}
+	}
+	return res
+}
+
+type Strings []string
+
+func (l Strings) Keep(predicate func(string) bool) Strings {
+	res := Strings{}
+	for _, elem := range l {
+		if predicate(elem) {
+			res = append(res, elem)
+		}
+	}
+	return res
+}
+
+func (l Strings) Discard(predicate func(string) bool) Strings {
+	res := Strings{}
+	for _, elem := range l {
+		if !predicate(elem) {
+			res = append(res, elem)
+		}
+	}
+	return res
+}
