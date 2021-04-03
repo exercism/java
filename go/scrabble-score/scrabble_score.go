14ce830634dd4d674cc9965806d577796f15e090
André Santos <andrerfcsantos@gmail.com>
2021-04-03 18:34:01 +0100

    [Go] Update "Scrabble Score"

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,22 @@
+package scrabble
+
+import "strings"
+
+var letterScore = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
+}
+
+// Score gives the scrabble score for a word
+func Score(word string) int {
+	res := 0
+	for _, r := range strings.ToUpper(word) {
+		res += letterScore[r]
+	}
+	return res
+}
