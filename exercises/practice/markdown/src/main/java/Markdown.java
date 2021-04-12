128ecdcc83d8dbf48c176f4cd5fc5a8515b708f2
mirko <mirko.perillo@gmail.com>
2021-04-12 21:24:52 +0200

    fix #1844 restore original exercise starting solution

--- exercises/practice/markdown/src/main/java/Markdown.java
+++ exercises/practice/markdown/src/main/java/Markdown.java
@@ -1,10 +1,79 @@
-/*
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    String parse(String markdown) {
+        String[] lines = markdown.split("\n");
+        String result = "";
+        boolean activeList = false;
 
-Please remove this comment when submitting your solution.
+        for (int i = 0; i < lines.length; i++) {
 
-*/
+            String theLine = parseHeader(lines[i]);
+
+            if (theLine == null) {
+              theLine = parseListItem(lines[i]);
+            }
+
+            if (theLine == null) 
+            {
+                theLine = parseParagraph(lines[i]);
+            }
+
+            if (theLine.matches("(<li>).*") && !theLine.matches("(<h).*") && !theLine.matches("(<p>).*") && !activeList) {
+                activeList = true;
+              result = result + "<ul>";
+                result = result + theLine;
+            } 
+
+            else if (!theLine.matches("(<li>).*") && activeList) {
+                activeList = false;
+                result = result + "</ul>";
+                result = result + theLine;
+            } else {
+              result = result + theLine;
+            }
+        }
+
+        if (activeList) {
+            result = result + "</ul>";
+        }
+
+        return result;
+    }
+
+    private String parseHeader(String markdown) {
+        int count = 0;
+
+        for (int i = 0; i < markdown.length() && markdown.charAt(i) == '#'; i++) 
+        {
+            count++;
+        }
+
+        if (count == 0) { return null; }
+
+        return "<h" + Integer.toString(count) + ">" + markdown.substring(count + 1) + "</h" + Integer.toString(count)+ ">";
+    }
+
+    private String parseListItem(String markdown) {
+        if (markdown.startsWith("*")) {
+            String skipAsterisk = markdown.substring(2);
+            String listItemString = parseSomeSymbols(skipAsterisk);
+            return "<li>" + listItemString + "</li>";
+        }
+
+        return null;
+    }
+
+    private String parseParagraph(String markdown) {
+        return "<p>" + parseSomeSymbols(markdown) + "</p>";
+    }
+
+    private String parseSomeSymbols(String markdown) {
+
+        String lookingFor = "__(.+)__";
+        String update = "<strong>$1</strong>";
+        String workingOn = markdown.replaceAll(lookingFor, update);
+
+        lookingFor = "_(.+)_";
+        update = "<em>$1</em>";
+        return workingOn.replaceAll(lookingFor, update);
+    }
+} 
