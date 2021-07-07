2cc7fd19593062856128ea00fa558c46fe302ea1
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-07 14:13:32 +0200

    Simplify the `hello-world` exercise's stub. The goal of this is to make things easier for students to get started and to introduce as little syntax as possible. (#1950)

--- exercises/practice/hello-world/src/main/java/Greeter.java
+++ exercises/practice/hello-world/src/main/java/Greeter.java
@@ -1,7 +1,7 @@
 class Greeter {
 
     String getGreeting() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return "Goodbye, Mars!";
     }
 
 }
