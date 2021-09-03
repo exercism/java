3a69356840c27023d9b4a0b08627b66ca2978f5b
Uzi Landsmann <uzi.landsmann@gmail.com>
2021-09-03 06:57:40 +0200

    Correct parameter types and names in smallHand() (#1979)

--- exercises/concept/blackjack/src/main/java/Blackjack.java
+++ exercises/concept/blackjack/src/main/java/Blackjack.java
@@ -12,14 +12,14 @@ public class Blackjack{
         throw new UnsupportedOperationException("Please implement the Blackjack.largeHand method");
     }
 
-    public String smallHand(handScore int, dealerScore int) {
+    public String smallHand(int handScore, int dealerScore) {
         throw new UnsupportedOperationException("Please implement the Blackjack.smallHand method");
     }
 
     // FirstTurn returns the semi-optimal decision for the first turn, given the cards of the player and the dealer.
     // This function is already implemented and does not need to be edited. It pulls the other functions together in a
     // complete decision tree for the first turn.
-    public String  firstTurn(String card1, String card2, String dealerCard) {
+    public String firstTurn(String card1, String card2, String dealerCard) {
         int handScore = parseCard(card1) + parseCard(card2);
         int dealerScore = parseCard(dealerCard);
 
