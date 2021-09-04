30ff7755c73ef95734a9203059d861bebe28088d
Tom Troyer <tom.troyer@gmail.com>
2021-09-04 19:49:18 -0400

    Introduce assertj in resistor-color exercise  (#1984)
    * Changing ResistorColor unit tests to use AssertJ.
    
    * Adjusting assertj assertion to be more readable.
    
    * Fixing style check violation.

--- exercises/practice/resistor-color/src/test/java/ResistorColorTest.java
+++ exercises/practice/resistor-color/src/test/java/ResistorColorTest.java
@@ -1,9 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Ignore;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResistorColorTest {
 
@@ -16,36 +14,26 @@ public class ResistorColorTest {
 
     @Test
     public void testBlackColorCode() {
-        String input = "black";
-        int expected = 0;
-
-        assertEquals(expected, resistorColor.colorCode(input));
+        assertThat(resistorColor.colorCode("black")).isEqualTo(0);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testWhiteColorCode() {
-        String input = "white";
-        int expected = 9;
-
-        assertEquals(expected, resistorColor.colorCode(input));
+        assertThat(resistorColor.colorCode("white")).isEqualTo(9);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testOrangeColorCode() {
-        String input = "orange";
-        int expected = 3;
-
-        assertEquals(expected, resistorColor.colorCode(input));
+        assertThat(resistorColor.colorCode("orange")).isEqualTo(3);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testColors() {
-        String[] expected = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
-
-        assertArrayEquals(expected, resistorColor.colors());
+        assertThat(resistorColor.colors()).containsExactly(
+                "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"
+        );
     }
-    
 }
