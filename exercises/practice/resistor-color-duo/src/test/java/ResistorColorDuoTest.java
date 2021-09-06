a624e32d3945c8a4c0401122f02ec0e04158a6e0
Tom Troyer <tom.troyer@gmail.com>
2021-09-05 21:45:17 -0400

    Updating ResistorColorDuo tests to use AssertJ (#1999)
    * Updating ResistorColorDuo tests to use AssertJ
    
    * Removing unused import.

--- exercises/practice/resistor-color-duo/src/test/java/ResistorColorDuoTest.java
+++ exercises/practice/resistor-color-duo/src/test/java/ResistorColorDuoTest.java
@@ -2,7 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.Ignore;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResistorColorDuoTest {
     private ResistorColorDuo resistorColorDuo;
@@ -14,50 +14,40 @@ public class ResistorColorDuoTest {
 
     @Test
     public void testBrownAndBlack() {
-        String[] input = { "brown", "black" };
-        int expected = 10;
-        int actual = resistorColorDuo.value(input);
-
-        assertEquals(expected, actual);
+        assertThat(
+                resistorColorDuo.value(new String[]{"brown", "black"})
+        ).isEqualTo(10);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testBlueAndGrey() {
-        String[] input = { "blue", "grey" };
-        int expected = 68;
-        int actual = resistorColorDuo.value(input);
-
-        assertEquals(expected, actual);
+        assertThat(
+                resistorColorDuo.value(new String[]{ "blue", "grey" })
+        ).isEqualTo(68);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testYellowAndViolet() {
-        String[] input = { "yellow", "violet" };
-        int expected = 47;
-        int actual = resistorColorDuo.value(input);
-
-        assertEquals(expected, actual);
+        assertThat(resistorColorDuo.value(
+                new String[]{ "yellow", "violet" })
+        ).isEqualTo(47);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testOrangeAndOrange() {
-        String[] input = { "orange", "orange" };
-        int expected = 33;
-        int actual = resistorColorDuo.value(input);
-
-        assertEquals(expected, actual);
+        assertThat(
+                resistorColorDuo.value(new String[]{ "orange", "orange" })
+        ).isEqualTo(33);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testIgnoreAdditionalColors() {
-        String[] input = { "green", "brown", "orange" };
-        int expected = 51;
-        int actual = resistorColorDuo.value(input);
-
-        assertEquals(expected, actual);
+        assertThat(
+                resistorColorDuo.value(new String[]{ "green", "brown", "orange" })
+        ).isEqualTo(51);
     }
 }
