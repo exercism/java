584bc31bd29299d4460bbc0bb5a3586361572d35
Jason Runkle <jmrunkle@gmail.com>
2021-09-08 04:14:46 +0000

    Begin setting up the language-list exercise

---
+++ exercises/concept/language-list/.docs/instructions.md
@@ -0,0 +1,13 @@
+## 1. Define a function to check if the language list is empty
+
+## 2. Define a function to add a language to the list
+
+## 3. Define a function to remove a language from the list
+
+## 4. Define a function to return the first item in the list
+
+## 5. Define a function to return how many languages are in the list
+
+## 6. Define a function to determine if a language is in the list
+
+## 7. Define a function to determine if the list is exciting
---
+++ exercises/concept/language-list/src/main/java/LanguageList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LanguageList {
+    private final List<String> languages = new ArrayList<>();
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Please implement the isEmpty() method");
+    }
+
+    public void addLanguage(String language) {
+        throw new UnsupportedOperationException("Please implement the addLanguage() method");
+    }
+
+    public void removeLanguage(String language) {
+        throw new UnsupportedOperationException("Please implement the removeLanguage() method");
+    }
+
+    public String firstLanguage() {
+        throw new UnsupportedOperationException("Please implement the firstLanguage() method");
+    }
+
+    public int count() {
+        throw new UnsupportedOperationException("Please implement the count() method");
+    }
+
+    public boolean containsLanguage(String language) {
+        throw new UnsupportedOperationException("Please implement the isExciting() method");
+    }
+
+    public boolean isExciting() {
+        throw new UnsupportedOperationException("Please implement the isExciting() method");
+    }
+}
