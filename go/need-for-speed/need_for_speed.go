152bf88628129a17ac592acecf41705d3f8941dd
André Santos <andrerfcsantos@gmail.com>
2021-09-14 21:17:30 +0100

    [Go|Concepts] Add "Need for Speed"

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,45 @@
+package speed
+
+// Car implements a remote controlled car.
+type Car struct {
+	speed        int
+	batteryDrain int
+	battery      int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		speed:        speed,
+		batteryDrain: batteryDrain,
+		battery:      100,
+	}
+}
+
+// Track implements a race track.
+type Track struct {
+	distance int
+}
+
+// NewTrack created a new track
+func NewTrack(distance int) Track {
+	return Track{
+		distance: distance,
+	}
+}
+
+// Drive drives the car one time. If there is not enough battry to drive on more time,
+// the car will not move but use the leftover battery.
+func Drive(car Car) Car {
+	if car.battery-car.batteryDrain >= 0 {
+		car.distance += car.speed
+		car.battery -= car.batteryDrain
+	}
+	return car
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	return (track.distance/car.speed)*car.batteryDrain < car.battery
+}
