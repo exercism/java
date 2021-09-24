c62b4fffaf5ea59fac62a6477fc23c08172df140
kkyb123 <kouokamkuisu123@gmail.com>
2021-09-24 03:26:19 +0100

    #1743 | Update starter implementation

--- exercises/practice/connect/src/main/java/Connect.java
+++ exercises/practice/connect/src/main/java/Connect.java
@@ -1,6 +1,11 @@
 class Connect {
+
+    public Connect(String[] board) {
+        throw new UnsupportedOperationException("Implement this function");
+    }
+
     public Winner computeWinner(String[] board) {
-        throw new UnsupportedOperationException("Implement this fucntion");
+        throw new UnsupportedOperationException("Implement this function");
     }
 
     enum Winner {
