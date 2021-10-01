84c109aec71ca08eb0c9ee6da30224632e6e43b2
myucesan <my96@live.nl>
2021-10-01 15:11:34 +0200

    Renamed QuestLogic to correct class

--- exercises/concept/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ exercises/concept/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,17 +1,17 @@
 class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) QuestLogic.canFastAttack() method");
+        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFastAttack() method");
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) QuestLogic.canSpy() method");
+        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSpy() method");
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) QuestLogic.canSignalPrisoner() method");
+        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        throw new UnsupportedOperationException("Please implement the (static) QuestLogic.canFreePrisoner() method");
+        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
     }
 }
