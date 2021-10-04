a65037956d029c302dfaecf62946dcdd0b2d6f7a
André Santos <andrerfcsantos@gmail.com>
2021-10-04 12:49:37 +0100

    [Go] Add "isbn verifier"

---
+++ go/isbn-verifier/isbn_verifier.go
@@ -0,0 +1,28 @@
+package isbn
+
+import "unicode"
+
+func IsValidISBN(isbn string) bool {
+	ndigits, checksum := 0, 0
+
+	for _, ch := range isbn {
+
+		multiplier := 10 - ndigits
+
+		switch {
+		case unicode.IsDigit(ch):
+			number := int(ch - '0')
+			checksum += number * multiplier
+			ndigits++
+		case ch == 'X':
+			if ndigits != 9 {
+				return false
+			}
+			checksum += 10 * multiplier
+			ndigits++
+		}
+
+	}
+
+	return checksum%11 == 0 && ndigits == 10
+}
