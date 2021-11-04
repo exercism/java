86955119dd5d358ef2e6d95987761fb82daf2524
Leonardo Kasperavičius <leocck@gmail.com>
2021-11-04 16:37:04 +0100

    Group unused methods with region

--- exercises/concept/lasagna/src/test/java/utils/ReflectionProxy.java
+++ exercises/concept/lasagna/src/test/java/utils/ReflectionProxy.java
@@ -46,6 +46,136 @@ public abstract class ReflectionProxy {
     }
 
     /**
+     * Gets the target class
+     * @return The target class if it exists, null otherwise
+     */
+    public Class<?> getTargetClass() {
+        try {
+            return forName(this.getTargetClassName());
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Checks if the target class has a specific method
+     * @param name The name of the method to find
+     * @param parameterTypes The list of parameter types
+     * @return True if the method is found, false otherwise
+     */
+    public boolean hasMethod(String name, Class<?>... parameterTypes) {
+        Class<?> targetClass = getTargetClass();
+        if (targetClass == null || name == null) {
+            return false;
+        }
+        try {
+            Method m = targetClass.getDeclaredMethod(name, parameterTypes);
+            return m != null;
+        } catch (NoSuchMethodException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if a method from the target class is public
+     * @param name The name of the method
+     * @param parameterTypes A list of method parameters
+     * @return True if the method exists and is public, false otherwise
+     */
+    public boolean isMethodPublic(String name, Class<?>... parameterTypes) {
+        Class<?> targetClass = getTargetClass();
+        if (targetClass == null || name == null) {
+            return false;
+        }
+        try {
+            Method m = targetClass.getDeclaredMethod(name, parameterTypes);
+            return Modifier.isPublic(m.getModifiers());
+        } catch (NoSuchMethodException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if a method from the target class returns the correct type
+     * @param returnType The type of return value
+     * @param name The name of the method
+     * @param parameterTypes The list of method parameters
+     * @return
+     */
+    public boolean isMethodReturnType(Class<?> returnType, String name, Class<?>... parameterTypes) {
+        return isMethodReturnType(returnType, null, name, parameterTypes);
+    }
+
+    /**
+     * Invokes a method from the target instance
+     * @param methodName The name of the method
+     * @param parameterTypes The list of parameter types
+     * @param parameterValues The list with values for the method parameters
+     * @param <T> The result type we expect the method to be
+     * @return The value returned by the method
+     */
+    protected  <T> T invokeMethod(String methodName, Class<?>[] parameterTypes, Object... parameterValues) {
+        if (target == null) {
+            return null;
+        }
+        try {
+            // getDeclaredMethod is used to get protected/private methods
+            Method method = target.getClass().getDeclaredMethod(methodName, parameterTypes);
+            method.setAccessible(true);
+            return (T) method.invoke(target, parameterValues);
+        } catch (NoSuchMethodException e) {
+            try {
+                // try getting it from parent class, but only public methods will work
+                Method method = target.getClass().getMethod(methodName, parameterTypes);
+                method.setAccessible(true);
+                return (T) method.invoke(target, parameterValues);
+            } catch (Exception ex) {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * Creates an instance of the target class
+     * @param args The list of constructor parameters
+     * @return An instance of the target class, if found, or null otherwise
+     */
+    private Object instantiateTarget(Object... args) {
+        Class<?> targetClass = getTargetClass();
+        if (targetClass == null) {
+            return null;
+        }
+        Constructor<?>[] constructors = getAllConstructors();
+        for (Constructor<?> c : constructors) {
+            if (c.getParameterCount() == args.length) {
+                try {
+                    return c.newInstance(args);
+                } catch (Exception e) {
+                    // do nothing;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Gets a list with all the constructors defined by the target class
+     * @return A list with all constructor definitions
+     */
+    private Constructor<?>[] getAllConstructors() {
+        Class<?> targetClass = getTargetClass();
+        if(targetClass == null) {
+            return new Constructor<?>[]{};
+        }
+        return targetClass.getConstructors();
+    }
+
+
+    //region Unused - Left for future evaluation
+
+    /**
      * Checks if the target class exists
      * @return True if the class exists, false otherwise
      */
@@ -144,36 +274,6 @@ public abstract class ReflectionProxy {
     }
 
     /**
-     * Checks if the target class has a specific method
-     * @param name The name of the method to find
-     * @param parameterTypes The list of parameter types
-     * @return True if the method is found, false otherwise
-     */
-    public boolean hasMethod(String name, Class<?>... parameterTypes) {
-        Class<?> targetClass = getTargetClass();
-        if (targetClass == null || name == null) {
-            return false;
-        }
-        try {
-            Method m = targetClass.getDeclaredMethod(name, parameterTypes);
-            return m != null;
-        } catch (NoSuchMethodException e) {
-            return false;
-        }
-    }
-
-    /**
-     * Checks if a method from the target class returns the correct type
-     * @param returnType The type of return value
-     * @param name The name of the method
-     * @param parameterTypes The list of method parameters
-     * @return
-     */
-    public boolean isMethodReturnType(Class<?> returnType, String name, Class<?>... parameterTypes) {
-        return isMethodReturnType(returnType, null, name, parameterTypes);
-    }
-
-    /**
      * Checks if a method from the target class returns a correct parameterized collection (Generics)
      * @param returnType The return type we expect (eg. List)
      * @param parameterizedType The parameterized type we expect (eg. String)
@@ -199,25 +299,6 @@ public abstract class ReflectionProxy {
             }
             ParameterizedType pType = (ParameterizedType)m.getGenericReturnType();
             return pType.getActualTypeArguments()[0].equals(parameterizedType);
-        } catch (NoSuchMethodException e) {
-            return false;
-        }
-    }
-
-    /**
-     * Checks if a method from the target class is public
-     * @param name The name of the method
-     * @param parameterTypes A list of method parameters
-     * @return True if the method exists and is public, false otherwise
-     */
-    public boolean isMethodPublic(String name, Class<?>... parameterTypes) {
-        Class<?> targetClass = getTargetClass();
-        if (targetClass == null || name == null) {
-            return false;
-        }
-        try {
-            Method m = targetClass.getDeclaredMethod(name, parameterTypes);
-            return Modifier.isPublic(m.getModifiers());
         } catch (NoSuchMethodException e) {
             return false;
         }
@@ -322,84 +403,6 @@ public abstract class ReflectionProxy {
     }
 
     /**
-     * Invokes a method from the target instance
-     * @param methodName The name of the method
-     * @param parameterTypes The list of parameter types
-     * @param parameterValues The list with values for the method parameters
-     * @param <T> The result type we expect the method to be
-     * @return The value returned by the method
-     */
-    protected  <T> T invokeMethod(String methodName, Class<?>[] parameterTypes, Object... parameterValues) {
-        if (target == null) {
-            return null;
-        }
-        try {
-            // getDeclaredMethod is used to get protected/private methods
-            Method method = target.getClass().getDeclaredMethod(methodName, parameterTypes);
-            method.setAccessible(true);
-            return (T) method.invoke(target, parameterValues);
-        } catch (NoSuchMethodException e) {
-            try {
-                // try getting it from parent class, but only public methods will work
-                Method method = target.getClass().getMethod(methodName, parameterTypes);
-                method.setAccessible(true);
-                return (T) method.invoke(target, parameterValues);
-            } catch (Exception ex) {
-                return null;
-            }
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    /**
-     * Gets the target class
-     * @return The target class if it exists, null otherwise
-     */
-    public Class<?> getTargetClass() {
-        try {
-            return forName(this.getTargetClassName());
-        } catch (ClassNotFoundException e) {
-            return null;
-        }
-    }
-
-    /**
-     * Gets a list with all the constructors defined by the target class
-     * @return A list with all constructor definitions
-     */
-    private Constructor<?>[] getAllConstructors() {
-        Class<?> targetClass = getTargetClass();
-        if(targetClass == null) {
-            return new Constructor<?>[]{};
-        }
-        return targetClass.getConstructors();
-    }
-
-    /**
-     * Creates an instance of the target class
-     * @param args The list of constructor parameters
-     * @return An instance of the target class, if found, or null otherwise
-     */
-    private Object instantiateTarget(Object... args) {
-        Class<?> targetClass = getTargetClass();
-        if (targetClass == null) {
-            return null;
-        }
-        Constructor<?>[] constructors = getAllConstructors();
-        for (Constructor<?> c : constructors) {
-            if (c.getParameterCount() == args.length) {
-                try {
-                    return c.newInstance(args);
-                } catch (Exception e) {
-                    // do nothing;
-                }
-            }
-        }
-        return null;
-    }
-
-    /**
      * Checks if the target class is abstract
      * @return True if the target class exists and is abstract, false otherwise
      */
@@ -478,4 +481,6 @@ public abstract class ReflectionProxy {
             return false;
         }
     }
+
+    //endregion
 }
