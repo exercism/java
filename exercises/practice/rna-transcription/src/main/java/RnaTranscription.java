d86871e5b16fd58a380ad01fed7e5c99d8b303b7
PoorviIPatil <69724173+PoorviIPatil@users.noreply.github.com>
2021-12-14 12:06:11 +0530

    Update RnaTranscription.java

--- exercises/practice/rna-transcription/src/main/java/RnaTranscription.java
+++ exercises/practice/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,32 @@
-class RnaTranscription {
+//class RnaTranscription {
+    
+import java.util.*;
+public class RnaTranscription {
+    private static int nucleotideMap(int c){
+        switch (c){
+            case 'G' : return 'C';
+            case 'C' : return 'G';
+            case 'T' : return 'A';
+            case 'A' : return 'U';
+        }
+        throw new IllegalArgumentException("Invalid input");
+    }
+    public String transcribe(String dnaStrand) {
+        return dnaStrand
+            .codePoints()
+            .map(Character::toUpperCase)
+            .map(RnaTranscription::nucleotideMap)
+            .collect(StringBuilder::new,
+                     StringBuilder::appendCodePoint,
+                     StringBuilder::append)
+            .toString();
+    }
+}
 
+    /*
     String transcribe(String dnaStrand) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
+    */
 
-}
+//}
