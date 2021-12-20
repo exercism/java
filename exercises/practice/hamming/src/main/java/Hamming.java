ef01a5aadda11782257a22797da829be26a9db3f
yenugukeerthana <96348559+yenugukeerthana@users.noreply.github.com>
2021-12-20 16:40:33 +0530

    Update Hamming.java

--- exercises/practice/hamming/src/main/java/Hamming.java
+++ exercises/practice/hamming/src/main/java/Hamming.java
@@ -1,9 +1,29 @@
-public class Hamming {
-    public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+import java.util.stream.IntStream;
+class Hamming {
+    private final String leftStrand;
+    private final String rightStrand;
+    Hamming(String leftStrand, String rightStrand) {
+        if(leftStrand.length() > rightStrand.length() && rightStrand.length() != 0) {   //check sequence lengths to make sure they match
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        if(leftStrand.length() < rightStrand.length() && leftStrand.length() != 0) {   //check sequence lengths to make sure they match
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        if(leftStrand.isEmpty() && rightStrand.length() != 0) {   //check sequence lengths to make sure they match
+            throw new IllegalArgumentException("left strand must not be empty.");
+        }
+        if(rightStrand.isEmpty() && leftStrand.length() != 0) {   //check sequence lengths to make sure they match
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
     }
-
-    public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    long getHammingDistance() {
+        long hammingDis = IntStream
+            .range(0, leftStrand.length())
+            .filter(i -> leftStrand.charAt(i) != rightStrand.charAt(i))
+            .count();
+            
+        return hammingDis;
     }
 }
