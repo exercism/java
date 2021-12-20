4282f75ace3bf246e8dddc222b62aaad55e9704e
yenugukeerthana <96348559+yenugukeerthana@users.noreply.github.com>
2021-12-20 11:08:20 +0530

    Update AnnalynsInfiltration.java

--- exercises/concept/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ exercises/concept/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,17 +1,14 @@
 class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFastAttack() method");
+        return !knightIsAwake;
     }
-
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSpy() method");
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
-
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
+        return prisonerIsAwake && !archerIsAwake;
     }
-
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+        return (prisonerIsAwake && !knightIsAwake && !archerIsAwake) || (!archerIsAwake && petDogIsPresent);
     }
 }
