2de31fdfc2b95ae21b71ed22760db886a4823b84
André Santos <andrerfcsantos@gmail.com>
2022-06-16 16:25:47 +0100

    [Go] Improve "Robot Name"

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -2,10 +2,8 @@ package robotname
 
 import (
 	"errors"
-	"math/rand"
+	"fmt"
 )
-
-var generatedNames = map[string]bool{}
 
 var MAX_NAMES = 26 * 26 * 10 * 10 * 10
 
@@ -13,13 +11,34 @@ type Robot struct {
 	name string
 }
 
+// total contains the total of robots created so far
+var total int
+
+// alpha controls the generation of the 2 letters of the robot name
+var alpha int
+
+// num controls the generation of the 3 digits of the robot name
+var num int = -1
+
 func (r *Robot) Name() (string, error) {
-	if len(generatedNames) == MAX_NAMES {
+	if total == MAX_NAMES {
 		return "", errors.New("max amount of names reached")
 	}
 
 	if r.name == "" {
-		r.name = newUniqueName()
+		num += 1
+		total += 1
+
+		a1, a2 := alpha/26, alpha%26
+		if num%1000 == 0 {
+			// last number generated ended in 999
+			// reset num back to 0 and
+			// increase alpha to move on to the next set of letters
+			num = 0
+			alpha++
+		}
+
+		r.name = fmt.Sprintf("%c%c%03d", 'A'+a1, 'A'+a2, num)
 	}
 
 	return r.name, nil
@@ -28,28 +47,3 @@ func (r *Robot) Name() (string, error) {
 func (r *Robot) Reset() {
 	r.name = ""
 }
-
-var letterRunes = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-var numberRunes = []rune("0123456789")
-
-func newUniqueName() string {
-	name := newName()
-
-	for generatedNames[name] {
-		name = newName()
-	}
-	generatedNames[name] = true
-	return name
-}
-
-func newName() string {
-	b := []rune{
-		letterRunes[rand.Intn(len(letterRunes))],
-		letterRunes[rand.Intn(len(letterRunes))],
-		numberRunes[rand.Intn(len(numberRunes))],
-		numberRunes[rand.Intn(len(numberRunes))],
-		numberRunes[rand.Intn(len(numberRunes))],
-	}
-	return string(b)
-}
-
