cbcb48a69f61f772d69395c5ea5b57c5f1a09b8f
Emilio Lopez Gabeiras <emiliolg@mac.com>
2022-06-20 13:07:36 -0300

    Add a test to check conversion to camel case when the character after the dash is not a letter

--- exercises/concept/squeaky-clean/.meta/src/reference/java/SqueakyClean.java
+++ exercises/concept/squeaky-clean/.meta/src/reference/java/SqueakyClean.java
@@ -1,20 +1,25 @@
 class SqueakyClean {
     static String clean(String identifier) {
         final StringBuilder cleanIdentifier = new StringBuilder();
+        boolean kebab = false;
         for (int i = 0; i < identifier.length(); i++) {
             final char ch = identifier.charAt(i);
             if (Character.isSpaceChar(ch)) {
                 cleanIdentifier.append("_");
             } else if (Character.isISOControl(ch)) {
                 cleanIdentifier.append("CTRL");
-            } else if (ch == '-' && i < identifier.length() + 1) {
-                i++;
-                cleanIdentifier.append(Character.toUpperCase(identifier.charAt(i)));
-            } else if (ch >= 'α' && ch <= 'ω') {
-            } else if (Character.isAlphabetic(ch)) {
-                cleanIdentifier.append(ch);
+            } else if (ch == '-') {
+                kebab = true;
+            } else if (isLetter(ch)) {
+                cleanIdentifier.append(
+                        kebab ? Character.toUpperCase(ch) : ch
+                );
+                kebab = false;
             }
         }
         return cleanIdentifier.toString();
+    }
+    private static boolean isLetter(char ch) {
+        return Character.isLetter(ch) && !(ch >= 'α' && ch <= 'ω');
     }
 }
