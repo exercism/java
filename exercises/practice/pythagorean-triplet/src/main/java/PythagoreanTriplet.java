93b20d04a81f711f108e21ea4e2cf82d9f2815a7
Gunjan Agarwal <68350045+gunjan2405@users.noreply.github.com>
2022-07-12 17:46:41 +0530

    Update PythagoreanTriplet.java

--- exercises/practice/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ exercises/practice/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,10 +1,48 @@
-/*
-
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
+import java.util.*;
+import java.math.*;
+class PythagoreanTriplet{
+    private int a;
+    private int b;
+    private int c;
+    PythagoreanTriplet(int a,int b,int c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    @Override
+    public boolean equals(Object object) {
+        PythagoreanTriplet other = (PythagoreanTriplet) object;
+        return a == other.a && b == other.b && c == other.c;
+    }
+    static PythagoreanTripletBuilder makeTripletsList() {
+        return new PythagoreanTripletBuilder();
+    }
+}
+class PythagoreanTripletBuilder{
+    private int sum;
+    private int maximum;
+    PythagoreanTripletBuilder  thatSumTo(int value){
+        this.sum = value;
+        return this;
+    }
+    
+    PythagoreanTripletBuilder withFactorsLessThanOrEqualTo(int value){
+        this.maximum = value;
+        return this;
+    }
+    List<PythagoreanTriplet> build() {
+        List<PythagoreanTriplet> triples = new ArrayList<PythagoreanTriplet>(); 
+        for(int a=1;a<=maximum/2;a++){
+            for(int b=a+1;b<=(maximum-a)/2;b++){
+                if(b!=a){
+                    int c = sum - a - b;
+                        if(Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2)){
+                            triples.add(new PythagoreanTriplet(a, b, c));
+                        }
+                    }
+                        
+            }
+        }
+        return triples;
+    }
+}
