08adf38d651bc7f44a3744d016b0be5c42e5a6e1
Jenny Lian <jennylian100@gmail.com>
2022-10-16 12:31:30 -0700

    add assertJ to haming (#2178)

--- exercises/practice/hamming/src/test/java/HammingTest.java
+++ exercises/practice/hamming/src/test/java/HammingTest.java
@@ -1,5 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -38,49 +38,33 @@ public class HammingTest {
     @Ignore("Remove to run test")
     @Test
     public void testValidatesFirstStrandNotLonger() {
-        IllegalArgumentException expected =
-            assertThrows(
-                IllegalArgumentException.class,
-                () -> new Hamming("AATG", "AAA"));
-
-        assertThat(expected)
-            .hasMessage("leftStrand and rightStrand must be of equal length.");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Hamming("AATG", "AAA"))
+                .withMessage("leftStrand and rightStrand must be of equal length.");
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testValidatesSecondStrandNotLonger() {
-        IllegalArgumentException expected =
-            assertThrows(
-                IllegalArgumentException.class,
-                () -> new Hamming("ATA", "AGTG"));
-
-        assertThat(expected)
-            .hasMessage("leftStrand and rightStrand must be of equal length.");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Hamming("ATA", "AGTG"))
+                .withMessage("leftStrand and rightStrand must be of equal length.");
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testDisallowLeftEmptyStrand() {
-        IllegalArgumentException expected =
-            assertThrows(
-                IllegalArgumentException.class,
-                () -> new Hamming("", "G"));
-
-        assertThat(expected)
-            .hasMessage("left strand must not be empty.");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Hamming("", "G"))
+                .withMessage("left strand must not be empty.");
     }
 
     @Ignore("Remove to run test")
     @Test
     public void testDisallowRightEmptyStrand() {
-        IllegalArgumentException expected =
-            assertThrows(
-                IllegalArgumentException.class,
-                () -> new Hamming("G", ""));
-
-        assertThat(expected)
-            .hasMessage("right strand must not be empty.");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Hamming("G", ""))
+                .withMessage("right strand must not be empty.");
     }
 
 }
