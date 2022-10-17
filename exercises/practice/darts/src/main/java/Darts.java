e9e5275954630fbcea75f1ffe5a5752a5481f5df
Jenny L <>
2022-10-16 17:43:03 -0700

    just a first step

--- exercises/practice/darts/src/main/java/Darts.java
+++ exercises/practice/darts/src/main/java/Darts.java
@@ -1,10 +1,5 @@
 class Darts {
-
-    Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
-    int score() {
+    static int score(int xOfDart, int yOfDart) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
