647e51d99eef530c5b00b68c49cb559ef7ba2dac
PakkuDon <pakku.pd@gmail.com>
2022-10-24 01:08:18 +1100

    zebra-puzzle: add exercise stub

--- exercises/practice/zebra-puzzle/src/main/java/ZebraPuzzle.java
+++ exercises/practice/zebra-puzzle/src/main/java/ZebraPuzzle.java
@@ -1,10 +1,9 @@
-/*
+class ZebraPuzzle {
+    String getWaterDrinker() {
+        throw new UnsupportedOperationException("Please implement the ZebraPuzzle.getWaterDrinker() method.");
+    }
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
+    String getZebraOwner() {
+        throw new UnsupportedOperationException("Please implement the ZebraPuzzle.getZebraOwner() method.");
+    }
+}
