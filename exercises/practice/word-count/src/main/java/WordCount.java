c4f8dacb223c79f52529334ade529e1faf20ef1f
PakkuDon <pakku.pd@gmail.com>
2022-10-24 00:59:38 +1100

    word-count: add exercise stub

--- exercises/practice/word-count/src/main/java/WordCount.java
+++ exercises/practice/word-count/src/main/java/WordCount.java
@@ -1,10 +1,7 @@
-/*
+import java.util.Map;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
+class WordCount {
+    public Map<String, Integer> phrase(String input) {
+        throw new UnsupportedOperationException("Please implement the WordCount.phrase() method.");
+    }
+}
