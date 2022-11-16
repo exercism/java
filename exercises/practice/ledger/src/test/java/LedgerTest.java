285ac8fd1862d8bc5ac204e61322ef1578cf494f
rik <p3mal88@gmail.com>
2022-11-16 14:38:23 +0100

    Fix policies

--- exercises/practice/ledger/src/test/java/LedgerTest.java
+++ exercises/practice/ledger/src/test/java/LedgerTest.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -16,65 +17,56 @@ public class LedgerTest {
     private Ledger ledger;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         ledger = new Ledger();
     }
 
     @Test
     public void emptyLedgerUS() {
-        // given
         var entries = new Ledger.LedgerEntry[]{};
 
-        // when
         String actual = ledger.format(USD_CURRENCY, US_LOCALE, entries);
 
-        // then
         String expected = "Date       | Description               | Change       ";
         Assert.assertEquals(expected, actual);
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void emptyLedgerNL() {
-        // given
         var entries = new Ledger.LedgerEntry[]{};
 
-        // when
         String actual = ledger.format(EUR_CURRENCY, NL_LOCALE, entries);
 
-        // then
         String expected = "Datum      | Omschrijving              | Verandering  ";
         Assert.assertEquals(expected, actual);
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void oneEntry() {
-        // given
         var entries = new Ledger.LedgerEntry[]{
                 ledger.createLedgerEntry("2015-02-01", "Buy present", -1000)
         };
 
-        // when
         String actual = ledger.format(USD_CURRENCY, US_LOCALE, entries);
 
-        // then
         String expected =
                 "Date       | Description               | Change       \n"
                         + "02/01/2015 | Buy present               |       -$10.00";
         Assert.assertEquals(expected, actual);
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void creditAndDebit() {
-        // given
         var entries = new Ledger.LedgerEntry[]{
                 ledger.createLedgerEntry("2015-01-02", "Get present", 1000),
                 ledger.createLedgerEntry("2015-01-01", "Buy present", -1000)
         };
 
-        // when
         String actual = ledger.format(USD_CURRENCY, US_LOCALE, entries);
 
-        // then
         String expected =
                 """
                         Date       | Description               | Change      \s
@@ -84,18 +76,16 @@ public class LedgerTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void multipleEntriesOnSameDateOrderedByDescription() {
-        // given
         var entries = new Ledger.LedgerEntry[]{
                 ledger.createLedgerEntry("2015-01-01", "Get present", 1000),
                 ledger.createLedgerEntry("2015-01-01", "Buy present", -1000)
         };
 
-        // when
         String actual = ledger.format(USD_CURRENCY, US_LOCALE, entries);
 
-        // then
         String expected =
                 """
                         Date       | Description               | Change      \s
@@ -105,41 +95,36 @@ public class LedgerTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void finalOrderTieBreakerIsChange() {
-        // given
         var entries = new Ledger.LedgerEntry[]{
                 ledger.createLedgerEntry("2015-01-01", "Something", 0),
                 ledger.createLedgerEntry("2015-01-01", "Something", -1),
                 ledger.createLedgerEntry("2015-01-01", "Something", 1)
         };
 
-        // when
         String actual = ledger.format(USD_CURRENCY, US_LOCALE, entries);
 
-        // then
         String expected =
                 """
                         Date       | Description               | Change      \s
                         01/01/2015 | Something                 |        -$0.01
                         01/01/2015 | Something                 |         $0.00
                         01/01/2015 | Something                 |         $0.01""";
-        ;
 
         Assert.assertEquals(expected, actual);
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void overlongDescriptions() {
-        // given
         var entries = new Ledger.LedgerEntry[]{
                 ledger.createLedgerEntry("2015-01-01", "Freude schoner Gotterfunken", -123456)
         };
 
-        // when
         String actual = ledger.format(USD_CURRENCY, US_LOCALE, entries);
 
-        // then
         String expected =
                 "Date       | Description               | Change       \n" +
                         "01/01/2015 | Freude schoner Gotterf... |    -$1,234.56";
@@ -147,17 +132,15 @@ public class LedgerTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void euros() {
-        // given
         var entries = new Ledger.LedgerEntry[]{
                 ledger.createLedgerEntry("2015-01-01", "Buy present", -1000)
         };
 
-        // when
         String actual = ledger.format(EUR_CURRENCY, US_LOCALE, entries);
 
-        // then
         var expected =
                 "Date       | Description               | Change       \n" +
                         "01/01/2015 | Buy present               |       -€10.00";
@@ -165,17 +148,15 @@ public class LedgerTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void dutchLocale() {
-        // given
         var entries = new Ledger.LedgerEntry[]{
                 ledger.createLedgerEntry("2015-03-12", "Buy present", 123456)
         };
 
-        // when
         String actual = ledger.format(USD_CURRENCY, NL_LOCALE, entries);
 
-        // then
         var expected =
                 "Datum      | Omschrijving              | Verandering  \n" +
                         "12/03/2015 | Buy present               |    $ 1.234,56";
@@ -183,17 +164,15 @@ public class LedgerTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void dutchNegativeNumberWith3DigitsBeforeDecimalPoint() {
-        // given
         var entries = new Ledger.LedgerEntry[]{
                 ledger.createLedgerEntry("2015-03-12", "Buy present", -12345)
         };
 
-        // when
         String actual = ledger.format(USD_CURRENCY, NL_LOCALE, entries);
 
-        // then
         var expected =
                 "Datum      | Omschrijving              | Verandering  \n" +
                         "12/03/2015 | Buy present               |     $ -123,45";
@@ -201,19 +180,15 @@ public class LedgerTest {
         Assert.assertEquals(expected, actual);
     }
 
-
+    @Ignore("Remove to run test")
     @Test
     public void americanNegativeNumberWith3DigitsBeforeDecimalPoint() {
-
-        // given
         var entries = new Ledger.LedgerEntry[]{
                 ledger.createLedgerEntry("2015-03-12", "Buy present", -12345)
         };
 
-        // when
         String actual = ledger.format(USD_CURRENCY, US_LOCALE, entries);
 
-        // then
         var expected =
                 "Date       | Description               | Change       \n" +
                         "03/12/2015 | Buy present               |      -$123.45";
@@ -221,31 +196,25 @@ public class LedgerTest {
         Assert.assertEquals(expected, actual);
     }
 
-
+    @Ignore("Remove to run test")
     @Test
     public void givenInvalidLocale_ThenException() {
-        // given
         var entries = new Ledger.LedgerEntry[0];
         var locale = "it-IT";
 
-        // when
         IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> ledger.format(EUR_CURRENCY, locale, entries));
 
-        // then
         assertEquals("Invalid locale", illegalArgumentException.getMessage());
-
     }
 
+    @Ignore("Remove to run test")
     @Test
     public void givenInvalidCurrency_ThenException() {
-        // given
         var entries = new Ledger.LedgerEntry[0];
         var currency = "Pieces o' Eight";
 
-        // when
         IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> ledger.format(currency, US_LOCALE, entries));
 
-        // then
         assertEquals("Invalid currency", illegalArgumentException.getMessage());
     }
 }
