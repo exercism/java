bb8093db974b3e856510e9f2ad5279d47a47f34e
Grigory Stepanov <rio09876@gmail.com>
2023-01-18 17:24:43 +0300

    2133 stub for binary exercise

---
+++ exercises/practice/hexadecimal/src/main/java/Hexadecimal.java
@@ -0,0 +1,6 @@
+public class Hexadecimal {
+
+    public static int toDecimal(String input) {
+        throw new UnsupportedOperationException("Please implement the Hexadecimal.toDecimal() method.");
+    }
+}
