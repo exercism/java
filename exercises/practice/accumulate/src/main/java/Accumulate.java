5721b9ca9ba7c73e627338312d55f7ba3a0bea3e
Grigory Stepanov <rio09876@gmail.com>
2023-01-18 17:24:43 +0300

    2133 stub for binary exercise

---
+++ exercises/practice/accumulate/src/main/java/Accumulate.java
@@ -0,0 +1,6 @@
+public class Accumulate {
+
+    public static <T> List<T> accumulate(List<T> list, UnaryOperator<T> operator) {
+        throw new UnsupportedOperationException("Please implement the Accumulate.accumulate() method.");
+    }
+}
---
+++ exercises/practice/strain/src/main/java/Strain.java
@@ -0,0 +1,12 @@
+
+public class Strain {
+
+    public static <T> Collection<T> keep(Collection<T> collection, Predicate<T> predicate) {
+        throw new UnsupportedOperationException("Please implement the Strain.keep() method.");
+    }
+
+    public static <T> Collection<T> discard(Collection<T> collection, Predicate<T> predicate) {
+        throw new UnsupportedOperationException("Please implement the Strain.discard() method.");
+    }
+
+}
