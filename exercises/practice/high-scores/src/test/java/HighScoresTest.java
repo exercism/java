e8eb08a6767f7267f3d863c8b4d70167582a09f1
Rodrigo Oscar Gonzalez <ro_gonzalez@hotmail.com>
2023-02-18 09:31:00 +0100

    Using assertThat rather than assertEquals

--- exercises/practice/high-scores/src/test/java/HighScoresTest.java
+++ exercises/practice/high-scores/src/test/java/HighScoresTest.java
@@ -1,63 +1,63 @@
 import java.util.Arrays;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HighScoresTest {
 
     @Test
     public void shouldReturnListOfScores() {
         HighScores highScores = new HighScores(Arrays.asList(30, 50, 20, 70));
-        assertEquals(Arrays.asList(30, 50, 20, 70), highScores.scores());
+        assertThat(Arrays.asList(30, 50, 20, 70)).isEqualTo(highScores.scores());
     }
 
     @Test
     @Ignore("Remove to run test")
     public void shouldReturnLatestAddedScore() {
         HighScores highScores = new HighScores(Arrays.asList(100, 0, 90, 30));
-        assertEquals((Integer) 30, highScores.latest());
+        assertThat(30).isEqualTo(highScores.latest());
     }
 
     @Test
     @Ignore("Remove to run test")
     public void shouldReturnPersonalBest() {
         HighScores highScores = new HighScores(Arrays.asList(40, 100, 70));
-        assertEquals((Integer) 100, highScores.personalBest());
+        assertThat(100).isEqualTo(highScores.personalBest());
     }
 
     @Test
     @Ignore("Remove to run test")
     public void shouldReturnPersonalTopThreeFromListOfScores() {
         HighScores highScores = new HighScores(Arrays.asList(10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70));
-        assertEquals(Arrays.asList(100, 90, 70), highScores.personalTopThree());
+        assertThat(Arrays.asList(100, 90, 70)).isEqualTo(highScores.personalTopThree());
     }
 
     @Test
     @Ignore("Remove to run test")
     public void shouldReturnPersonalTopThreeSortedHighestToLowest() {
         HighScores highScores = new HighScores(Arrays.asList(20, 10, 30));
-        assertEquals(Arrays.asList(30, 20, 10), highScores.personalTopThree());
+        assertThat(Arrays.asList(30, 20, 10)).isEqualTo(highScores.personalTopThree());
     }
 
     @Test
     @Ignore("Remove to run test")
     public void shouldReturnPersonalTopThreeWhenThereIsATie() {
         HighScores highScores = new HighScores(Arrays.asList(40, 20, 40, 30));
-        assertEquals(Arrays.asList(40, 40, 30), highScores.personalTopThree());
+        assertThat(Arrays.asList(40, 40, 30)).isEqualTo(highScores.personalTopThree());
     }
 
     @Test
     @Ignore("Remove to run test")
     public void shouldReturnPersonalTopWhenThereIsLessThanThreeScores() {
         HighScores highScores = new HighScores(Arrays.asList(30, 70));
-        assertEquals(Arrays.asList(70, 30), highScores.personalTopThree());
+        assertThat(Arrays.asList(70, 30)).isEqualTo(highScores.personalTopThree());
     }
 
     @Test
     @Ignore("Remove to run test")
     public void shouldReturnPersonalTopWhenThereIsOnlyOneScore() {
         HighScores highScores = new HighScores(Arrays.asList(40));
-        assertEquals(Arrays.asList(40), highScores.personalTopThree());
+        assertThat(Arrays.asList(40)).isEqualTo(highScores.personalTopThree());
     }
 
     @Test
@@ -65,7 +65,7 @@ public class HighScoresTest {
     public void callingLatestAfterPersonalTopThree() {
         HighScores highScores = new HighScores(Arrays.asList(70, 50, 20, 30));
         highScores.personalTopThree();
-        assertEquals((Integer) 30, highScores.latest());
+        assertThat(30).isEqualTo(highScores.latest());
     }
 
     @Test
@@ -73,7 +73,7 @@ public class HighScoresTest {
     public void callingScoresAfterPersonalTopThree() {
         HighScores highScores = new HighScores(Arrays.asList(30, 50, 20, 70));
         highScores.personalTopThree();
-        assertEquals(Arrays.asList(30, 50, 20, 70), highScores.scores());
+        assertThat(Arrays.asList(30, 50, 20, 70)).isEqualTo(highScores.scores());
     }
 
     @Test
@@ -81,7 +81,7 @@ public class HighScoresTest {
     public void callingLatestAfterPersonalBest() {
         HighScores highScores = new HighScores(Arrays.asList(20, 70, 15, 25, 30));
         highScores.personalBest();
-        assertEquals((Integer) 30, highScores.latest());
+        assertThat(30).isEqualTo(highScores.latest());
     }
 
     @Test
@@ -89,6 +89,6 @@ public class HighScoresTest {
     public void callingScoresAfterPersonalBest() {
         HighScores highScores = new HighScores(Arrays.asList(20, 70, 15, 25, 30));
         highScores.personalBest();
-        assertEquals(Arrays.asList(20, 70, 15, 25, 30), highScores.scores());
+        assertThat(Arrays.asList(20, 70, 15, 25, 30)).isEqualTo(highScores.scores());
     }
 }
