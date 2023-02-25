643200e33b7d7191f6f7e91b58493422a49e8d0c
Trympyrym <rio09876@gmail.com>
2023-02-25 04:42:06 +0300

    Stubs for some exercises (#2232)

---
+++ exercises/practice/binary/src/main/java/Binary.java
@@ -0,0 +1,10 @@
+public class Binary {
+
+    public Binary(String input) {
+        throw new UnsupportedOperationException("Please implement the Binary constructor.");
+    }
+
+    public int getDecimal() {
+        throw new UnsupportedOperationException("Please implement the Binary.getDecimal() method.");
+    }
+}
---
+++ exercises/practice/strain/src/main/java/Strain.java
@@ -0,0 +1,14 @@
+import java.util.Collection;
+import java.util.function.Predicate;
+
+public class Strain {
+
+    public static <T> Collection<T> keep(Collection<T> collection, Predicate<T> predicate) {
+        throw new UnsupportedOperationException("Please implement the Strain.keep() method.");
+    }
+
+    public static <T> Collection<T> discard(Collection<T> collection, Predicate<T> predicate) {
+        throw new UnsupportedOperationException("Please implement the Strain.discard() method.");
+    }
+
+}
