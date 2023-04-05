6c3e85874a2107990ad1db4b90ff2249d45603ba
Elena Neuschild <elena.neuschild@cosee.biz>
2023-04-05 12:20:48 +0200

    add tests to RemoteControlCompetition
    - adapt instructions to match new tests
    - change ranking semantics to more intuitive more-wins-ranks-higher order

--- exercises/concept/remote-control-competition/src/test/java/RemoteControlCarTest.java
+++ exercises/concept/remote-control-competition/src/test/java/RemoteControlCarTest.java
@@ -1,10 +1,39 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RemoteControlCarTest {
+    @Test
+    public void productionRccIsRemoteControlCar() {
+        ProductionRemoteControlCar productionCar = new ProductionRemoteControlCar();
+        assertThat(productionCar instanceof RemoteControlCar).isTrue();
+    }
+
+    @Test
+    public void experimentalRccIsRemoteControlCar() {
+        ExperimentalRemoteControlCar experimentalCar = new ExperimentalRemoteControlCar();
+        assertThat(experimentalCar instanceof RemoteControlCar).isTrue();
+    }
+
+    @Test
+    public void productionCarTravels10UnitsPerDrive() {
+        ProductionRemoteControlCar car = new ProductionRemoteControlCar();
+        assertThat(car.getDistanceTravelled()).isEqualTo(0);
+        car.drive();
+        assertThat(car.getDistanceTravelled()).isEqualTo(10);
+    }
+
+    @Test
+    public void experimentalCarTravels20UnitsPerDrive() {
+        ExperimentalRemoteControlCar car = new ExperimentalRemoteControlCar();
+        assertThat(car.getDistanceTravelled()).isEqualTo(0);
+        car.drive();
+        assertThat(car.getDistanceTravelled()).isEqualTo(20);
+    }
+
     @Test
     public void race() {
         ProductionRemoteControlCar productionCar = new ProductionRemoteControlCar();
@@ -17,17 +46,47 @@ public class RemoteControlCarTest {
     }
 
     @Test
-    public void rankCars() {
+    public void ensureCarsAreComparable() {
+        assertThat(Comparable.class).isAssignableFrom(ProductionRemoteControlCar.class);
+    }
+
+    private static ProductionRemoteControlCar getCarWithVictories(int numberOfVictories) {
         ProductionRemoteControlCar prc1 = new ProductionRemoteControlCar();
-        ProductionRemoteControlCar prc2 = new ProductionRemoteControlCar();
-        prc1.setNumberOfVictories(3);
-        prc2.setNumberOfVictories(2);
-        List<ProductionRemoteControlCar> rankings = TestTrack.getRankedCars(prc1, prc2);
-        assertThat(rankings.get(1)).isEqualTo(prc1);
+        prc1.setNumberOfVictories(numberOfVictories);
+        return prc1;
     }
 
     @Test
-    public void ensureCarsAreComparable() {
-        assertThat(Comparable.class).isAssignableFrom(ProductionRemoteControlCar.class);
+    public void rankTwoCars() {
+        List<ProductionRemoteControlCar> unsortedCars = new ArrayList<>() {
+            {
+                add(getCarWithVictories(2));
+                add(getCarWithVictories(3));
+            }
+        };
+        List<ProductionRemoteControlCar> rankings = TestTrack.getRankedCars(unsortedCars);
+        assertThat(rankings.get(0).getNumberOfVictories()).isEqualTo(3);
+        assertThat(rankings.get(1).getNumberOfVictories()).isEqualTo(2);
+    }
+
+    @Test
+    public void rankManyCars() {
+        List<ProductionRemoteControlCar> unsortedCars = new ArrayList<>() {
+            {
+                add(getCarWithVictories(0));
+                add(getCarWithVictories(3));
+                add(getCarWithVictories(5));
+                add(getCarWithVictories(7));
+                add(getCarWithVictories(2));
+                add(getCarWithVictories(1));
+            }
+        };
+        List<ProductionRemoteControlCar> rankings = TestTrack.getRankedCars(unsortedCars);
+        assertThat(rankings.get(0).getNumberOfVictories()).isEqualTo(7);
+        assertThat(rankings.get(1).getNumberOfVictories()).isEqualTo(5);
+        assertThat(rankings.get(2).getNumberOfVictories()).isEqualTo(3);
+        assertThat(rankings.get(3).getNumberOfVictories()).isEqualTo(2);
+        assertThat(rankings.get(4).getNumberOfVictories()).isEqualTo(1);
+        assertThat(rankings.get(5).getNumberOfVictories()).isEqualTo(0);
     }
 }
