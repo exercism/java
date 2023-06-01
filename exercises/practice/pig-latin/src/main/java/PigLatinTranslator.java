2aba987af940cd30edf608a20cb3383b4fe9f32b
Mark Nyon <msnyon@hey.com>
2023-06-01 04:27:37 -0400

    #2133: Added UnsupportedOperationException for constructor and translate() method

--- exercises/practice/pig-latin/src/main/java/PigLatinTranslator.java
+++ exercises/practice/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,10 +1,10 @@
-/*
+import java.lang.String;
+public class PigLatinTranslator {
+    public PigLatinTranslator() {
+        throw new UnsupportedOperationException("Please implement the PigLatinTranslator constructor.");
+    }
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
+    public String translate(String text) {
+        throw new UnsupportedOperationException("Please implement the PigLatinTranslator.translate method");
+    }
+}
