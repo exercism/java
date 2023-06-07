0cfca42ec396353aa6f258ffc2d3a88a00032e56
DWalz <d.walz@zoho.com>
2023-06-07 17:51:59 +0200

    Add exercise stub for 'dominoes' exercise

--- exercises/practice/dominoes/src/main/java/Dominoes.java
+++ exercises/practice/dominoes/src/main/java/Dominoes.java
@@ -1,10 +1,9 @@
-/*
+import java.util.List;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class Dominoes {
 
-Please remove this comment when submitting your solution.
+    List<Domino> formChain(List<Domino> inputDominoes) throws ChainNotFoundException {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 
-*/
+}
