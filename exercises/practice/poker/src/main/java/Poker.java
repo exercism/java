71254b9e6aab8219037a075629bf4b632da3df51
DWalz <d.walz@zoho.com>
2023-06-07 20:36:00 +0200

    Add exercise stub for 'poker' exercise

--- exercises/practice/poker/src/main/java/Poker.java
+++ exercises/practice/poker/src/main/java/Poker.java
@@ -1,10 +1,13 @@
-/*
+import java.util.List;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class Poker {
 
-Please remove this comment when submitting your solution.
+    Poker(final List<String> hand) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 
-*/
+    List<String> getBestHands() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
