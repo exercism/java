82dc7d4060db55a64b6cabf3da1168a55074fd7c
DWalz <d.walz@zoho.com>
2023-06-07 17:44:29 +0200

    Add exercise stub for 'custom-set' exercise

--- exercises/practice/custom-set/src/main/java/CustomSet.java
+++ exercises/practice/custom-set/src/main/java/CustomSet.java
@@ -1,10 +1,46 @@
-/*
+import java.util.Collection;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class CustomSet<T> {
 
-Please remove this comment when submitting your solution.
+    CustomSet() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 
-*/
+    CustomSet(final Collection<T> data) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    boolean isEmpty() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    boolean contains(final T element) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    boolean isDisjoint(final CustomSet<T> other) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    boolean add(final T element) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    CustomSet<T> getIntersection(final CustomSet<T> other) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    CustomSet<T> getUnion(final CustomSet<T> other) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    CustomSet<T> getDifference(final CustomSet<T> other) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
