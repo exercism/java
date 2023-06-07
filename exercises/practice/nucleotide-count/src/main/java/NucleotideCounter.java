1c6727e8b980d4c92ac4e64af888f15249bb0894
DWalz <d.walz@zoho.com>
2023-06-07 20:13:18 +0200

    Add exercise stub for 'nucleotide-count' exercise

--- exercises/practice/nucleotide-count/src/main/java/NucleotideCounter.java
+++ exercises/practice/nucleotide-count/src/main/java/NucleotideCounter.java
@@ -1,10 +1,17 @@
-/*
+import java.util.Map;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class NucleotideCounter {
 
-Please remove this comment when submitting your solution.
+    NucleotideCounter(final String sequence) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 
-*/
+    int count(final char base) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    Map<Character, Integer> nucleotideCounts() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
