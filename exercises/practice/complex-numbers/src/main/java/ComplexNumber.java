490a50b263ca25d8cbbbb3818c29522388c4e38f
DWalz <d.walz@zoho.com>
2023-06-07 17:11:56 +0200

    Add exercise stub for 'complex' exercise

--- exercises/practice/complex-numbers/src/main/java/ComplexNumber.java
+++ exercises/practice/complex-numbers/src/main/java/ComplexNumber.java
@@ -1,10 +1,51 @@
-/*
+class ComplexNumber {
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    ComplexNumber(final double real, final double imag) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 
-Please remove this comment when submitting your solution.
+    double getReal() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 
-*/
+    double getImag() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    double abs() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    ComplexNumber add(final ComplexNumber other) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    ComplexNumber minus(final ComplexNumber other) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    ComplexNumber times(final ComplexNumber other) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    ComplexNumber times(final double factor) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    ComplexNumber div(final ComplexNumber other) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    ComplexNumber div(final double divisor) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    ComplexNumber conjugate() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    ComplexNumber exponentialOf() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
