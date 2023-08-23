76ffa9163715b3119da50e41c7a5b2bf1d0f67c4
Sander Ploegsma <sanderploegsma@gmail.com>
2023-08-23 11:40:56 +0200

    Fix compilation errors in Calculator Conundrum
    This is not the best solution, probably it would be better if the CI script is able to parse the config.json file and copy all files listed in `files.editor` and `files.exemplar`, but for now duplicating the editor files into the exemplar implementation should help to unblock the CI build.

---
+++ exercises/concept/calculator-conundrum/.meta/src/reference/java/IllegalOperationException.java
@@ -0,0 +1,11 @@
+public class IllegalOperationException extends RuntimeException {
+
+    public IllegalOperationException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public IllegalOperationException(String errorMessage, Throwable cause) {
+        super(errorMessage, cause);
+    }
+}
+
