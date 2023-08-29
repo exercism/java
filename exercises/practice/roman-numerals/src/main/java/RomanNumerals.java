7516062677a59e6a4e6603ea94edc13aa35e18d7
Sander Ploegsma <sanderploegsma@gmail.com>
2023-08-29 09:59:13 +0200

    Fix stub implementation for `roman-numerals`

--- exercises/practice/roman-numerals/src/main/java/RomanNumerals.java
+++ exercises/practice/roman-numerals/src/main/java/RomanNumerals.java
@@ -1,7 +1,9 @@
 class RomanNumerals {
-
     RomanNumerals(int number) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
+    String getRomanNumeral() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 }
