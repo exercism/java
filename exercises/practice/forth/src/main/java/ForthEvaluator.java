535a2e281b693702bf5078fd338a8dbd008088cd
Sander Ploegsma <sanderploegsma@gmail.com>
2023-08-29 08:49:31 +0200

    Add stubs to `forth` exercise

--- exercises/practice/forth/src/main/java/ForthEvaluator.java
+++ exercises/practice/forth/src/main/java/ForthEvaluator.java
@@ -1,10 +1,7 @@
-/*
+import java.util.List;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
+class ForthEvaluator {
+    List<Integer> evaluateProgram(List<String> input) {
+        throw new UnsupportedOperationException("Please implement the evaluateProgram method");
+    }
+}
