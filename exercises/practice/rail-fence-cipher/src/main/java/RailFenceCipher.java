e35cc9785010c995b4d58350f07cc415405b1d08
Sander Ploegsma <sanderploegsma@gmail.com>
2023-08-29 09:53:50 +0200

    Fix stub implementation for `rail-fence-cipher`

--- exercises/practice/rail-fence-cipher/src/main/java/RailFenceCipher.java
+++ exercises/practice/rail-fence-cipher/src/main/java/RailFenceCipher.java
@@ -8,4 +8,8 @@ class RailFenceCipher {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
+    String getDecryptedData(String message) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
 }
