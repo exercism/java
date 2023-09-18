6b2befe2fa1c349bb485ea854d5a463e540c6a1d
Sander Ploegsma <sanderploegsma@gmail.com>
2023-09-18 21:43:18 +0200

    Update lasagna to use test runner v3 features (#2354)

--- exercises/concept/lasagna/src/test/java/LasagnaTest.java
+++ exercises/concept/lasagna/src/test/java/LasagnaTest.java
@@ -1,4 +1,6 @@
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 import utils.Lasagna;
 
@@ -7,6 +9,8 @@ import static org.assertj.core.api.Assertions.*;
 public class LasagnaTest {
 
     @Test
+    @Tag("task:1")
+    @DisplayName("Implemented the expectedMinutesInOven method")
     public void implemented_expected_minutes_in_oven() {
         assertThat(new Lasagna().hasMethod("expectedMinutesInOven"))
                 .withFailMessage("Method expectedMinutesInOven must be created")
@@ -20,11 +24,15 @@ public class LasagnaTest {
     }
 
     @Test
+    @Tag("task:1")
+    @DisplayName("The expectedMinutesInOven method returns the correct value")
     public void expected_minutes_in_oven() {
         assertThat(new Lasagna().expectedMinutesInOven()).isEqualTo(40);
     }
 
     @Test
+    @Tag("task:2")
+    @DisplayName("Implemented the remainingMinutesInOven method")
     public void implemented_remaining_minutes_in_oven() {
         assertThat(new Lasagna().hasMethod("remainingMinutesInOven", int.class))
                 .withFailMessage("Method remainingMinutesInOven must be created")
@@ -38,11 +46,15 @@ public class LasagnaTest {
     }
 
     @Test
+    @Tag("task:2")
+    @DisplayName("The remainingMinutesInOven method calculates and returns the correct value")
     public void remaining_minutes_in_oven() {
         assertThat(new Lasagna().remainingMinutesInOven(25)).isEqualTo(15);
     }
 
     @Test
+    @Tag("task:3")
+    @DisplayName("Implemented the preparationTimeInMinutes method")
     public void implemented_preparation_time_in_minutes() {
         assertThat(new Lasagna().hasMethod("preparationTimeInMinutes", int.class))
                 .withFailMessage("Method preparationTimeInMinutes must be created")
@@ -56,16 +68,22 @@ public class LasagnaTest {
     }
 
     @Test
+    @Tag("task:3")
+    @DisplayName("The preparationTimeInMinutes method calculates the correct value for single layer")
     public void preparation_time_in_minutes_for_one_layer() {
         assertThat(new Lasagna().preparationTimeInMinutes(1)).isEqualTo(2);
     }
 
     @Test
+    @Tag("task:3")
+    @DisplayName("The preparationTimeInMinutes method calculates the correct value for multiple layers")
     public void preparation_time_in_minutes_for_multiple_layers() {
         assertThat(new Lasagna().preparationTimeInMinutes(4)).isEqualTo(8);
     }
 
     @Test
+    @Tag("task:4")
+    @DisplayName("Implemented the totalTimeInMinutes method")
     public void implemented_total_time_in_minutes() {
         assertThat(new Lasagna().hasMethod("totalTimeInMinutes", int.class, int.class))
                 .withFailMessage("Method totalTimeInMinutes must be created")
@@ -79,11 +97,15 @@ public class LasagnaTest {
     }
 
     @Test
+    @Tag("task:4")
+    @DisplayName("The totalTimeInMinutes method calculates the correct value for single layer")
     public void total_time_in_minutes_for_one_layer() {
         assertThat(new Lasagna().totalTimeInMinutes(1, 30)).isEqualTo(32);
     }
 
     @Test
+    @Tag("task:4")
+    @DisplayName("The totalTimeInMinutes method calculates the correct value for multiple layers")
     public void total_time_in_minutes_for_multiple_layers() {
         assertThat(new Lasagna().totalTimeInMinutes(4, 8)).isEqualTo(16);
     }
