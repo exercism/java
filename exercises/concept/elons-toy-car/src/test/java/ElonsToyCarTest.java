dc7c0d3b8b9a9ef52ef350f8de4c3221c727aa75
aldotele <aldo.telese@hotmail.it>
2023-09-25 16:18:54 +0200

    removed unused import

--- exercises/concept/elons-toy-car/src/test/java/ElonsToyCarTest.java
+++ exercises/concept/elons-toy-car/src/test/java/ElonsToyCarTest.java
@@ -1,9 +1,8 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ElonsToyCarTest {
     @Test
