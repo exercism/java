9b1343e3f842e8dc69e7970702871715b134d69d
aldotele <aldo.telese@hotmail.it>
2023-09-26 08:06:58 +0200

    fixed typo in display name

--- exercises/concept/log-levels/src/test/java/LogLevelsTest.java
+++ exercises/concept/log-levels/src/test/java/LogLevelsTest.java
@@ -7,49 +7,49 @@ import static org.assertj.core.api.Assertions.*;
 public class LogLevelsTest {
     @Test
     @Tag("task:1")
-    @DisplayName("The message method method returns the log line's message of an error log")
+    @DisplayName("The message method returns the log line's message of an error log")
     public void error_message() {
         assertThat(LogLevels.message("[ERROR]: Stack overflow")).isEqualTo("Stack overflow");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The message method method returns the log line's message of a warning log")
+    @DisplayName("The message method returns the log line's message of a warning log")
     public void warning_message() {
         assertThat(LogLevels.message("[WARNING]: Disk almost full")).isEqualTo("Disk almost full");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The message method method returns the log line's message of an info log")
+    @DisplayName("The message method returns the log line's message of an info log")
     public void info_message() {
         assertThat(LogLevels.message("[INFO]: File moved")).isEqualTo("File moved");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The message method method returns the log line's message after removing leading and trailing spaces")
+    @DisplayName("The message method returns the log line's message after removing leading and trailing spaces")
     public void message_with_leading_and_trailing_white_space() {
         assertThat(LogLevels.message("[WARNING]:   \tTimezone not set  \r\n")).isEqualTo("Timezone not set");
     }
 
     @Test
     @Tag("task:2")
-    @DisplayName("The logLevel method method returns the log level of an error log line")
+    @DisplayName("The logLevel method returns the log level of an error log line")
     public void error_log_level() {
         assertThat(LogLevels.logLevel("[ERROR]: Disk full")).isEqualTo("error");
     }
 
     @Test
     @Tag("task:2")
-    @DisplayName("The logLevel method method returns the log level of a warning log line")
+    @DisplayName("The logLevel method returns the log level of a warning log line")
     public void warning_log_level() {
         assertThat(LogLevels.logLevel("[WARNING]: Unsafe password")).isEqualTo("warning");
     }
 
     @Test
     @Tag("task:2")
-    @DisplayName("The logLevel method method returns the log level of an info log line")
+    @DisplayName("The logLevel method returns the log level of an info log line")
     public void info_log_level() {
         assertThat(LogLevels.logLevel("[INFO]: Timezone changed")).isEqualTo("info");
     }
