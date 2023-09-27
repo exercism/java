e9e2aa5ac07ff3707d94779e428082da4a1bdbe4
aldotele <aldo.telese@hotmail.it>
2023-09-27 10:39:05 +0200

    refactored tests and simplified display names

--- exercises/concept/karls-languages/src/test/java/LanguageListTest.java
+++ exercises/concept/karls-languages/src/test/java/LanguageListTest.java
@@ -10,14 +10,14 @@ public class LanguageListTest {
 
     @Test
     @Tag("task:1")
-    @DisplayName("The isEmpty method returns true when no language was added to the list")
+    @DisplayName("The isEmpty method returns true when the list contains no languages")
     public void empty() {
         assertThat(languageList.isEmpty()).isTrue();
     }
 
     @Test
     @Tag("task:2")
-    @DisplayName("The addLanguage method correctly adds a language to the list so that list is no more empty")
+    @DisplayName("The isEmpty method returns false after adding a language to the list")
     public void nonEmpty() {
         languageList.addLanguage("Java");
 
@@ -29,19 +29,14 @@ public class LanguageListTest {
     @DisplayName("The removeLanguage method correctly removes a language from the list")
     public void removeLanguage() {
         languageList.addLanguage("Java");
-        languageList.addLanguage("Python");
-        languageList.addLanguage("Ruby");
+        languageList.removeLanguage("Java");
 
-        languageList.removeLanguage("Python");
-
-        assertThat(languageList.containsLanguage("Java")).isTrue();
-        assertThat(languageList.containsLanguage("Python")).isFalse();
-        assertThat(languageList.containsLanguage("Ruby")).isTrue();
+        assertThat(languageList.isEmpty()).isTrue();
     }
 
     @Test
     @Tag("task:4")
-    @DisplayName("The firstLanguage method correctly retrieves the first language that was added to the list")
+    @DisplayName("The firstLanguage method returns the first language that was added to the list")
     public void firstLanguage() {
         languageList.addLanguage("Java");
         languageList.addLanguage("Python");
@@ -52,7 +47,7 @@ public class LanguageListTest {
 
     @Test
     @Tag("task:5")
-    @DisplayName("The count method returns the correct number of languages that were added to the list")
+    @DisplayName("The count method returns the number of languages in the list")
     public void countThree() {
         languageList.addLanguage("Java");
         languageList.addLanguage("Python");
@@ -63,38 +58,32 @@ public class LanguageListTest {
 
     @Test
     @Tag("task:5")
-    @DisplayName("The count method returns 0 in case no language was added to the list")
+    @DisplayName("The count method returns 0 when the list is empty")
     public void countEmpty() {
         assertThat(languageList.count()).isEqualTo(0);
     }
 
     @Test
     @Tag("task:6")
-    @DisplayName("The containsLanguage method returns the correct booleans after adding a language to the list")
-    public void addOneLanguage() {
+    @DisplayName("The containsLanguage method returns true when the language is in the list")
+    public void containsLanguage() {
         languageList.addLanguage("Java");
 
         assertThat(languageList.containsLanguage("Java")).isTrue();
-        assertThat(languageList.containsLanguage("Python")).isFalse();
     }
 
     @Test
     @Tag("task:6")
-    @DisplayName("The containsLanguage method returns the correct booleans after adding several languages to the list")
-    public void addMultipleLanguages() {
-        languageList.addLanguage("Java");
-        languageList.addLanguage("Ruby");
-        languageList.addLanguage("C++");
+    @DisplayName("The containsLanguage method returns false when the language is not in the list")
+    public void doesNotContainLanguage() {
+        languageList.addLanguage("Kotlin");
 
-        assertThat(languageList.containsLanguage("Java")).isTrue();
-        assertThat(languageList.containsLanguage("Ruby")).isTrue();
-        assertThat(languageList.containsLanguage("C++")).isTrue();
-        assertThat(languageList.containsLanguage("Python")).isFalse();
+        assertThat(languageList.containsLanguage("Java")).isFalse();
     }
 
     @Test
     @Tag("task:7")
-    @DisplayName("The isExciting method returns true after Java was added to the list")
+    @DisplayName("The isExciting method returns true when the list contains Java")
     public void excitingLanguageListWithJava() {
         languageList.addLanguage("Java");
 
@@ -103,7 +92,7 @@ public class LanguageListTest {
 
     @Test
     @Tag("task:7")
-    @DisplayName("The isExciting method returns true after Kotlin was added to the list")
+    @DisplayName("The isExciting method returns true when the list contains Kotlin")
     public void excitingLanguageListWithKotlin() {
         languageList.addLanguage("Python");
         languageList.addLanguage("Kotlin");
@@ -113,7 +102,7 @@ public class LanguageListTest {
 
     @Test
     @Tag("task:7")
-    @DisplayName("The isExciting method returns false when neither Java nor Kotlin were added to the list")
+    @DisplayName("The isExciting method returns false when the list contains neither Java nor Kotlin")
     public void boringLanguageList() {
         languageList.addLanguage("Python");
         languageList.addLanguage("Ruby");
