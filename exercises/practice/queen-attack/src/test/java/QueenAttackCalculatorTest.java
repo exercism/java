55a3bce3bfc8e2d29c0223ec794602dbd6a09700
Oliver Keszöcze <oliver.keszoecze@fau.de>
2023-09-29 09:35:00 +0200

    Use AssertJ in Queen Attack Calculator exercise

--- exercises/practice/queen-attack/src/test/java/QueenAttackCalculatorTest.java
+++ exercises/practice/queen-attack/src/test/java/QueenAttackCalculatorTest.java
@@ -1,9 +1,8 @@
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 
 public class QueenAttackCalculatorTest {
@@ -51,7 +50,7 @@ public class QueenAttackCalculatorTest {
         QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 4), new Queen(6, 6));
 
-        assertFalse(calculator.canQueensAttackOneAnother());
+        assertThat(calculator.canQueensAttackOneAnother()).isFalse();
     }
 
     @Ignore("Remove to run test")
@@ -60,7 +59,7 @@ public class QueenAttackCalculatorTest {
         QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 4), new Queen(2, 6));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
+        assertThat(calculator.canQueensAttackOneAnother()).isTrue();
     }
 
     @Ignore("Remove to run test")
@@ -69,7 +68,7 @@ public class QueenAttackCalculatorTest {
         QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(4, 5), new Queen(2, 5));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
+        assertThat(calculator.canQueensAttackOneAnother()).isTrue();
     }
 
     @Ignore("Remove to run test")
@@ -78,7 +77,7 @@ public class QueenAttackCalculatorTest {
         QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 2), new Queen(0, 4));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
+        assertThat(calculator.canQueensAttackOneAnother()).isTrue();
     }
 
     @Ignore("Remove to run test")
@@ -87,7 +86,7 @@ public class QueenAttackCalculatorTest {
         QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 2), new Queen(3, 1));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
+        assertThat(calculator.canQueensAttackOneAnother()).isTrue();
     }
 
     @Ignore("Remove to run test")
@@ -96,7 +95,7 @@ public class QueenAttackCalculatorTest {
         QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(2, 2), new Queen(1, 1));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
+        assertThat(calculator.canQueensAttackOneAnother()).isTrue();
     }
 
     @Ignore("Remove to run test")
@@ -105,7 +104,7 @@ public class QueenAttackCalculatorTest {
         QueenAttackCalculator calculator
                 = new QueenAttackCalculator(new Queen(1, 7), new Queen(0, 6));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
+        assertThat(calculator.canQueensAttackOneAnother()).isTrue();
     }
 
     @Ignore("Remove to run test")
