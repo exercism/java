a956fcc783df9cae5776ccf6ad9ec6fd39ed5424
aldotele <aldo.telese@hotmail.it>
2023-09-29 08:24:59 +0200

    added util imports

--- exercises/concept/remote-control-competition/src/test/java/RemoteControlCarTest.java
+++ exercises/concept/remote-control-competition/src/test/java/RemoteControlCarTest.java
@@ -2,7 +2,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RemoteControlCarTest {
     @Test
