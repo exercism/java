a64fb0c50ef0fe0bf8520eaf7005aa45f58dcb71
Oliver Keszöcze <oliver.keszoecze@fau.de>
2023-10-04 17:08:45 +0200

    remove unnecessary import

--- exercises/practice/dnd-character/src/test/java/DnDCharacterTest.java
+++ exercises/practice/dnd-character/src/test/java/DnDCharacterTest.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.Assert.assertEquals;
-
 public class DnDCharacterTest {
 
     private DnDCharacter dndCharacter = new DnDCharacter();
