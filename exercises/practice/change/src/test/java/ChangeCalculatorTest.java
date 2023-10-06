f8f69a67a0c601e73d2cab13e6a6907ea086317f
Oliver Keszöcze <oliver.keszoecze@fau.de>
2023-10-06 09:55:32 +0200

    Use AssertJ in Change exercise

--- exercises/practice/change/src/test/java/ChangeCalculatorTest.java
+++ exercises/practice/change/src/test/java/ChangeCalculatorTest.java
@@ -1,8 +1,9 @@
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.junit.Assert.assertEquals;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -13,9 +14,7 @@ public class ChangeCalculatorTest {
     public void testChangeThatCanBeGivenInASingleCoin() {
         ChangeCalculator changeCalculator = new ChangeCalculator(asList(1, 5, 10, 25, 100));
 
-        assertEquals(
-                singletonList(25),
-                changeCalculator.computeMostEfficientChange(25));
+        assertThat(changeCalculator.computeMostEfficientChange(25)).isEqualTo(singletonList(25));
     }
 
     @Ignore("Remove to run test")
@@ -23,9 +22,7 @@ public class ChangeCalculatorTest {
     public void testChangeThatMustBeGivenInMultipleCoins() {
         ChangeCalculator changeCalculator = new ChangeCalculator(asList(1, 5, 10, 25, 100));
 
-        assertEquals(
-                asList(5, 10),
-                changeCalculator.computeMostEfficientChange(15));
+        assertThat(changeCalculator.computeMostEfficientChange(15)).isEqualTo(asList(5, 10));
     }
 
     @Ignore("Remove to run test")
@@ -34,9 +31,8 @@ public class ChangeCalculatorTest {
     public void testLilliputianCurrency() {
         ChangeCalculator changeCalculator = new ChangeCalculator(asList(1, 4, 15, 20, 50));
 
-        assertEquals(
-                asList(4, 4, 15),
-                changeCalculator.computeMostEfficientChange(23));
+        assertThat(changeCalculator.computeMostEfficientChange(23)).isEqualTo(asList(4, 4, 15));
+
     }
 
     @Ignore("Remove to run test")
@@ -45,9 +41,7 @@ public class ChangeCalculatorTest {
     public void testLowerElbonianCurrency() {
         ChangeCalculator changeCalculator = new ChangeCalculator(asList(1, 5, 10, 21, 25));
 
-        assertEquals(
-                asList(21, 21, 21),
-                changeCalculator.computeMostEfficientChange(63));
+        assertThat(changeCalculator.computeMostEfficientChange(63)).isEqualTo(asList(21, 21, 21));
     }
 
     @Ignore("Remove to run test")
@@ -55,9 +49,8 @@ public class ChangeCalculatorTest {
     public void testLargeAmountOfChange() {
         ChangeCalculator changeCalculator = new ChangeCalculator(asList(1, 2, 5, 10, 20, 50, 100));
 
-        assertEquals(
-                asList(2, 2, 5, 20, 20, 50, 100, 100, 100, 100, 100, 100, 100, 100, 100),
-                changeCalculator.computeMostEfficientChange(999));
+        assertThat(changeCalculator.computeMostEfficientChange(999))
+                .isEqualTo(asList(2, 2, 5, 20, 20, 50, 100, 100, 100, 100, 100, 100, 100, 100, 100));
     }
 
     @Ignore("Remove to run test")
@@ -66,9 +59,7 @@ public class ChangeCalculatorTest {
     public void testPossibleChangeWithoutUnitCoinAvailable() {
         ChangeCalculator changeCalculator = new ChangeCalculator(asList(2, 5, 10, 20, 50));
 
-        assertEquals(
-                asList(2, 2, 2, 5, 10),
-                changeCalculator.computeMostEfficientChange(21));
+        assertThat(changeCalculator.computeMostEfficientChange(21)).isEqualTo(asList(2, 2, 2, 5, 10));
     }
 
     @Ignore("Remove to run test")
@@ -77,9 +68,7 @@ public class ChangeCalculatorTest {
     public void testAnotherPossibleChangeWithoutUnitCoinAvailable() {
         ChangeCalculator changeCalculator = new ChangeCalculator(asList(4, 5));
 
-        assertEquals(
-                asList(4, 4, 4, 5, 5, 5),
-                changeCalculator.computeMostEfficientChange(27));
+        assertThat(changeCalculator.computeMostEfficientChange(27)).isEqualTo(asList(4, 4, 4, 5, 5, 5));
     }
 
     @Ignore("Remove to run test")
@@ -87,9 +76,7 @@ public class ChangeCalculatorTest {
     public void testZeroChange() {
         ChangeCalculator changeCalculator = new ChangeCalculator(asList(1, 5, 10, 21, 25));
 
-        assertEquals(
-                emptyList(),
-                changeCalculator.computeMostEfficientChange(0));
+        assertThat(changeCalculator.computeMostEfficientChange(0)).isEqualTo(emptyList());
     }
 
     @Ignore("Remove to run test")
