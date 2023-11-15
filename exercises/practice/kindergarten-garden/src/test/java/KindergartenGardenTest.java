28a3e64750801bc8b52b066cff3cf045d6f98cf5
Sander Ploegsma <sanderploegsma@gmail.com>
2023-11-15 10:52:13 +0100

    Sync tests for practice exercise `kindergarten-garden` (#2571)
    [no important files changed]

--- exercises/practice/kindergarten-garden/src/test/java/KindergartenGardenTest.java
+++ exercises/practice/kindergarten-garden/src/test/java/KindergartenGardenTest.java
@@ -8,91 +8,171 @@ public class KindergartenGardenTest {
 
     @Test
     public void singleStudent() {
-        assertThat(
-            new KindergartenGarden("RC\nGG").getPlantsOfStudent("Alice")
-        ).containsExactly(
-            Plant.RADISHES, Plant.CLOVER, Plant.GRASS, Plant.GRASS
-        );
+        String garden = "RC\nGG";
+        String student = "Alice";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.RADISHES, Plant.CLOVER, Plant.GRASS, Plant.GRASS);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void singleStudent2() {
-        assertThat(
-            new KindergartenGarden("VC\nRC").getPlantsOfStudent("Alice")
-        ).containsExactly(
-            Plant.VIOLETS, Plant.CLOVER, Plant.RADISHES, Plant.CLOVER
-        );
+        String garden = "VC\nRC";
+        String student = "Alice";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.VIOLETS, Plant.CLOVER, Plant.RADISHES, Plant.CLOVER);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void twoStudents() {
-        assertThat(
-            new KindergartenGarden("VVCG\nVVRC").getPlantsOfStudent("Bob")
-        ).containsExactly(
-            Plant.CLOVER, Plant.GRASS, Plant.RADISHES, Plant.CLOVER
-        );
+        String garden = "VVCG\nVVRC";
+        String student = "Bob";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.CLOVER, Plant.GRASS, Plant.RADISHES, Plant.CLOVER);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void oneGardenSecondStudent() {
-        assertThat(
-            new KindergartenGarden("VVCCGG\nVVCCGG").getPlantsOfStudent("Bob")
-        ).containsExactly(
-            Plant.CLOVER, Plant.CLOVER, Plant.CLOVER, Plant.CLOVER
-        );
+        String garden = "VVCCGG\nVVCCGG";
+        String student = "Bob";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.CLOVER, Plant.CLOVER, Plant.CLOVER, Plant.CLOVER);
     }
 
     @Ignore("Remove to run test")
     @Test
     public void oneGardenThirdStudent() {
-        assertThat(
-            new KindergartenGarden("VVCCGG\nVVCCGG").getPlantsOfStudent("Charlie")
-        ).containsExactly(
-            Plant.GRASS, Plant.GRASS, Plant.GRASS, Plant.GRASS
-        );
+        String garden = "VVCCGG\nVVCCGG";
+        String student = "Charlie";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.GRASS, Plant.GRASS, Plant.GRASS, Plant.GRASS);
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void fullGardenFirstStudent() {
-        assertThat(
-            new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV").getPlantsOfStudent("Alice")
-        ).containsExactly(
-            Plant.VIOLETS, Plant.RADISHES, Plant.VIOLETS, Plant.RADISHES
-        );
+    public void fullGardenForAlice() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Alice";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.VIOLETS, Plant.RADISHES, Plant.VIOLETS, Plant.RADISHES);
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void fullGardenSecondStudent() {
-        assertThat(
-            new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV").getPlantsOfStudent("Bob")
-        ).containsExactly(
-            Plant.CLOVER, Plant.GRASS, Plant.CLOVER, Plant.CLOVER
-        );
+    public void fullGardenForBob() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Bob";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.CLOVER, Plant.GRASS, Plant.CLOVER, Plant.CLOVER);
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void fullGardenSecondToLastStudent() {
-        assertThat(
-            new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV").getPlantsOfStudent("Kincaid")
-        ).containsExactly(
-            Plant.GRASS, Plant.CLOVER, Plant.CLOVER, Plant.GRASS
-        );
+    public void fullGardenForCharlie() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Charlie";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.VIOLETS, Plant.VIOLETS, Plant.CLOVER, Plant.GRASS);
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void fullGardenLastStudent() {
-        assertThat(
-            new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV").getPlantsOfStudent("Larry")
-        ).containsExactly(
-            Plant.GRASS, Plant.VIOLETS, Plant.CLOVER, Plant.VIOLETS
-        );
+    public void fullGardenForDavid() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "David";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.RADISHES, Plant.VIOLETS, Plant.CLOVER, Plant.RADISHES);
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void fullGardenForEve() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Eve";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.CLOVER, Plant.GRASS, Plant.RADISHES, Plant.GRASS);
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void fullGardenForFred() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Fred";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.GRASS, Plant.CLOVER, Plant.VIOLETS, Plant.CLOVER);
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void fullGardenForGinny() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Ginny";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.CLOVER, Plant.GRASS, Plant.GRASS, Plant.CLOVER);
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void fullGardenForHarriet() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Harriet";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.VIOLETS, Plant.RADISHES, Plant.RADISHES, Plant.VIOLETS);
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void fullGardenForIleana() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Ileana";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.GRASS, Plant.CLOVER, Plant.VIOLETS, Plant.CLOVER);
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void fullGardenForJoseph() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Joseph";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.VIOLETS, Plant.CLOVER, Plant.VIOLETS, Plant.GRASS);
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void fullGardenForKincaid() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Kincaid";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.GRASS, Plant.CLOVER, Plant.CLOVER, Plant.GRASS);
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void fullGardenForLarry() {
+        String garden = "VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV";
+        String student = "Larry";
+
+        assertThat(new KindergartenGarden(garden).getPlantsOfStudent(student))
+                .containsExactly(Plant.GRASS, Plant.VIOLETS, Plant.CLOVER, Plant.VIOLETS);
     }
 
 }
