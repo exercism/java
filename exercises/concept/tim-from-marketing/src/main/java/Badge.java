5c17d88a26380ccd86587b3301ceb0483fe4de69
Sean Mc Garry <shan468@hotmail.com>
2023-11-25 00:58:02 +0000

    Updating error message

--- exercises/concept/tim-from-marketing/src/main/java/Badge.java
+++ exercises/concept/tim-from-marketing/src/main/java/Badge.java
@@ -1,5 +1,5 @@
 public class Badge {
     public static String print(Integer id, String name, String department) {
-        throw new UnsupportedOperationException("Please implement the (static) TimFromMarketing.Print() method");
+        throw new UnsupportedOperationException("Please implement the (static) Badge.print() method");
     }
 }
