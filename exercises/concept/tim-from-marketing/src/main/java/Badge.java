e937a27ff2c2e10442f3f9dc13276cfcb96aff97
smcg468 <49883535+smcg468@users.noreply.github.com>
2023-11-25 21:39:09 +0000

    Add nullability concept (#2584)
    * Nullability concept added
    
    * Updating error message
    
    * Updating config.json
    
    * Linting issues and prerequisites updated in config.json
    
    * Addressing review comments
    
    * Removing trailing space
    
    Fixes #2553

---
+++ exercises/concept/tim-from-marketing/src/main/java/Badge.java
@@ -0,0 +1,5 @@
+class Badge {
+    public String print(Integer id, String name, String department) {
+        throw new UnsupportedOperationException("Please implement the (static) Badge.print() method");
+    }
+}
