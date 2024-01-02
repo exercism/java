a28547cb49cf4515bb3cd444528baf31354204c2
manumafe98 <manumaxerapro@gmail.com>
2024-01-02 14:47:15 -0300

    Updating reference resolution to match the new tests

--- exercises/practice/parallel-letter-frequency/.meta/src/reference/java/ParallelLetterFrequency.java
+++ exercises/practice/parallel-letter-frequency/.meta/src/reference/java/ParallelLetterFrequency.java
@@ -1,16 +1,31 @@
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class ParallelLetterFrequency {
-
-    private static Map<Character, Integer> letterFrequencyMap;
+    private Map<Character, Integer> letterFrequencyMap;
+    private String letter;
 
     ParallelLetterFrequency(String[] letters) {
+        this.letterFrequencyMap = new HashMap<>();
+        this.letter = Arrays.stream(letters)
+                .collect(Collectors.joining())
+                .replaceAll("[^\\p{L}a-zA-Z]", "");
     }
 
     Map<Character, Integer> letterCounts() {
+
+        if (letter.length() == 0) {
+            return letterFrequencyMap;
+        }
+
+        for (char ch : letter.toCharArray()) {
+            ch = Character.toLowerCase(ch);
+            letterFrequencyMap.put(ch, letterFrequencyMap.getOrDefault(ch, 0) + 1);
+        }
+
         return letterFrequencyMap;
     }
+
 }
