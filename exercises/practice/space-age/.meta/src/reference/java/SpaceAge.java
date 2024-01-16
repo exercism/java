434b8d57948232e93d0fda4a08b4338fbbe3ee56
manumafe98 <manumaxerapro@gmail.com>
2024-01-16 17:51:42 -0300

    Rollbacking changes that were applied after an invalid tests, setting that tests to include = false

--- exercises/practice/space-age/.meta/src/reference/java/SpaceAge.java
+++ exercises/practice/space-age/.meta/src/reference/java/SpaceAge.java
@@ -1,37 +1,69 @@
-import java.util.Map;
-import java.util.HashMap;
-
 class SpaceAge {
 
-    private Map<String, Double> planetsOrbitalPeriod = new HashMap<>() {{
-            put("Mercury", 0.2408467);
-            put("Venus", 0.61519726);
-            put("Mars", 1.8808158);
-            put("Jupiter", 11.862615);
-            put("Saturn", 29.447498);
-            put("Uranus", 84.016846);
-            put("Neptune", 164.79132);
-            put("Earth", 1.0);
-        }};
+    private enum Planet {
+        EARTH(1.0),
+        MERCURY(0.2408467),
+        VENUS(0.61519726),
+        MARS(1.8808158),
+        JUPITER(11.862615),
+        SATURN(29.447498),
+        URANUS(84.016846),
+        NEPTUNE(164.79132);
 
-    private static final double EARTH_ORBITAL_PERIOD_IN_SECONDS = 31557600.0;
-    private double seconds;
-    private String planet;
+        private final double relativeOrbitalPeriod;
 
-    SpaceAge(double seconds, String planet) {
-        this.seconds = seconds;
-        this.planet = planet;
+        Planet(double relativeOrbitalPeriod) {
+            this.relativeOrbitalPeriod = relativeOrbitalPeriod;
+        }
 
-        if (planetsOrbitalPeriod.get(planet) == null) {
-            throw new IllegalArgumentException("not a planet");
+        private double getRelativeOrbitalPeriod() {
+            return relativeOrbitalPeriod;
         }
     }
 
-    double calculateAge() {
-        double earthyears = seconds / EARTH_ORBITAL_PERIOD_IN_SECONDS;
-        double years = earthyears / planetsOrbitalPeriod.get(planet);
+    private static final double EARTH_ORBITAL_PERIOD_IN_SECONDS = 31557600.0;
+    private double seconds;
+
+    SpaceAge(double seconds) {
+        this.seconds = seconds;
+    }
+
+    double onEarth() {
+        return calculateAge(Planet.EARTH);
+    }
+
+    double onMercury() {
+        return calculateAge(Planet.MERCURY);
+    }
+
+    double onVenus() {
+        return calculateAge(Planet.VENUS);
+    }
+
+    double onMars() {
+        return calculateAge(Planet.MARS);
+    }
+
+    double onJupiter() {
+        return calculateAge(Planet.JUPITER);
+    }
+
+    double onSaturn() {
+        return calculateAge(Planet.SATURN);
+    }
+
+    double onUranus() {
+        return calculateAge(Planet.URANUS);
+    }
+
+    double onNeptune() {
+        return calculateAge(Planet.NEPTUNE);
+    }
+
+    private double calculateAge(Planet planet) {
+        double planetYears = (seconds / EARTH_ORBITAL_PERIOD_IN_SECONDS) / planet.getRelativeOrbitalPeriod();
         
-        return (double) Math.round(years * 100) / 100;
+        return (double) Math.round(planetYears * 100) / 100;
     }
 
 }
--- exercises/practice/space-age/src/test/java/SpaceAgeTest.java
+++ exercises/practice/space-age/src/test/java/SpaceAgeTest.java
@@ -2,7 +2,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.within;
 
 public class SpaceAgeTest {
@@ -11,73 +10,64 @@ public class SpaceAgeTest {
 
     @Test
     public void ageOnEarth() {
-        SpaceAge age = new SpaceAge(1000000000, "Earth");
+        SpaceAge age = new SpaceAge(1000000000);
 
-        assertThat(age.calculateAge()).isEqualTo(31.69, within(MAXIMUM_DELTA));
+        assertThat(age.onEarth()).isEqualTo(31.69, within(MAXIMUM_DELTA));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void ageOnMercury() {
-        SpaceAge age = new SpaceAge(2134835688, "Mercury");
+        SpaceAge age = new SpaceAge(2134835688);
 
-        assertThat(age.calculateAge()).isEqualTo(280.88, within(MAXIMUM_DELTA));
+        assertThat(age.onMercury()).isEqualTo(280.88, within(MAXIMUM_DELTA));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void ageOnVenus() {
-        SpaceAge age = new SpaceAge(189839836, "Venus");
+        SpaceAge age = new SpaceAge(189839836);
 
-        assertThat(age.calculateAge()).isEqualTo(9.78, within(MAXIMUM_DELTA));
+        assertThat(age.onVenus()).isEqualTo(9.78, within(MAXIMUM_DELTA));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void ageOnMars() {
-        SpaceAge age = new SpaceAge(2129871239L, "Mars");
+        SpaceAge age = new SpaceAge(2129871239L);
 
-        assertThat(age.calculateAge()).isEqualTo(35.88, within(MAXIMUM_DELTA));
+        assertThat(age.onMars()).isEqualTo(35.88, within(MAXIMUM_DELTA));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void ageOnJupiter() {
-        SpaceAge age = new SpaceAge(901876382, "Jupiter");
+        SpaceAge age = new SpaceAge(901876382);
 
-        assertThat(age.calculateAge()).isEqualTo(2.41, within(MAXIMUM_DELTA));
+        assertThat(age.onJupiter()).isEqualTo(2.41, within(MAXIMUM_DELTA));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void ageOnSaturn() {
-        SpaceAge age = new SpaceAge(2000000000L, "Saturn");
+        SpaceAge age = new SpaceAge(2000000000L);
 
-        assertThat(age.calculateAge()).isEqualTo(2.15, within(MAXIMUM_DELTA));
+        assertThat(age.onSaturn()).isEqualTo(2.15, within(MAXIMUM_DELTA));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void ageOnUranus() {
-        SpaceAge age = new SpaceAge(1210123456L, "Uranus");
+        SpaceAge age = new SpaceAge(1210123456L);
 
-        assertThat(age.calculateAge()).isEqualTo(0.46, within(MAXIMUM_DELTA));
+        assertThat(age.onUranus()).isEqualTo(0.46, within(MAXIMUM_DELTA));
     }
 
     @Ignore("Remove to run test")
     @Test
     public void ageOnNeptune() {
-        SpaceAge age = new SpaceAge(1821023456L, "Neptune");
+        SpaceAge age = new SpaceAge(1821023456L);
 
-        assertThat(age.calculateAge()).isEqualTo(0.35, within(MAXIMUM_DELTA));
-    }
-
-    @Ignore("Remove to run test")
-    @Test
-    public void invalidPlanetCausesError() {
-
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new SpaceAge(680804807L, "Sun"))
-                .withMessage("not a planet");
+        assertThat(age.onNeptune()).isEqualTo(0.35, within(MAXIMUM_DELTA));
     }
 }
