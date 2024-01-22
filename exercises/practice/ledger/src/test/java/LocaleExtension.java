31c3616a53dd6a125889f025fba4c5e1304d2863
manumafe98 <manumaxerapro@gmail.com>
2024-01-22 17:07:28 -0300

    Fixing lint issue, and order of execution of the extension

--- exercises/practice/ledger/src/test/java/LocaleExtension.java
+++ exercises/practice/ledger/src/test/java/LocaleExtension.java
@@ -4,19 +4,18 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.util.Locale;
 
-public class LocaleExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback{
+public class LocaleExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
     private Locale originalLocale;
 
     @Override
-    public void afterTestExecution(ExtensionContext context) throws Exception {
+    public void beforeTestExecution(ExtensionContext context) throws Exception {
         originalLocale = Locale.getDefault();
 
         Locale.setDefault(Locale.US);
     }
 
     @Override
-    public void beforeTestExecution(ExtensionContext context) throws Exception {
+    public void afterTestExecution(ExtensionContext context) throws Exception {
         Locale.setDefault(originalLocale);
     }
-    
 }
