032ab311cd745c85bb9fb1e0a68a2a3ee69fdf16
Isaac Good <IsaacG@users.noreply.github.com>
2024-02-16 13:17:27 -0800

    log-levels: Update message to correct class name (#2732)

--- exercises/concept/log-levels/src/main/java/LogLevels.java
+++ exercises/concept/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,14 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        throw new UnsupportedOperationException("Please implement the (static) LogLevels.message() method");
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+        throw new UnsupportedOperationException("Please implement the (static) LogLevels.logLevel() method");
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+        throw new UnsupportedOperationException("Please implement the (static) LogLevels.reformat() method");
     }
 }
