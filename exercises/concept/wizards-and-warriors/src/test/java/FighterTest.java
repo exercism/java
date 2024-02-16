38b0d9a3b9b399dacb16a512c1e24c2319206b92
Sander Ploegsma <sanderploegsma@gmail.com>
2024-02-16 09:38:11 +0100

    Update test method names and descriptions

--- exercises/concept/wizards-and-warriors/src/test/java/FighterTest.java
+++ exercises/concept/wizards-and-warriors/src/test/java/FighterTest.java
@@ -18,7 +18,7 @@ class FighterTest {
 
     @Test
     @Tag("task:1")
-    @DisplayName("Test that the Warrior class exists in the code")
+    @DisplayName("The Warrior class is defined")
     void testWarriorClassExists() {
         try {
             Class.forName("Warrior");
@@ -29,15 +29,15 @@ class FighterTest {
 
     @Test
     @Tag("task:1")
-    @DisplayName("Test that the Warrior class is an instance of the Fighter class")
+    @DisplayName("The Warrior class inherits from the Fighter class")
     void testWarriorIsInstanceOfFighter() throws ClassNotFoundException {
         assertThat(Class.forName("Warrior")).isAssignableTo(Fighter.class);
     }
 
     @Test
     @Tag("task:2")
-    @DisplayName("Warrior has the toString method implemented")
-    void implemented_to_string_for_warrior() {
+    @DisplayName("The Warrior class overrides the toString method")
+    void testWarriorOverridesToStringMethod() {
         assertThat(warrior.hasMethod("toString"))
                 .withFailMessage("Method toString must be created")
                 .isTrue();
@@ -58,8 +58,8 @@ class FighterTest {
 
     @Test
     @Tag("task:3")
-    @DisplayName("Warrior has the isVulnerable method implemented")
-    void implemented_is_vulnerable_for_warrior() {
+    @DisplayName("The Warrior class overrides the isVulnerable method")
+    void testWarriorOverridesIsVulnerableMethod() {
         assertThat(warrior.hasMethod("isVulnerable"))
                 .withFailMessage("Method isVulnerable must be created")
                 .isTrue();
@@ -70,15 +70,15 @@ class FighterTest {
 
     @Test
     @Tag("task:3")
-    @DisplayName("The isVulnerable method of the Warrior always returns false")
+    @DisplayName("A Warrior is never vulnerable")
     void testWarriorAlwaysInvulnerable() {
         assertThat(warrior.isVulnerable()).isFalse();
     }
 
     @Test
     @Tag("task:4")
-    @DisplayName("Warrior has the getDamagePoints method implemented")
-    void implemented_get_damage_points_for_warrior() {
+    @DisplayName("The Warrior class overrides the getDamagePoints(Fighter) method")
+    void testWarriorOverridesGetDamagePointsMethod() {
         assertThat(warrior.hasMethod("getDamagePoints", Fighter.class))
                 .withFailMessage("Method getDamagePoints(Fighter) must be created")
                 .isTrue();
@@ -89,21 +89,21 @@ class FighterTest {
 
     @Test
     @Tag("task:4")
-    @DisplayName("The getDamagePoints method of the Warrior returns 10 when target is vulnerable")
+    @DisplayName("A Warrior deals 10 damage to a vulnerable target")
     void testWarriorsDamagePointsWhenTargetVulnerable() {
         assertThat(warrior.getDamagePoints(new VulnerableFighter())).isEqualTo(10);
     }
 
     @Test
     @Tag("task:4")
-    @DisplayName("The getDamagePoints method of the Warrior returns 6 when target is not vulnerable")
+    @DisplayName("A Warrior deals 6 damage to an invulnerable target")
     void testWarriorsDamagePointsWhenTargetNotVulnerable() {
         assertThat(warrior.getDamagePoints(new InvulnerableFighter())).isEqualTo(6);
     }
 
     @Test
     @Tag("task:5")
-    @DisplayName("Test that the Wizard class exists in the code")
+    @DisplayName("The Wizard class is defined")
     void testWizardClassExists() {
         try {
             Class.forName("Wizard");
@@ -114,17 +114,20 @@ class FighterTest {
 
     @Test
     @Tag("task:5")
-    @DisplayName("Test that the Wizard class is an instance of the Fighter class")
+    @DisplayName("The Wizard class inherits from the Fighter class")
     void testWizardIsInstanceOfFighter() throws ClassNotFoundException {
         assertThat(Class.forName("Wizard")).isAssignableTo(Fighter.class);
     }
 
     @Test
     @Tag("task:6")
-    @DisplayName("Wizard has the toString method implemented")
-    void implemented_to_string_for_wizard() {
+    @DisplayName("The Wizard class overrides the toString method")
+    void testWizardOverridesToStringMethod() {
         assertThat(wizard.hasMethod("toString"))
                 .withFailMessage("Method toString must be created")
+                .isTrue();
+        assertThat(warrior.isMethodPublic("toString"))
+                .withFailMessage("Method toString must be public")
                 .isTrue();
         assertThat(wizard.isMethodReturnType(String.class, "toString"))
                 .withFailMessage("Method toString must return a String")
@@ -140,8 +143,8 @@ class FighterTest {
 
     @Test
     @Tag("task:7")
-    @DisplayName("Wizard has the prepareSpell method implemented")
-    void implemented_prepare_spell_for_wizard() {
+    @DisplayName("The Wizard class contains the prepareSpell method")
+    void testWizardHasPrepareSpellMethod() {
         assertThat(wizard.hasMethod("prepareSpell"))
                 .withFailMessage("Method prepareSpell must be created")
                 .isTrue();
@@ -149,16 +152,8 @@ class FighterTest {
 
     @Test
     @Tag("task:7")
-    @DisplayName("The prepareSpell method makes the Wizard not vulnerable")
-    void testWizardVulnerable() {
-        wizard.prepareSpell();
-        assertThat(wizard.isVulnerable()).isFalse();
-    }
-
-    @Test
-    @Tag("task:7")
-    @DisplayName("Wizard has the isVulnerable method implemented")
-    void implemented_is_vulnerable_for_wizard() {
+    @DisplayName("The Wizard class overrides the isVulnerable method")
+    void testWizardOverridesIsVulnerableMethod() {
         assertThat(wizard.hasMethod("isVulnerable"))
                 .withFailMessage("Method isVulnerable must be created")
                 .isTrue();
@@ -169,15 +164,23 @@ class FighterTest {
 
     @Test
     @Tag("task:7")
-    @DisplayName("The isVulnerable method of the Wizard returns true by default")
+    @DisplayName("A Wizard is vulnerable when not prepared with a spell")
     void testWizardVulnerableByDefault() {
         assertThat(wizard.isVulnerable()).isTrue();
     }
 
     @Test
+    @Tag("task:7")
+    @DisplayName("A Wizard is not vulnerable when prepared with a spell")
+    void testWizardVulnerable() {
+        wizard.prepareSpell();
+        assertThat(wizard.isVulnerable()).isFalse();
+    }
+
+    @Test
     @Tag("task:8")
-    @DisplayName("Wizard has the getDamagePoints method implemented")
-    void implemented_get_damage_points_for_wizard() {
+    @DisplayName("The Wizard class overrides the getDamagePoints(Fighter) method")
+    void testWizardOverridesGetDamagePointsMethod() {
         assertThat(wizard.hasMethod("getDamagePoints", Fighter.class))
                 .withFailMessage("Method getDamagePoints(Fighter) must be created")
                 .isTrue();
@@ -188,14 +191,14 @@ class FighterTest {
 
     @Test
     @Tag("task:8")
-    @DisplayName("The getDamagePoints method of the Wizard returns 3 when spell has not been prepared")
+    @DisplayName("A Wizard deals 3 damage when no spell has been prepared")
     void testWizardsDamagePoints() {
         assertThat(wizard.getDamagePoints(new Fighter())).isEqualTo(3);
     }
 
     @Test
     @Tag("task:8")
-    @DisplayName("The getDamagePoints method of the Wizard returns 12 after a spell has been prepared")
+    @DisplayName("A Wizard deals 12 damage after a spell has been prepared")
     void testWizardsDamagePointsAfterPreparingSpell() {
         wizard.prepareSpell();
         assertThat(wizard.getDamagePoints(new Fighter())).isEqualTo(12);
