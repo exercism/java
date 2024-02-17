b8506ad2272cd9c81ac2f7ba6f774582c2111d2b
Manuel Maxera <95315128+manumafe98@users.noreply.github.com>
2024-02-17 12:55:32 -0300

    Updating class message to correct one for elons-toy-car and bird-watcher (#2733)
    [no important files changed]

--- exercises/concept/elons-toy-car/src/main/java/ElonsToyCar.java
+++ exercises/concept/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -1,17 +1,17 @@
 public class ElonsToyCar {
     public static ElonsToyCar buy() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.buy()  method");
+        throw new UnsupportedOperationException("Please implement the (static) ElonsToyCar.buy()  method");
     }
 
     public String distanceDisplay() {
-        throw new UnsupportedOperationException("Please implement the RemoteControlCar.distanceDisplay()  method");
+        throw new UnsupportedOperationException("Please implement the ElonsToyCar.distanceDisplay()  method");
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the RemoteControlCar.batteryDisplay()  method");
+        throw new UnsupportedOperationException("Please implement the ElonsToyCar.batteryDisplay()  method");
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the RemoteControlCar.drive()  method");
+        throw new UnsupportedOperationException("Please implement the ElonsToyCar.drive()  method");
     }
 }
