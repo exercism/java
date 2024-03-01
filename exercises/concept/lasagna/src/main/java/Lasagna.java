6183a73e47f046e1030da0b7b04b83fe7d7fccd9
sharmaashish54 <sharmaashish54@gmail.com>
2024-03-01 10:10:06 +0530

    Update Lasagna.java

--- exercises/concept/lasagna/src/main/java/Lasagna.java
+++ exercises/concept/lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,19 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
 
-    // TODO: define the 'remainingMinutesInOven()' method
+    public int expectedMinutesInOven(){
+        return 40;
+    }
 
-    // TODO: define the 'preparationTimeInMinutes()' method
+    public int remainingMinutesInOven(int minutes){
+        return expectedMinutesInOven() - minutes;
+    }
+    public int preparationTimeInMinutes(int layers){
+        return 2*layers;
+    
+    }
 
-    // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int noOfLayers, int min){
+        return preparationTimeInMinutes(noOfLayers) + min;
+
+    }
 }
