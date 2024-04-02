b63d173eae3fcd9fbc16b41420662b7cf750ddc6
Manuel Maxera <95315128+manumafe98@users.noreply.github.com>
2024-04-02 16:19:15 -0300

    Adding analyzer feedback for `cars-assemble` concept exercise (#2756)
    * Adding analyzer feedback for cars-assemble concept exercise
    
    * Update exercises/concept/cars-assemble/.meta/design.md
    
    Co-authored-by: Sander Ploegsma <sanderploegsma@gmail.com>
    
    * Update exercises/concept/cars-assemble/.meta/design.md
    
    Co-authored-by: Sander Ploegsma <sanderploegsma@gmail.com>
    
    * Update exercises/concept/cars-assemble/.meta/design.md
    
    Co-authored-by: Sander Ploegsma <sanderploegsma@gmail.com>
    
    ---------
    
    Co-authored-by: Sander Ploegsma <sanderploegsma@gmail.com>

--- exercises/concept/cars-assemble/.meta/src/reference/java/CarsAssemble.java
+++ exercises/concept/cars-assemble/.meta/src/reference/java/CarsAssemble.java
@@ -4,15 +4,11 @@ public class CarsAssemble {
     private final int defaultProductionRate = 221;
 
     public double productionRatePerHour(int speed) {
-        return productionRatePerHourForSpeed(speed) * successRate(speed);
+        return defaultProductionRate * speed * successRate(speed);
     }
 
     public int workingItemsPerMinute(int speed) {
         return (int) (productionRatePerHour(speed) / 60);
-    }
-
-    private int productionRatePerHourForSpeed(int speed) {
-        return defaultProductionRate * speed;
     }
 
     private double successRate(int speed) {
