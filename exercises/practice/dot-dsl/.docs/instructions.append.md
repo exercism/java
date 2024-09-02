86856d5d63675f193d1ebadfe20dffb7c297c479
Kah Goh <villastar@yahoo.com.au>
2024-09-02 22:56:58 +0800

    * Add instruction to implement methods in exceptions
    * Change prerequisite to "inheritance"
    
    * Add instructions.append.md to hint at use of method chaining

---
+++ exercises/practice/dot-dsl/.docs/instructions.append.md
@@ -0,0 +1,4 @@
+# Instructions append
+
+The DSL uses method chaining.
+You can learn more about method chaining [here](https://www.geeksforgeeks.org/method-chaining-in-java-with-examples/).
--- exercises/practice/dot-dsl/src/main/java/DotDsl.java
+++ exercises/practice/dot-dsl/src/main/java/DotDsl.java
@@ -11,55 +11,55 @@ class DotDsl {
         }
 
         public Collection<Node> getNodes() {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
 
         public Collection<Edge> getEdges() {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
 
         public Graph node(String name) {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
 
         public Graph node(String name, Map<String, String> attributes) {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
 
         public Graph edge(String start, String end) {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
 
         public Graph edge(String start, String end, Map<String, String> attributes) {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
 
         public Map<String, String> getAttributes() {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
     }
 
     static class Node {
         public String getName() {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
 
         public Map<String, String> getAttributes() {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
     }
 
     static class Edge {
         public String getStart() {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
 
         public String getEnd() {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
 
         public Map<String, String> getAttributes() {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         }
     }
 }
