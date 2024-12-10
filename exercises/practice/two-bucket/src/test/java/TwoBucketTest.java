45e971dab1586e02f2783f4d27dc7a9205a819b7
Jagdish Prajapati <jagadishdrp@gmail.com>
2024-12-10 19:41:54 +0530

    Adding missing import statement in test file for two-bucket

--- exercises/practice/two-bucket/src/test/java/TwoBucketTest.java
+++ exercises/practice/two-bucket/src/test/java/TwoBucketTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.Collections;
 
