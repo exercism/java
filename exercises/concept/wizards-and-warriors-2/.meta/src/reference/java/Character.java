6b2211e59dbae35116e9a3bcf2a99fc3936c819a
Jagdish Prajapati <jagadishdrp@gmail.com>
2024-12-31 02:24:05 +0530

    changing redirecting paths with duplicate code

--- exercises/concept/wizards-and-warriors-2/.meta/src/reference/java/Character.java
+++ exercises/concept/wizards-and-warriors-2/.meta/src/reference/java/Character.java
@@ -1,1 +1,29 @@
-../../../../src/main/java/Character.java
+public class Character {
+    private String characterClass;
+    private int level;
+    private int hitPoints;
+
+    public String getCharacterClass() {
+        return characterClass;
+    }
+
+    public void setCharacterClass(String characterClass) {
+        this.characterClass = characterClass;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+}
