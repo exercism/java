bdb27f47d2c30cfd6cb14d0ffd61fc05835b01b5
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-05-31 12:51:44 -0700

    Initial structure

---
+++ exercises/practice/relative-distance/.meta/src/reference/java/RelativeDistance.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.Map;
+
+class RelativeDistance {
+
+    RelativeDistance(Map<String, List<String>>  familyTree) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    int degreeOfSeparation(String personA, String personB) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
