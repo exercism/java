a63769fb01d8d35325bdd1a76ebe025c81c82e86
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-07-05 00:38:29 +0530

    Add gradle and test file

--- exercises/practice/intergalactic-transmission/src/main/java/IntergalacticTransmission.java
+++ exercises/practice/intergalactic-transmission/src/main/java/IntergalacticTransmission.java
@@ -2,12 +2,12 @@ import java.util.List;
 
 public class IntergalacticTransmission {
 
-	public static List<String> getTransmitSequence(List<String> message) {
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-	}
+    public static List<String> getTransmitSequence(List<String> message) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 
-	public static List<String> decodeSequence(List<String> sequence) {
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-	}
+    public static List<String> decodeSequence(List<String> sequence) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 
 }
