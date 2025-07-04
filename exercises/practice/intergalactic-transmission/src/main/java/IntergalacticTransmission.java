09354f9096fd1b9dff8d69e896cb0e85548bef6a
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-07-04 21:19:49 +0530

    Add stub

---
+++ exercises/practice/intergalactic-transmission/src/main/java/IntergalacticTransmission.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class IntergalacticTransmission {
+
+	public static List<String> getTransmitSequence(List<String> message) {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
+
+	public static List<String> decodeSequence(List<String> sequence) {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
+
+}
