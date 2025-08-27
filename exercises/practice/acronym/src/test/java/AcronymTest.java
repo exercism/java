ac4aeb208ccc14f524fa4a958dca0c8a051849cd
Atharv Patil <atharvdpatil909@gmail.com>
2025-08-27 14:39:17 +0530

    Change import order

--- exercises/practice/acronym/src/test/java/AcronymTest.java
+++ exercises/practice/acronym/src/test/java/AcronymTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
