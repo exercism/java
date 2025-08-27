97ff91f6ea0ea7c97b398237cac77091c9a20a71
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-08-27 14:48:19 +0530

    Update AcronymTest.java

--- exercises/practice/acronym/src/test/java/AcronymTest.java
+++ exercises/practice/acronym/src/test/java/AcronymTest.java
@@ -2,11 +2,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AcronymTest {
-
     
     @Test
     @DisplayName("basic")
