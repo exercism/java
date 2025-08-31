67c4d79b69b250cc131342b908be1d5aceef2d04
Atharv Patil <atharvdpatil909@gmail.com>
2025-08-31 23:28:59 +0530

    circular-buffer, clock, collatz-conjecture

--- exercises/practice/clock/src/test/java/ClockEqualTest.java
+++ exercises/practice/clock/src/test/java/ClockEqualTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,6 +8,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with same time")
     public void clocksWithSameTimeAreEqual() {
         assertThat(new Clock(15, 37))
                 .isEqualTo(new Clock(15, 37));
@@ -14,6 +16,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks a minute apart")
     public void clocksAMinuteApartAreNotEqual() {
         assertThat(new Clock(15, 36))
                 .isNotEqualTo(new Clock(15, 37));
@@ -21,6 +24,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks an hour apart")
     public void clocksAnHourApartAreNotEqual() {
         assertThat(new Clock(14, 37))
                 .isNotEqualTo(new Clock(15, 37));
@@ -28,6 +32,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with hour overflow")
     public void clocksWithHourOverflow() {
         assertThat(new Clock(34, 37))
                 .isEqualTo(new Clock(10, 37));
@@ -35,6 +40,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with hour overflow by several days")
     public void clocksWithHourOverflowBySeveralDays() {
         assertThat(new Clock(99, 11))
                 .isEqualTo(new Clock(3, 11));
@@ -42,6 +48,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with negative hour")
     public void clocksWithNegateHour() {
         assertThat(new Clock(-2, 40))
                 .isEqualTo(new Clock(22, 40));
@@ -49,6 +56,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with negative hour that wraps")
     public void clocksWithNegativeHourThatWraps() {
         assertThat(new Clock(-31, 3))
                 .isEqualTo(new Clock(17, 3));
@@ -56,6 +64,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with negative hour that wraps multiple times")
     public void clocksWithNegativeHourThatWrapsMultipleTimes() {
         assertThat(new Clock(-83, 49))
                 .isEqualTo(new Clock(13, 49));
@@ -63,6 +72,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with minute overflow")
     public void clocksWithMinuteOverflow() {
         assertThat(new Clock(0, 1441))
                 .isEqualTo(new Clock(0, 1));
@@ -70,6 +80,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with minute overflow by several days")
     public void clocksWithMinuteOverflowBySeveralDays() {
         assertThat(new Clock(2, 4322))
                 .isEqualTo(new Clock(2, 2));
@@ -77,6 +88,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with negative minute")
     public void clocksWithNegativeMinutes() {
         assertThat(new Clock(3, -20))
                 .isEqualTo(new Clock(2, 40));
@@ -84,6 +96,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with negative minute that wraps")
     public void clocksWithNegativeMinutesThatWraps() {
         assertThat(new Clock(5, -1490))
                 .isEqualTo(new Clock(4, 10));
@@ -91,6 +104,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with negative minute that wraps multiple times")
     public void clocksWithNegativeMinutesThatWrapsMultipleTimes() {
         assertThat(new Clock(6, -4305))
                 .isEqualTo(new Clock(6, 15));
@@ -98,6 +112,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with negative hours and minutes")
     public void clocksWithNegativeHoursAndMinutes() {
         assertThat(new Clock(-12, -268))
                 .isEqualTo(new Clock(7, 32));
@@ -105,6 +120,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("clocks with negative hours and minutes that wrap")
     public void clocksWithNegativeHoursAndMinutesThatWrap() {
         assertThat(new Clock(-54, -11513))
                 .isEqualTo(new Clock(18, 7));
@@ -112,6 +128,7 @@ public class ClockEqualTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("full clock and zeroed clock")
     public void clocksWithFullClockAndZeroedClockAreEqual() {
         assertThat(new Clock(24, 0))
                 .isEqualTo(new Clock(0, 0));
