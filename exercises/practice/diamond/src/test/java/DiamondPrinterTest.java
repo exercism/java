373fc98acb9ddac32a46ea8b8c7a7dbbcf38588b
Atharv Patil <atharvdpatil909@gmail.com>
2025-09-03 17:58:47 +0530

    Update exercises/practice/diamond/src/test/java/DiamondPrinterTest.java
    Co-authored-by: Jagdish Prajapati <jagadishdrp@gmail.com>

--- exercises/practice/diamond/src/test/java/DiamondPrinterTest.java
+++ exercises/practice/diamond/src/test/java/DiamondPrinterTest.java
@@ -1,8 +1,8 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DiamondPrinterTest {
