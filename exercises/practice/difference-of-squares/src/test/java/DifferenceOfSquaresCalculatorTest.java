d869d4590ab6c38bea08075ce499fff16332c369
Atharv Patil <atharvdpatil909@gmail.com>
2025-09-05 01:07:42 +0530

    finalized with adding @DisplayName to custom-set, darts, diamond, difference-of-squares (#2997)
    * custom-set, darts, diamond, difference-of-squares
    
    * Update exercises/practice/diamond/src/test/java/DiamondPrinterTest.java
    
    Co-authored-by: Jagdish Prajapati <jagadishdrp@gmail.com>
    
    [no important files changed]
    
    ---------
    
    Co-authored-by: Jagdish Prajapati <jagadishdrp@gmail.com>

--- exercises/practice/difference-of-squares/src/test/java/DifferenceOfSquaresCalculatorTest.java
+++ exercises/practice/difference-of-squares/src/test/java/DifferenceOfSquaresCalculatorTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +15,7 @@ public class DifferenceOfSquaresCalculatorTest {
     }
 
     @Test
+    @DisplayName("square of sum 1")
     public void testSquareOfSumUpToOne() {
         int expected = 1;
         int actual = calculator.computeSquareOfSumTo(1);
@@ -22,6 +24,7 @@ public class DifferenceOfSquaresCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("square of sum 5")
     public void testSquareOfSumUpToFive() {
         int expected = 225;
         int actual = calculator.computeSquareOfSumTo(5);
@@ -30,6 +33,7 @@ public class DifferenceOfSquaresCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("square of sum 100")
     public void testSquareOfSumUpToHundred() {
         int expected = 25502500;
         int actual = calculator.computeSquareOfSumTo(100);
@@ -38,6 +42,7 @@ public class DifferenceOfSquaresCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("sum of squares 1")
     public void testSumOfSquaresUpToOne() {
         int expected = 1;
         int actual = calculator.computeSumOfSquaresTo(1);
@@ -46,6 +51,7 @@ public class DifferenceOfSquaresCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("sum of squares 5")
     public void testSumOfSquaresUpToFive() {
         int expected = 55;
         int actual = calculator.computeSumOfSquaresTo(5);
@@ -54,6 +60,7 @@ public class DifferenceOfSquaresCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("sum of squares 100")
     public void testSumOfSquaresUpToHundred() {
         int expected = 338350;
         int actual = calculator.computeSumOfSquaresTo(100);
@@ -62,6 +69,7 @@ public class DifferenceOfSquaresCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("difference of squares 1")
     public void testDifferenceOfSquaresUpToOne() {
         int expected = 0;
         int actual = calculator.computeDifferenceOfSquares(1);
@@ -70,6 +78,7 @@ public class DifferenceOfSquaresCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("difference of squares 5")
     public void testDifferenceOfSquaresUpToFive() {
         int expected = 170;
         int actual = calculator.computeDifferenceOfSquares(5);
@@ -78,6 +87,7 @@ public class DifferenceOfSquaresCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("difference of squares 100")
     public void testDifferenceOfSquaresUpToHundred() {
         int expected = 25164150;
         int actual = calculator.computeDifferenceOfSquares(100);
