06cebe321f8d909db929393422d27743a6072d92
Bibek Parajuli <bibekparajuli275@gmail.com>
2025-09-05 11:31:05 +0530

    Fix import ordering

--- exercises/practice/resistor-color/src/test/java/ResistorColorTest.java
+++ exercises/practice/resistor-color/src/test/java/ResistorColorTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Disabled;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResistorColorTest {
