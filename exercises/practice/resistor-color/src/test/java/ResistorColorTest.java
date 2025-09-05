819c0a0456612e38c96d2f33d2339d94dd586294
Bibek Parajuli <bibekparajuli275@gmail.com>
2025-09-05 09:29:20 +0530

    Add DisplayName annotations to resistor-color

--- exercises/practice/resistor-color/src/test/java/ResistorColorTest.java
+++ exercises/practice/resistor-color/src/test/java/ResistorColorTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,24 +14,28 @@ public class ResistorColorTest {
     }
 
     @Test
+    @DisplayName("Black")
     public void testBlackColorCode() {
         assertThat(resistorColor.colorCode("black")).isEqualTo(0);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("white")
     public void testWhiteColorCode() {
         assertThat(resistorColor.colorCode("white")).isEqualTo(9);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Orange")
     public void testOrangeColorCode() {
         assertThat(resistorColor.colorCode("orange")).isEqualTo(3);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Colors")
     public void testColors() {
         assertThat(resistorColor.colors()).containsExactly(
                 "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"
