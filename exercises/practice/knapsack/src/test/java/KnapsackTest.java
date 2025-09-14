47bcb2192300c3f097721fe99f6ab0bd45ed6bd6
Atharv Patil <atharvdpatil909@gmail.com>
2025-09-14 17:58:17 +0530

    Update KnapsackTest.java

--- exercises/practice/knapsack/src/test/java/KnapsackTest.java
+++ exercises/practice/knapsack/src/test/java/KnapsackTest.java
@@ -1,6 +1,6 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
