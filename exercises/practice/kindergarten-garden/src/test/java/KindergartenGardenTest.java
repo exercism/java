1271d7ae7a093c1941e9ab97b155f9d33ffe1319
Atharv Patil <atharvdpatil909@gmail.com>
2025-09-14 17:57:03 +0530

    Update KindergartenGardenTest.java

--- exercises/practice/kindergarten-garden/src/test/java/KindergartenGardenTest.java
+++ exercises/practice/kindergarten-garden/src/test/java/KindergartenGardenTest.java
@@ -1,8 +1,6 @@
-
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class KindergartenGardenTest {
