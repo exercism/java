492f2080f2848ffc07331192786f32bffa14a626
Aditya5009 <138042821+Aditya5009@users.noreply.github.com>
2025-09-19 23:56:21 +0530

    MatrixTest, LuhnValidatorTest, PangramCheckerTest

--- exercises/practice/pangram/src/test/java/PangramCheckerTest.java
+++ exercises/practice/pangram/src/test/java/PangramCheckerTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,60 +15,70 @@ public class PangramCheckerTest {
     }
 
     @Test
+    @DisplayName("empty sentence")
     public void emptySentenceIsNotPangram() {
         assertThat(pangramChecker.isPangram("")).isFalse();
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("perfect lower case")
     public void perfectLowerCasePhraseIsPangram() {
         assertThat(pangramChecker.isPangram("abcdefghijklmnopqrstuvwxyz")).isTrue();
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("only lower case")
     public void phraseWithOnlyLowerCaseIsPangram() {
         assertThat(pangramChecker.isPangram("the quick brown fox jumps over the lazy dog")).isTrue();
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("missing the letter 'x'")
     public void phraseMissingCharacterXIsNotPangram() {
         assertThat(pangramChecker.isPangram("a quick movement of the enemy will jeopardize five gunboats")).isFalse();
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("missing the letter 'h'")
     public void phraseMissingCharacterHIsNotPangram() {
         assertThat(pangramChecker.isPangram("five boxing wizards jump quickly at it")).isFalse();
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("with underscores")
     public void phraseWithUnderscoresIsPangram() {
         assertThat(pangramChecker.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog")).isTrue();
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("with numbers")
     public void phraseWithNumbersIsPangram() {
         assertThat(pangramChecker.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs")).isTrue();
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("missing letters replaced by numbers")
     public void phraseWithMissingLettersReplacedByNumbersIsNotPangram() {
         assertThat(pangramChecker.isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog")).isFalse();
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("mixed case and punctuation")
     public void phraseWithMixedCaseAndPunctuationIsPangram() {
         assertThat(pangramChecker.isPangram("\"Five quacking Zephyrs jolt my wax bed.\"")).isTrue();
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("case insensitive")
     public void caseInsensitivePhraseIsNotPangram() {
         assertThat(pangramChecker.isPangram("abcdefghijklm ABCDEFGHIJKLM")).isFalse();
     }
