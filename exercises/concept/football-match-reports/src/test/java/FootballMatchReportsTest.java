e2c8bb01b46a59b79ab3e9a73977561af545bf68
Jorge <jorgermalves@protonmail.com>
2025-09-22 17:58:22 +0200

    fix(FootBallMatchReportsTest.java): remove unused import

--- exercises/concept/football-match-reports/src/test/java/FootballMatchReportsTest.java
+++ exercises/concept/football-match-reports/src/test/java/FootballMatchReportsTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class FootballMatchReportsTest {
 
