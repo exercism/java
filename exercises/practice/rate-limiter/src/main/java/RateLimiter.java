0b158f779ff91bb5c7985402c148a29db75385c5
Andrea <andreaxtan611@gmail.com>
2025-09-24 08:30:15 +0800

    Update exercises/practice/rate-limiter/src/main/java/RateLimiter.java
    Co-authored-by: Kah Goh <villastar@yahoo.com.au>

--- exercises/practice/rate-limiter/src/main/java/RateLimiter.java
+++ exercises/practice/rate-limiter/src/main/java/RateLimiter.java
@@ -4,7 +4,7 @@ import java.time.Instant;
 public class RateLimiter<K> {
 
     public RateLimiter(int limit, Duration windowSize, TimeSource timeSource) {
-
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
     public boolean allow(K clientId) {
