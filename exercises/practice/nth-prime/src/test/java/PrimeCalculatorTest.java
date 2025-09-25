96b7bed1aeb9987e9441f017fe15855991ba8514
Atharv Patil <atharvdpatil909@gmail.com>
2025-09-25 12:51:47 +0530

    nth-prime, nucleotide-count, ocr-numbers, palindrome-products

--- exercises/practice/nth-prime/src/test/java/PrimeCalculatorTest.java
+++ exercises/practice/nth-prime/src/test/java/PrimeCalculatorTest.java
@@ -3,6 +3,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PrimeCalculatorTest {
@@ -10,30 +11,35 @@ public class PrimeCalculatorTest {
     private PrimeCalculator primeCalculator = new PrimeCalculator();
 
     @Test
+    @DisplayName("first prime")
     public void testFirstPrime() {
         assertThat(primeCalculator.nth(1)).isEqualTo(2);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("second prime")
     public void testSecondPrime() {
         assertThat(primeCalculator.nth(2)).isEqualTo(3);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("sixth prime")
     public void testSixthPrime() {
         assertThat(primeCalculator.nth(6)).isEqualTo(13);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("big prime")
     public void testBigPrime() {
         assertThat(primeCalculator.nth(10001)).isEqualTo(104743);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("there is no zeroth prime")
     public void testUndefinedPrime() {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> primeCalculator.nth(0));
