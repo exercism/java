7fb1db8fc7ee66ce37be486c1f8edf9fd5983645
Atharv Patil <atharvdpatil909@gmail.com>
2025-09-25 17:45:48 +0530

    nth-prime, nucleotide-count, ocr-numbers, palindrome-products (#3024)
    * nth-prime, nucleotide-count, ocr-numbers, palindrome-products
    
    * Update PrimeCalculatorTest.java
    
    [no important files changed]

--- exercises/practice/ocr-numbers/src/test/java/OpticalCharacterReaderTest.java
+++ exercises/practice/ocr-numbers/src/test/java/OpticalCharacterReaderTest.java
@@ -2,6 +2,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,6 +10,7 @@ import java.util.Arrays;
 public class OpticalCharacterReaderTest {
 
     @Test
+    @DisplayName("Recognizes 0")
     public void testReaderRecognizesSingle0() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 " _ ",
@@ -23,6 +25,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes 1")
     public void testReaderRecognizesSingle1() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 "   ",
@@ -36,6 +39,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Unreadable but correctly sized inputs return ?")
     public void testReaderReturnsQuestionMarkForUnreadableButCorrectlySizedInput() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 "   ",
@@ -49,6 +53,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Input with a number of lines that is not a multiple of four raises an error")
     public void testReaderThrowsExceptionWhenNumberOfInputLinesIsNotAMultipleOf4() {
 
         assertThatExceptionOfType(IllegalArgumentException.class)
@@ -63,6 +68,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Input with a number of columns that is not a multiple of three raises an error")
     public void testReaderThrowsExceptionWhenNumberOfInputColumnsIsNotAMultipleOf3() {
 
 
@@ -79,6 +85,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes 110101100")
     public void testReaderRecognizesBinarySequence110101100() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 "       _     _        _  _ ",
@@ -92,6 +99,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Garbled numbers in a string are replaced with ?")
     public void testReaderReplacesUnreadableDigitsWithQuestionMarksWithinSequence() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 "       _     _           _ ",
@@ -106,6 +114,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes 2")
     public void testReaderRecognizesSingle2() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 " _ ",
@@ -119,6 +128,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes 3")
     public void testReaderRecognizesSingle3() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 " _ ",
@@ -132,6 +142,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes 4")
     public void testReaderRecognizesSingle4() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 "   ",
@@ -145,6 +156,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes 5")
     public void testReaderRecognizesSingle5() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 " _ ",
@@ -158,6 +170,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes 6")
     public void testReaderRecognizesSingle6() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 " _ ",
@@ -171,6 +184,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes 7")
     public void testReaderRecognizesSingle7() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 " _ ",
@@ -184,6 +198,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes 8")
     public void testReaderRecognizesSingle8() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 " _ ",
@@ -197,6 +212,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes 9")
     public void testReaderRecognizesSingle9() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 " _ ",
@@ -210,6 +226,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Recognizes string of decimal numbers")
     public void testReaderRecognizesSequence1234567890() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 "    _  _     _  _  _  _  _  _ ",
@@ -223,6 +240,7 @@ public class OpticalCharacterReaderTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("Numbers separated by empty lines are recognized. Lines are joined by commas.")
     public void testReaderRecognizesAndCorrectlyFormatsMultiRowInput() {
         String parsedInput = new OpticalCharacterReader().parse(Arrays.asList(
                 "    _  _ ",
