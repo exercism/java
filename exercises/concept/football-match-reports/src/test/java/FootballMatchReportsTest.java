2e926508455b86a80d9561e3c8f99c4dbf03678d
prohladenn <kovshov.valery@gmail.com>
2025-10-04 21:17:01 +0200

    Refactor method names to remove snake_case

--- exercises/concept/football-match-reports/src/test/java/FootballMatchReportsTest.java
+++ exercises/concept/football-match-reports/src/test/java/FootballMatchReportsTest.java
@@ -9,28 +9,28 @@ public class FootballMatchReportsTest {
     @Test
     @Tag("task:1")
     @DisplayName("The onField method returns the correct description of player with shirt number 1")
-    public void test_goal() {
+    public void testGoal() {
         assertThat(FootballMatchReports.onField(1)).isEqualTo("goalie");
     }
 
     @Test
     @Tag("task:1")
     @DisplayName("The onField method returns the correct description of player with shirt number 2")
-    public void test_left_back() {
+    public void testLeftBack() {
         assertThat(FootballMatchReports.onField(2)).isEqualTo("left back");
     }
 
     @Test
     @Tag("task:1")
     @DisplayName("The onField method returns the correct description of player with shirt number 5")
-    public void test_right_back() {
+    public void testRightBack() {
         assertThat(FootballMatchReports.onField(5)).isEqualTo("right back");
     }
 
     @Test
     @Tag("task:1")
     @DisplayName("The onField method returns the correct description of players with shirt numbers 3 and 4")
-    public void test_center_back() {
+    public void testCenterBack() {
         assertThat(FootballMatchReports.onField(3)).isEqualTo("center back");
         assertThat(FootballMatchReports.onField(4)).isEqualTo("center back");
     }
@@ -38,7 +38,7 @@ public class FootballMatchReportsTest {
     @Test
     @Tag("task:1")
     @DisplayName("The onField method returns the correct description of players with shirt numbers 6, 7 and 8")
-    public void test_midfielder() {
+    public void testMidfielder() {
         assertThat(FootballMatchReports.onField(6)).isEqualTo("midfielder");
         assertThat(FootballMatchReports.onField(7)).isEqualTo("midfielder");
         assertThat(FootballMatchReports.onField(8)).isEqualTo("midfielder");
@@ -47,35 +47,35 @@ public class FootballMatchReportsTest {
     @Test
     @Tag("task:1")
     @DisplayName("The onField method returns the correct description of player with shirt number 9")
-    public void test_left_wing() {
+    public void testLeftWing() {
         assertThat(FootballMatchReports.onField(9)).isEqualTo("left wing");
     }
 
     @Test
     @Tag("task:1")
     @DisplayName("The onField method returns the correct description of player with shirt number 10")
-    public void test_striker() {
+    public void testStriker() {
         assertThat(FootballMatchReports.onField(10)).isEqualTo("striker");
     }
 
     @Test
     @Tag("task:1")
     @DisplayName("The onField method returns the correct description of player with shirt number 11")
-    public void test_right_wing() {
+    public void testRightWing() {
         assertThat(FootballMatchReports.onField(11)).isEqualTo("right wing");
     }
 
     @Test
     @Tag("task:2")
     @DisplayName("The onField method returns 'invalid' for invalid shirt number")
-    public void test_exception() {
+    public void testException() {
         assertThat(FootballMatchReports.onField(13)).isEqualTo("invalid");
     }
 
     @Test
     @Tag("task:2")
     @DisplayName("The onField method returns 'invalid' for negative shirt number")
-    public void test_exception_negative_number() {
+    public void testExceptionNegativeNumber() {
         assertThat(FootballMatchReports.onField(-1)).isEqualTo("invalid");
     }
 }
--- exercises/concept/lasagna/src/test/java/LasagnaTest.java
+++ exercises/concept/lasagna/src/test/java/LasagnaTest.java
@@ -11,7 +11,7 @@ public class LasagnaTest {
     @Test
     @Tag("task:1")
     @DisplayName("Implemented the expectedMinutesInOven method")
-    public void implemented_expected_minutes_in_oven() {
+    public void implementedExpectedMinutesInOven() {
         assertThat(new Lasagna().hasMethod("expectedMinutesInOven"))
                 .withFailMessage("Method expectedMinutesInOven must be created")
                 .isTrue();
@@ -26,14 +26,14 @@ public class LasagnaTest {
     @Test
     @Tag("task:1")
     @DisplayName("The expectedMinutesInOven method returns the correct value")
-    public void expected_minutes_in_oven() {
+    public void expectedMinutesInOven() {
         assertThat(new Lasagna().expectedMinutesInOven()).isEqualTo(40);
     }
 
     @Test
     @Tag("task:2")
     @DisplayName("Implemented the remainingMinutesInOven method")
-    public void implemented_remaining_minutes_in_oven() {
+    public void implementedRemainingMinutesInOven() {
         assertThat(new Lasagna().hasMethod("remainingMinutesInOven", int.class))
                 .withFailMessage("Method remainingMinutesInOven must be created")
                 .isTrue();
@@ -48,14 +48,14 @@ public class LasagnaTest {
     @Test
     @Tag("task:2")
     @DisplayName("The remainingMinutesInOven method calculates and returns the correct value")
-    public void remaining_minutes_in_oven() {
+    public void remainingMinutesInOven() {
         assertThat(new Lasagna().remainingMinutesInOven(25)).isEqualTo(15);
     }
 
     @Test
     @Tag("task:3")
     @DisplayName("Implemented the preparationTimeInMinutes method")
-    public void implemented_preparation_time_in_minutes() {
+    public void implementedPreparationTimeInMinutes() {
         assertThat(new Lasagna().hasMethod("preparationTimeInMinutes", int.class))
                 .withFailMessage("Method preparationTimeInMinutes must be created")
                 .isTrue();
@@ -70,21 +70,21 @@ public class LasagnaTest {
     @Test
     @Tag("task:3")
     @DisplayName("The preparationTimeInMinutes method calculates the correct value for single layer")
-    public void preparation_time_in_minutes_for_one_layer() {
+    public void preparationTimeInMinutesForOneLayer() {
         assertThat(new Lasagna().preparationTimeInMinutes(1)).isEqualTo(2);
     }
 
     @Test
     @Tag("task:3")
     @DisplayName("The preparationTimeInMinutes method calculates the correct value for multiple layers")
-    public void preparation_time_in_minutes_for_multiple_layers() {
+    public void preparationTimeInMinutesForMultipleLayers() {
         assertThat(new Lasagna().preparationTimeInMinutes(4)).isEqualTo(8);
     }
 
     @Test
     @Tag("task:4")
     @DisplayName("Implemented the totalTimeInMinutes method")
-    public void implemented_total_time_in_minutes() {
+    public void implementedTotalTimeInMinutes() {
         assertThat(new Lasagna().hasMethod("totalTimeInMinutes", int.class, int.class))
                 .withFailMessage("Method totalTimeInMinutes must be created")
                 .isTrue();
@@ -99,14 +99,14 @@ public class LasagnaTest {
     @Test
     @Tag("task:4")
     @DisplayName("The totalTimeInMinutes method calculates the correct value for single layer")
-    public void total_time_in_minutes_for_one_layer() {
+    public void totalTimeInMinutesForOneLayer() {
         assertThat(new Lasagna().totalTimeInMinutes(1, 30)).isEqualTo(32);
     }
 
     @Test
     @Tag("task:4")
     @DisplayName("The totalTimeInMinutes method calculates the correct value for multiple layers")
-    public void total_time_in_minutes_for_multiple_layers() {
+    public void totalTimeInMinutesForMultipleLayers() {
         assertThat(new Lasagna().totalTimeInMinutes(4, 8)).isEqualTo(16);
     }
 }
