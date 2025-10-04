26e78d8682fe8844f52d8262f0d50405c3077a02
Bibek Parajuli <bibekparajuli275@gmail.com>
2025-10-04 17:17:45 +0545

    add displayname annotations to secret-handshake

--- exercises/practice/secret-handshake/src/test/java/HandshakeCalculatorTest.java
+++ exercises/practice/secret-handshake/src/test/java/HandshakeCalculatorTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,30 +9,35 @@ public class HandshakeCalculatorTest {
     private final HandshakeCalculator handshakeCalculator = new HandshakeCalculator();
 
     @Test
+    @DisplayName("wink for 1")
     public void testThatInput1YieldsAWink() {
         assertThat(handshakeCalculator.calculateHandshake(1)).containsExactly(Signal.WINK);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("double blink for 10")
     public void testThatInput2YieldsADoubleBlink() {
         assertThat(handshakeCalculator.calculateHandshake(2)).containsExactly(Signal.DOUBLE_BLINK);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("close your eyes for 100")
     public void testThatInput4YieldsACloseYourEyes() {
         assertThat(handshakeCalculator.calculateHandshake(4)).containsExactly(Signal.CLOSE_YOUR_EYES);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("jump for 1000")
     public void testThatInput8YieldsAJump() {
         assertThat(handshakeCalculator.calculateHandshake(8)).containsExactly(Signal.JUMP);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("combine two actions")
     public void testAnInputThatYieldsTwoActions() {
         assertThat(handshakeCalculator.calculateHandshake(3))
                 .containsExactly(Signal.WINK, Signal.DOUBLE_BLINK);
@@ -39,6 +45,7 @@ public class HandshakeCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("reverse two actions")
     public void testAnInputThatYieldsTwoReversedActions() {
         assertThat(handshakeCalculator.calculateHandshake(19))
                 .containsExactly(Signal.DOUBLE_BLINK, Signal.WINK);
@@ -46,18 +53,21 @@ public class HandshakeCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("reversing one action gives the same action")
     public void testReversingASingleActionYieldsTheSameAction() {
         assertThat(handshakeCalculator.calculateHandshake(24)).containsExactly(Signal.JUMP);
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("reversing no actions still gives no actions")
     public void testReversingNoActionsYieldsNoActions() {
         assertThat(handshakeCalculator.calculateHandshake(16)).isEmpty();
     }
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("all possible actions")
     public void testInputThatYieldsAllActions() {
         assertThat(handshakeCalculator.calculateHandshake(15))
                 .containsExactly(Signal.WINK, Signal.DOUBLE_BLINK, Signal.CLOSE_YOUR_EYES, Signal.JUMP);
@@ -65,6 +75,7 @@ public class HandshakeCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("reverse all possible actions")
     public void testInputThatYieldsAllActionsReversed() {
         assertThat(handshakeCalculator.calculateHandshake(31))
                 .containsExactly(Signal.JUMP, Signal.CLOSE_YOUR_EYES, Signal.DOUBLE_BLINK, Signal.WINK);
@@ -72,6 +83,7 @@ public class HandshakeCalculatorTest {
 
     @Disabled("Remove to run test")
     @Test
+    @DisplayName("do nothing for zero")
     public void testThatInput0YieldsNoActions() {
         assertThat(handshakeCalculator.calculateHandshake(0)).isEmpty();
     }
