5a9b18fd29ca799a57311145e00f5893f12ddc02
thibault2705 <thibault2705@gmail.com>
2025-11-17 11:17:09 -0600

    Add Camicia class & Implement tests

---
+++ exercises/practice/camicia/src/main/java/Camicia.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+class Camicia {
+
+    void simulateGame(List<String> playerA, List<String> playerB) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    String getStatus() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    int getCards() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    int getTricks() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
