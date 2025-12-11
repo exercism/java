1b99df00d9773d0e47d22baf471f1c313a28c2aa
Navneet Anand <1629076@kiit.ac.in>
2025-12-11 18:32:21 +0530

    Solution for the Bird Watcher Problem on Exercism. This learning exercise helped evolve your knowledge of Arrays, For-Each Loops, and For Loops.

--- exercises/concept/bird-watcher/src/main/java/BirdWatcher.java
+++ exercises/concept/bird-watcher/src/main/java/BirdWatcher.java
@@ -7,26 +7,55 @@ class BirdWatcher {
     }
 
     public int[] getLastWeek() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.getLastWeek() method");
+        //  throw new UnsupportedOperationException("Please implement the BirdWatcher.getLastWeek() method");
+        int[] birdCount = new int[] {0,2,5,3,7,8,4};
+        return birdCount;
     }
 
     public int getToday() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.getToday() method");
+        //  throw new UnsupportedOperationException("Please implement the BirdWatcher.getToday() method");
+        return birdsPerDay[birdsPerDay.length-1];
     }
 
     public void incrementTodaysCount() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.incrementTodaysCount() method");
+        ///   throw new UnsupportedOperationException("Please implement the BirdWatcher.incrementTodaysCount() method");
+        birdsPerDay[birdsPerDay.length-1] += 1;
     }
 
     public boolean hasDayWithoutBirds() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.hasDayWithoutBirds() method");
+        // throw new UnsupportedOperationException("Please implement the BirdWatcher.hasDayWithoutBirds() method");
+        boolean noBird = false;
+        for(int count : birdsPerDay){
+            if (count == 0){
+                noBird = true;
+            }
+
+        }
+        return noBird;
     }
 
     public int getCountForFirstDays(int numberOfDays) {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.getCountForFirstDays() method");
+        //  throw new UnsupportedOperationException("Please implement the BirdWatcher.getCountForFirstDays() method");
+        int getTotalCount=0;
+        if(numberOfDays>birdsPerDay.length){
+            numberOfDays = birdsPerDay.length;
+        }
+        for(int i=0;i<numberOfDays;i++){
+            getTotalCount += birdsPerDay[i];
+        }
+        return getTotalCount;
+
     }
 
     public int getBusyDays() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.getBusyDays() method");
+        //     throw new UnsupportedOperationException("Please implement the BirdWatcher.getBusyDays() method");
+
+        int busyDays=0;
+        for (int count : birdsPerDay){
+            if(count >= 5) {
+                busyDays++;
+            }
+        }
+        return busyDays;
     }
 }
