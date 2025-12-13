f2fc70d6f3fb7fbd5c2876df3b21dbab2da6f6aa
Navneet Anand <1629076@kiit.ac.in>
2025-12-13 13:09:37 +0530

    Solution for the Squeaky Clean Problem on Exercism. In this exercise you will implement a partial set of utility routines to help a developer clean up SqueakyClean names.
    In the 4 tasks you will gradually build up the clean method. A valid SqueakyClean name is comprised of zero or more letters and underscores.
    
    In all cases the input string is guaranteed to be non-null. Note that the clean method should treat an empty string as valid.

--- exercises/concept/squeaky-clean/src/main/java/SqueakyClean.java
+++ exercises/concept/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,35 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+     //   throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        int index =0;
+        String cleanString = identifier.replace(' ','_');
+        if(cleanString.contains("-")) {
+            index = cleanString.indexOf('-');
+            cleanString = cleanString.replace("-", "");
+            cleanString = cleanString.substring(0,index) + cleanString.substring(index,index+1).toUpperCase() + cleanString.substring(index+1);
+        }
+        char[] chars = cleanString.toCharArray();
+        String newString = "";
+        for(char c : chars) {
+            if(c=='4') c = 'a';
+            else if(c=='3') c = 'e';
+            else if(c=='0') c = 'o';
+            else if(c=='1') c = 'l';
+            else if(c=='7') c = 't';
+
+            newString += c;
+        }
+        cleanString = newString;
+
+        chars = cleanString.toCharArray();
+        newString = "";
+        for(char c : chars) {
+            if(Character.isAlphabetic(c)) newString += c;
+            else if(c=='_') newString += c;
+        }
+        cleanString = newString;
+
+
+        return cleanString;
     }
 }
