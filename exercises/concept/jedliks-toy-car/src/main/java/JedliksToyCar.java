756235fa58b94f1f64ba2c9e9586a42870b66704
Navneet Anand <1629076@kiit.ac.in>
2025-12-15 17:37:19 +0530

    Solution for the Jedlik's Toy Car Problem on Exercism. Instructions: In this exercise you'll be playing around with a remote controlled car, which you've finally saved enough money for to buy. Cars start with full (100%) batteries. Each time you drive the car using the remote control, it covers 20 meters and drains one percent of the battery.
    The remote controlled car has a fancy LED display that shows two bits of information:
    
    The total distance it has driven, displayed as: "Driven <METERS> meters".
    The remaining battery charge, displayed as: "Battery at <PERCENTAGE>%".
    If the battery is at 0%, you can't drive the car anymore and the battery display will show "Battery empty".
    
    You have six tasks, each of which will work with remote controlled car instances.

--- exercises/concept/jedliks-toy-car/src/main/java/JedliksToyCar.java
+++ exercises/concept/jedliks-toy-car/src/main/java/JedliksToyCar.java
@@ -1,17 +1,44 @@
 public class JedliksToyCar {
+    private int distance=0;
+    private int battery=100;
+
     public static JedliksToyCar buy() {
-        throw new UnsupportedOperationException("Please implement the (static) JedliksToyCar.buy()  method");
+        //    throw new UnsupportedOperationException("Please implement the (static) JedliksToyCar.buy()  method");
+        JedliksToyCar car = new JedliksToyCar();
+        return car;
     }
 
     public String distanceDisplay() {
-        throw new UnsupportedOperationException("Please implement the JedliksToyCar.distanceDisplay()  method");
+        //  throw new UnsupportedOperationException("Please implement the JedliksToyCar.distanceDisplay()  method");
+
+
+        String carDistanceDisplay = null;
+        if (this.distance <= 2000) {
+            carDistanceDisplay = "Driven " + this.distance + " meters";
+        }
+        else if(this.distance > 2000) {
+            carDistanceDisplay = "Driven 2000 meters";
+        }
+
+            return carDistanceDisplay;
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the JedliksToyCar.batteryDisplay()  method");
+      //  throw new UnsupportedOperationException("Please implement the JedliksToyCar.batteryDisplay()  method");
+        String carBatteryDisplay = null;
+        if (this.battery <= 100 && this.battery >= 1) {
+            carBatteryDisplay = "Battery at "+ this.battery +"%";
+        }
+        else if(this.battery <= 0) {
+            carBatteryDisplay = "Battery empty";
+        }
+        return carBatteryDisplay;
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the JedliksToyCar.drive()  method");
+        //   throw new UnsupportedOperationException("Please implement the JedliksToyCar.drive()  method");
+        this.distance += 20;
+        this.battery -= 1;
+
     }
 }
