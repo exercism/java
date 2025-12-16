c919dab5d7a832e2386e8d114bc81e134f7e30fc
Navneet Anand <1629076@kiit.ac.in>
2025-12-16 21:39:14 +0530

    Solution for the Booking up for a Beauty Problem on Exercism. Instructions: In this exercise you'll be working on an appointment scheduler for a beauty salon in New York that opened on September 15th in 2012.

--- exercises/concept/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
+++ exercises/concept/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -1,24 +1,36 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 class AppointmentScheduler {
     public LocalDateTime schedule(String appointmentDateDescription) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.schedule() method");
+        //  throw new UnsupportedOperationException("Please implement the AppointmentScheduler.schedule() method");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+        LocalDateTime date = LocalDateTime.parse(appointmentDateDescription, formatter);
+
+        DateTimeFormatter printer = DateTimeFormatter.ofPattern("yyyy, MM, dd, HH, mm, ss");
+        printer.format(date);
+        return LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(), date.getHour(), date.getMinute(), date.getSecond());
     }
 
     public boolean hasPassed(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+        // throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+        return appointmentDate.isBefore(LocalDateTime.now());
     }
 
     public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+        //  throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+        return appointmentDate.getHour() >= 12 && appointmentDate.getHour() < 18;
     }
 
     public String getDescription(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getDescription() method");
+        //  throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getDescription() method");
+        DateTimeFormatter printer = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy, 'at' h:mm a'.'");
+        return "You have an appointment on "+printer.format(appointmentDate);
     }
 
     public LocalDate getAnniversaryDate() {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+        //  throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+        return LocalDate.of(LocalDate.now().getYear(), 9,15);
     }
 }
