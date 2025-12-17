004b7f85b3faa299eb5ac5c429d986d42d72068c
Navneet Anand <1629076@kiit.ac.in>
2025-12-17 13:44:38 +0530

    Solution for the Karl's Languages Problem on Exercism. Instructions: Karl wants to keep track of a list of languages to learn on Exercism's website. Karl needs to be able to add new languages, remove old ones and check if certain languages are in the list. It would be very exciting if Karl wants to learn Java or Kotlin!

--- exercises/concept/karls-languages/src/main/java/LanguageList.java
+++ exercises/concept/karls-languages/src/main/java/LanguageList.java
@@ -5,30 +5,37 @@ public class LanguageList {
     private final List<String> languages = new ArrayList<>();
 
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Please implement the isEmpty() method");
+        //throw new UnsupportedOperationException("Please implement the isEmpty() method");
+        return languages.isEmpty();
     }
 
     public void addLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the addLanguage() method");
+         // throw new UnsupportedOperationException("Please implement the addLanguage() method");
+        languages.add(language);
     }
 
     public void removeLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the removeLanguage() method");
+      //  throw new UnsupportedOperationException("Please implement the removeLanguage() method");
+        languages.remove(language);
     }
 
     public String firstLanguage() {
-        throw new UnsupportedOperationException("Please implement the firstLanguage() method");
+      //  throw new UnsupportedOperationException("Please implement the firstLanguage() method");
+        return languages.getFirst();
     }
 
     public int count() {
-        throw new UnsupportedOperationException("Please implement the count() method");
+       // throw new UnsupportedOperationException("Please implement the count() method");
+        return languages.size();
     }
 
     public boolean containsLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the containsLanguage() method");
+       // throw new UnsupportedOperationException("Please implement the containsLanguage() method");
+        return languages.contains(language);
     }
 
     public boolean isExciting() {
-        throw new UnsupportedOperationException("Please implement the isExciting() method");
+        // throw new UnsupportedOperationException("Please implement the isExciting() method");
+     return languages.contains("Java") || languages.contains("Kotlin");
     }
 }
