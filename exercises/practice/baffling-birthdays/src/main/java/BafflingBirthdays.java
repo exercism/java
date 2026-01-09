36e31a6d1f9c778f4019c31c8acf0f1926e41e9e
Kacper Olszewski <78431595+Baboushka@users.noreply.github.com>
2026-01-09 01:36:13 +0100

    Add baffling birthdays exercice (#3075)

---
+++ exercises/practice/baffling-birthdays/src/main/java/BafflingBirthdays.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+class BafflingBirthdays {
+    boolean sharedBirthday(List<String> birthdates) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    List<String> randomBirthdates(int groupSize) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    double estimatedProbabilityOfSharedBirthday(int groupSize) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
