59e777bf342e3f27a1a2da283d25d9a3ccc315c2
Thibault Phan <thibault2705@gmail.com>
2026-04-20 09:29:12 -0600

    Make simulateGame static and return result record

--- exercises/practice/camicia/src/main/java/Camicia.java
+++ exercises/practice/camicia/src/main/java/Camicia.java
@@ -2,19 +2,8 @@ import java.util.List;
 
 public class Camicia {
 
-    void simulateGame(List<String> playerA, List<String> playerB) {
+    static CamiciaResult simulateGame(List<String> playerA, List<String> playerB) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
-    String getStatus() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
-    int getCards() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
-    int getTricks() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
 }
