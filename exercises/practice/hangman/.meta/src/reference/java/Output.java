1e6e148ae5f24dfcc113c48ef4225447f1008adf
Thibault Phan <thibault2705@gmail.com>
2026-04-22 12:32:46 -0600

    Synchronize tests & Update solution to match tests.toml

--- exercises/practice/hangman/.meta/src/reference/java/Output.java
+++ exercises/practice/hangman/.meta/src/reference/java/Output.java
@@ -1,89 +1,94 @@
-import static java.util.stream.Collectors.joining;
-
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import static java.util.stream.Collectors.joining;
+
 class Output {
 
-    public final String secret;
-    public final String discovered;
-    public final Set<String> guess;
+    static final int MAX_FAILURES = Part.values().length;
+
+    public final String word;
+    public final String maskedWord;
+    public final Set<String> guesses;
     public final Set<String> misses;
     public final List<Part> parts;
-    public final Status status;
+    public final Status state;
+    public final int remainingFailures;
 
     Output(
-        final String secret,
-        final String discovered,
-        final Set<String> guess,
-        final Set<String> misses,
-        final List<Part> parts,
-        final Status status) {
-        this.secret = secret;
-        this.discovered = discovered;
-        this.guess = Set.copyOf(guess);
+            final String word,
+            final String maskedWord,
+            final Set<String> guesses,
+            final Set<String> misses,
+            final List<Part> parts,
+            final Status state) {
+        this.word = word;
+        this.maskedWord = maskedWord;
+        this.guesses = Set.copyOf(guesses);
         this.misses = Set.copyOf(misses);
         this.parts = List.copyOf(parts);
-        this.status = status;
+        this.state = state;
+        this.remainingFailures = Math.max(0, MAX_FAILURES - 1 - parts.size());
     }
 
     static Output empty() {
         return new Output(
-            null,
-            null,
-            Collections.emptySet(),
-            Collections.emptySet(),
-            Collections.emptyList(),
-            null);
+                null,
+                null,
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptyList(),
+                null);
     }
 
     static Output initialState(final String secret) {
         return new Output(
-            secret,
-            getGuessedWord(secret, Collections.emptySet()),
-            new HashSet<>(),
-            new HashSet<>(),
-            new ArrayList<>(),
-            Status.PLAYING);
+                secret,
+                getGuessedWord(secret, Collections.emptySet()),
+                new LinkedHashSet<>(),
+                new LinkedHashSet<>(),
+                new ArrayList<>(),
+                Status.ON_GOING);
     }
 
     boolean isLetterAlreadyPlayed(final String letter) {
-        return guess.contains(letter) || misses.contains(letter);
+        return guesses.contains(letter) || misses.contains(letter);
     }
 
     boolean isLetterInSecret(final String letter) {
-        return secret.contains(letter);
+        return word.contains(letter);
     }
 
     static String getGuessedWord(String secret, Set<String> letters) {
         return secret.chars()
-            .mapToObj(i -> String.valueOf((char) i))
-            .map(c -> letters.contains(c) ? c : "_")
-            .collect(joining());
+                .mapToObj(i -> String.valueOf((char) i))
+                .map(c -> letters.contains(c) ? c : "_")
+                .collect(joining());
     }
 
     static boolean isWin(String secret, Set<String> guessedLetters) {
         return secret.chars()
-            .mapToObj(i -> String.valueOf((char) i))
-            .allMatch(guessedLetters::contains);
+                .mapToObj(i -> String.valueOf((char) i))
+                .allMatch(guessedLetters::contains);
     }
 
-    static boolean isLoss(Set<String> missedLetters) {
-        return missedLetters.size() >= Part.values().length;
+    static boolean isLoss(List<Part> parts) {
+        return parts.size() >= MAX_FAILURES;
     }
 
     @Override
     public String toString() {
         return "Output{" +
-            "secret='" + secret + '\'' +
-            ", discovered='" + discovered + '\'' +
-            ", guess=" + guess +
-            ", misses=" + misses +
-            ", parts=" + parts +
-            ", status=" + status +
-            '}';
+                "secret='" + word + '\'' +
+                ", discovered='" + maskedWord + '\'' +
+                ", guess=" + guesses +
+                ", misses=" + misses +
+                ", parts=" + parts +
+                ", status=" + state +
+                ", remainingFailures=" + remainingFailures +
+                '}';
     }
 }
--- exercises/practice/hangman/.meta/src/reference/java/Part.java
+++ exercises/practice/hangman/.meta/src/reference/java/Part.java
@@ -4,5 +4,9 @@ enum Part {
     LEFT_ARM,
     RIGHT_ARM,
     LEFT_LEG,
-    RIGHT_LEG
+    RIGHT_LEG,
+    LEFT_EYE,
+    RIGHT_EYE,
+    NOSE,
+    MOUTH
 }
