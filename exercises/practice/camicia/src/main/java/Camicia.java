21e60d8a68fa6ef89be4a9a9b710041e8dd17ad7
Thibault PHAN <thibault2705@gmail.com>
2026-04-24 16:14:55 -0600

    Implement Camicia Practice Exercise (#3105)
    * Initial setup
    
    * Add Camicia class & Implement tests
    
    * Implement solution
    
    * Implement solution
    
    * Implement solution
    
    * Implement solution
    
    * Implement solution
    
    * Implement solution
    
    * Implement solution
    
    * Implement solution
    
    * Implement solution
    
    * Implement solution
    
    * Implement solution
    
    * Implement solution
    
    * Move solution to the .meta package
    
    * Move to .meta/src/reference/java/
    
    * Config Camicia exercise
    
    * End file with a new line
    
    * Update author
    
    * Make simulateGame static and return result record
    
    * Update solution and disable more tests
    
    * Update gradle version
    
    * Update gradle
    
    * Update result record, config, instructions and tests
    
    * Format config

---
+++ exercises/practice/camicia/src/main/java/Camicia.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Camicia {
+
+    static CamiciaResult simulateGame(List<String> playerA, List<String> playerB) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
