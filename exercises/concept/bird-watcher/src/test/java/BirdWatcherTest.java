596d751ac197a8f53f86059279a3fe2c7b34293f
Jagdish Prajapati <jagadishdrp@gmail.com>
2026-05-09 01:46:51 +0530

    Refactor BirdWatcherTest to use new array for each test

--- exercises/concept/bird-watcher/src/test/java/BirdWatcherTest.java
+++ exercises/concept/bird-watcher/src/test/java/BirdWatcherTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -7,58 +6,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BirdWatcherTest {
 
-    private static final int DAY1 = 7;
-    private static final int DAY2 = 0;
-    private static final int DAY3 = 4;
-    private static final int DAY4 = 8;
-    private static final int DAY5 = 5;
-    private static final int DAY6 = 2;
-    private static final int TODAY = 3;
-
-    private BirdWatcher birdWatcher;
-    private final int[] currentWeek = {DAY1, DAY2, DAY3, DAY4, DAY5, DAY6, TODAY};
-    private final int[] lastWeek = {0, 2, 5, 3, 7, 8, 4};
-
-    @BeforeEach
-    public void setUp() {
-        birdWatcher = new BirdWatcher(currentWeek);
-    }
-
     @Test
     @Tag("task:1")
     @DisplayName("The getLastWeek method correctly returns last week's counts")
     public void itTestGetLastWeek() {
-        assertThat(birdWatcher.getLastWeek())
-            .isEqualTo(lastWeek);
+        assertThat(BirdWatcher.getLastWeek()).isEqualTo(new int[] {0, 2, 5, 3, 7, 8, 4});
     }
 
     @Test
     @Tag("task:2")
     @DisplayName("The getToday method correctly returns today's counts")
     public void itTestGetToday() {
-        assertThat(birdWatcher.getToday()).isEqualTo(TODAY);
+        int[] counts = new int[] {8, 8, 9, 5, 4, 7, 10};
+        BirdWatched birdWatcher = new BirdWatcher(counts);
+        assertThat(birdWatcher.getToday()).isEqualTo(10);
     }
 
     @Test
     @Tag("task:3")
     @DisplayName("The incrementTodaysCount method correctly increments today's counts")
     public void itIncrementTodaysCount() {
+        int[] counts = new int[] {8, 8, 9, 2, 1, 6, 4};
+        BirdWatched birdWatcher = new BirdWatcher(counts);
         birdWatcher.incrementTodaysCount();
-        assertThat(birdWatcher.getToday()).isEqualTo(TODAY + 1);
+        assertThat(birdWatcher.getToday()).isEqualTo(5);
     }
 
     @Test
     @Tag("task:3")
     @DisplayName("The incrementTodaysCount does not change count for other days")
     public void itIncrementDoesNotChangeCountForOtherDays() {
+        int[] counts = new int[] {5, 1, 0, 4, 2, 3, 0};
+        BirdWatched birdWatcher = new BirdWatcher(counts);
         birdWatcher.incrementTodaysCount();
-        assertThat(birdWatcher.getCountForFirstDays(6)).isEqualTo(DAY1 + DAY2 + DAY3 + DAY4 + DAY5 + DAY6);
+        assertThat(birdWatcher.getCountForFirstDays(6)).isEqualTo(15);
     }
 
     @Test
     @Tag("task:4")
     @DisplayName("The hasDayWithoutBirds method returns true when at least one day had no visits")
     public void itHasDayWithoutBirds() {
+        int[] counts = new int[] {5, 5, 4, 0, 7, 6, 7};
+        BirdWatched birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.hasDayWithoutBirds()).isTrue();
     }
 
@@ -66,7 +55,8 @@ public class BirdWatcherTest {
     @Tag("task:4")
     @DisplayName("The hasDayWithoutBirds method returns false when no day had zero visits")
     public void itShouldNotHaveDaysWithoutBirds() {
-        birdWatcher = new BirdWatcher(new int[]{1, 2, 5, 3, 7, 8, 4});
+        int[] counts = new int[] {4, 5, 9, 10, 9, 4, 3};
+        BirdWatched birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.hasDayWithoutBirds()).isFalse();
     }
 
@@ -74,7 +64,8 @@ public class BirdWatcherTest {
     @Tag("task:4")
     @DisplayName("The hasDayWithoutBirds method returns true if the last day has zero visits")
     public void itHasLastDayWithoutBirds() {
-        birdWatcher = new BirdWatcher(new int[]{1, 2, 5, 3, 7, 8, 0});
+        int[] counts = new int[] {1, 2, 5, 3, 7, 8, 0};
+        BirdWatched birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.hasDayWithoutBirds()).isTrue();
     }
 
@@ -82,30 +73,35 @@ public class BirdWatcherTest {
     @Tag("task:5")
     @DisplayName("The getCountForFirstDays method returns correct visits' count for given number of days")
     public void itTestGetCountForFirstDays() {
-        assertThat(birdWatcher.getCountForFirstDays(4)).isEqualTo(DAY1 + DAY2 + DAY3 + DAY4);
+        int[] counts = new int[] {5, 9, 12, 6, 8, 8, 17};
+        BirdWatched birdWatcher = new BirdWatcher(counts);
+        assertThat(birdWatcher.getCountForFirstDays(4)).isEqualTo(32);
     }
 
     @Test
     @Tag("task:5")
     @DisplayName("The getCountForFirstDays method returns overall count when number of days is higher than array size")
     public void itTestGetCountForMoreDaysThanTheArraySize() {
-        assertThat(birdWatcher.getCountForFirstDays(10))
-            .isEqualTo(DAY1 + DAY2 + DAY3 + DAY4 + DAY5 + DAY6 + TODAY);
+        int[] counts = new int[] {5, 9, 12, 6, 8, 8, 17};
+        BirdWatched birdWatcher = new BirdWatcher(counts);
+        assertThat(birdWatcher.getCountForFirstDays(65)).isEqualTo();
     }
 
     @Test
     @Tag("task:6")
     @DisplayName("The getBusyDays method returns the correct count of busy days")
     public void itTestGetCountForBusyDays() {
-        // DAY1, DAY4 and DAY5 are all >= 5 birds
-        assertThat(birdWatcher.getBusyDays()).isEqualTo(3);
+        int[] counts = new int[] {4, 9, 5, 7, 8, 8, 2};
+        BirdWatched birdWatcher = new BirdWatcher(counts);
+        assertThat(birdWatcher.getBusyDays()).isEqualTo(5);
     }
 
     @Test
     @Tag("task:6")
     @DisplayName("The getBusyDays method correctly returns zero in case of no busy days")
     public void itShouldNotHaveBusyDays() {
-        birdWatcher = new BirdWatcher(new int[]{1, 2, 3, 3, 2, 1, 4});
+        int[] counts = new int[] {1, 2, 3, 3, 2, 1, 4};
+        BirdWatched birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.getBusyDays()).isEqualTo(0);
     }
 }
