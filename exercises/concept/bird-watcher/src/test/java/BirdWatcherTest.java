f89eb6744b64007403c70d93af45f672ce1b527d
Jagdish Prajapati <jagadishdrp@gmail.com>
2026-05-09 01:50:09 +0530

    Fix class name from BirdWatched to BirdWatcher

--- exercises/concept/bird-watcher/src/test/java/BirdWatcherTest.java
+++ exercises/concept/bird-watcher/src/test/java/BirdWatcherTest.java
@@ -18,7 +18,7 @@ public class BirdWatcherTest {
     @DisplayName("The getToday method correctly returns today's counts")
     public void itTestGetToday() {
         int[] counts = new int[] {8, 8, 9, 5, 4, 7, 10};
-        BirdWatched birdWatcher = new BirdWatcher(counts);
+        BirdWatcher birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.getToday()).isEqualTo(10);
     }
 
@@ -27,7 +27,7 @@ public class BirdWatcherTest {
     @DisplayName("The incrementTodaysCount method correctly increments today's counts")
     public void itIncrementTodaysCount() {
         int[] counts = new int[] {8, 8, 9, 2, 1, 6, 4};
-        BirdWatched birdWatcher = new BirdWatcher(counts);
+        BirdWatcher birdWatcher = new BirdWatcher(counts);
         birdWatcher.incrementTodaysCount();
         assertThat(birdWatcher.getToday()).isEqualTo(5);
     }
@@ -37,7 +37,7 @@ public class BirdWatcherTest {
     @DisplayName("The incrementTodaysCount does not change count for other days")
     public void itIncrementDoesNotChangeCountForOtherDays() {
         int[] counts = new int[] {5, 1, 0, 4, 2, 3, 0};
-        BirdWatched birdWatcher = new BirdWatcher(counts);
+        BirdWatcher birdWatcher = new BirdWatcher(counts);
         birdWatcher.incrementTodaysCount();
         assertThat(birdWatcher.getCountForFirstDays(6)).isEqualTo(15);
     }
@@ -47,7 +47,7 @@ public class BirdWatcherTest {
     @DisplayName("The hasDayWithoutBirds method returns true when at least one day had no visits")
     public void itHasDayWithoutBirds() {
         int[] counts = new int[] {5, 5, 4, 0, 7, 6, 7};
-        BirdWatched birdWatcher = new BirdWatcher(counts);
+        BirdWatcher birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.hasDayWithoutBirds()).isTrue();
     }
 
@@ -56,7 +56,7 @@ public class BirdWatcherTest {
     @DisplayName("The hasDayWithoutBirds method returns false when no day had zero visits")
     public void itShouldNotHaveDaysWithoutBirds() {
         int[] counts = new int[] {4, 5, 9, 10, 9, 4, 3};
-        BirdWatched birdWatcher = new BirdWatcher(counts);
+        BirdWatcher birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.hasDayWithoutBirds()).isFalse();
     }
 
@@ -65,7 +65,7 @@ public class BirdWatcherTest {
     @DisplayName("The hasDayWithoutBirds method returns true if the last day has zero visits")
     public void itHasLastDayWithoutBirds() {
         int[] counts = new int[] {1, 2, 5, 3, 7, 8, 0};
-        BirdWatched birdWatcher = new BirdWatcher(counts);
+        BirdWatcher birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.hasDayWithoutBirds()).isTrue();
     }
 
@@ -74,7 +74,7 @@ public class BirdWatcherTest {
     @DisplayName("The getCountForFirstDays method returns correct visits' count for given number of days")
     public void itTestGetCountForFirstDays() {
         int[] counts = new int[] {5, 9, 12, 6, 8, 8, 17};
-        BirdWatched birdWatcher = new BirdWatcher(counts);
+        BirdWatcher birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.getCountForFirstDays(4)).isEqualTo(32);
     }
 
@@ -83,7 +83,7 @@ public class BirdWatcherTest {
     @DisplayName("The getCountForFirstDays method returns overall count when number of days is higher than array size")
     public void itTestGetCountForMoreDaysThanTheArraySize() {
         int[] counts = new int[] {5, 9, 12, 6, 8, 8, 17};
-        BirdWatched birdWatcher = new BirdWatcher(counts);
+        BirdWatcher birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.getCountForFirstDays(65)).isEqualTo();
     }
 
@@ -92,7 +92,7 @@ public class BirdWatcherTest {
     @DisplayName("The getBusyDays method returns the correct count of busy days")
     public void itTestGetCountForBusyDays() {
         int[] counts = new int[] {4, 9, 5, 7, 8, 8, 2};
-        BirdWatched birdWatcher = new BirdWatcher(counts);
+        BirdWatcher birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.getBusyDays()).isEqualTo(5);
     }
 
@@ -101,7 +101,7 @@ public class BirdWatcherTest {
     @DisplayName("The getBusyDays method correctly returns zero in case of no busy days")
     public void itShouldNotHaveBusyDays() {
         int[] counts = new int[] {1, 2, 3, 3, 2, 1, 4};
-        BirdWatched birdWatcher = new BirdWatcher(counts);
+        BirdWatcher birdWatcher = new BirdWatcher(counts);
         assertThat(birdWatcher.getBusyDays()).isEqualTo(0);
     }
 }
