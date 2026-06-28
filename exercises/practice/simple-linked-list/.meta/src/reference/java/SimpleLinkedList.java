132b92e7730967782e923b5c8ca47cd7e84fe4b7
AdityaPudasaini <workingaditya00@gmail.com>
2026-06-28 10:03:09 +0545

    simple-linked-list: remove unused imports

--- exercises/practice/simple-linked-list/.meta/src/reference/java/SimpleLinkedList.java
+++ exercises/practice/simple-linked-list/.meta/src/reference/java/SimpleLinkedList.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
--- exercises/practice/simple-linked-list/src/test/java/SimpleLinkedListTest.java
+++ exercises/practice/simple-linked-list/src/test/java/SimpleLinkedListTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.util.List;
 import java.util.NoSuchElementException;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
