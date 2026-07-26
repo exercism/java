46565626dbee94acb1675687fa1aee1d59b473de
David Corrales <98057946+devdavco@users.noreply.github.com>
2026-07-26 08:58:21 -0500

    Import DateTimeFormatter in AppointmentScheduler
    Added import for DateTimeFormatter to format date and time.

--- exercises/concept/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
+++ exercises/concept/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 class AppointmentScheduler {
     public LocalDateTime schedule(String appointmentDateDescription) {
